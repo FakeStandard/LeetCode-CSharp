27a36de53cc889fd5624af22941c7bfd5264a7ec
FakeStandard <rileylin.space@gmail.com>
2021-06-01 21:45:49 +0800

    new problem

---
+++ 0977.Squares-of-a-sort-array/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _0977.Squares_of_a_sort_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, -1, 0, 3, 10 };
+
+            Solution s = new Solution();
+
+            int[] result = s.SortedSquares(arr);
+
+            foreach (var item in result)
+                Console.Write($"[{item}] ");
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+        public int[] SortedSquares(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+                nums[i] = nums[i] * nums[i];
+
+            Array.Sort(nums);
+
+            return nums;
+        }
+    }
+}

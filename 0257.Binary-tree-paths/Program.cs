ba173bc58eea68ddd059cdba433bc5d1e109d5c1
FakeStandard <rileylin.space@gmail.com>
2021-06-01 17:41:51 +0800

    (pending)

---
+++ 0257.Binary-tree-paths/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0257.Binary_tree_paths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Input: root = [1, 2, 3, null, 5]
+            // Output:["1->2->5","1->3"]
+
+
+            Console.ReadLine();
+        }
+    }
+
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     public int val;
+     *     public TreeNode left;
+     *     public TreeNode right;
+     *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public IList<string> BinaryTreePaths(TreeNode root)
+        {
+            return null;
+        }
+    }
+}
---
+++ 0257.Binary-tree-paths/README.md
@@ -0,0 +1,16 @@
+# 0257. Binary Tree Paths
+Given the `root` of a binary tree, return *all root-to-leaf paths in **any order**.*
+
+A **leaf** is a node with no childern.
+
+### Example 1:
+```
+Input: root = [1,2,3,null,5]
+Output: ["1->2->5","1->3"]
+```
+
+### Example 2:
+```
+Input: root = [1]
+Output: ["1"]
+```

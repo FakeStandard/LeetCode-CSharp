c5ba680293c3e722355c7e6891270e8ccb54b132
FakeStandard <rileylin.space@gmail.com>
2021-06-01 20:51:56 +0800

    Improvement performance

--- 0700.Search-in-a-inary-search-tree/Program.cs
+++ 0700.Search-in-a-inary-search-tree/Program.cs
@@ -56,16 +56,24 @@ namespace _0700.Search_in_a_inary_search_tree
     {
         public TreeNode SearchBST(TreeNode root, int val)
         {
+            //if (root == null)
+            //    return null;
+            //else if (root.val == val)
+            //    return root;
+            //else if (val < root.val)
+            //    return SearchBST(root.left, val);
+            //else if (val > root.val)
+            //    return SearchBST(root.right, val);
+            //else
+            //    return null;
+
             if (root == null)
                 return null;
-            else if (root.val == val)
+
+            if (root.val == val)
                 return root;
-            else if (val < root.val)
-                return SearchBST(root.left, val);
-            else if (val > root.val)
-                return SearchBST(root.right, val);
-            else
-                return null;
+
+            return val < root.val ? SearchBST(root.left, val) : SearchBST(root.right, val);
         }
     }
 }

ee50dd65c98ff9c0bedc1e685121653d805b3e27
FakeStandard <rileylin.space@gmail.com>
2021-06-03 15:37:19 +0800

    done

--- 0257.Binary-tree-paths/Program.cs
+++ 0257.Binary-tree-paths/Program.cs
@@ -47,7 +47,29 @@ namespace _0257.Binary_tree_paths
     {
         public IList<string> BinaryTreePaths(TreeNode root)
         {
-            return null;
+            List<string> res = new List<string>();
+
+            if (root == null) return null;
+
+            Path(root, string.Empty, res);
+
+            return res;
+        }
+
+        private void Path(TreeNode node, string currentPath, List<string> res)
+        {
+            currentPath += node.val + "->";
+
+            if (node.left == null && node.right == null)
+            {
+                res.Add(currentPath.Substring(0, currentPath.Length - 2));
+                return;
+            }
+
+            if (node.left != null)
+                Path(node.left, currentPath, res);
+            if (node.right != null)
+                Path(node.right, currentPath, res);
         }
     }
 }

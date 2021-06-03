010fd6c3406408aef2347b71a2d62aaf6b5b70c0
FakeStandard <rileylin.space@gmail.com>
2021-06-03 16:51:51 +0800

    new

---
+++ 0021.Merge-two-sorted-lists/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace _0021.Merge_two_sorted_lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     public int val;
+     *     public ListNode next;
+     *     public ListNode(int val=0, ListNode next=null) {
+     *         this.val = val;
+     *         this.next = next;
+     *     }
+     * }
+     */
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode MergeTwoLists(ListNode l1, ListNode l2)
+        {
+            if (l1 == null || l2 == null) return l1 == null ? l2 : l1;
+
+            ListNode res = new ListNode(), cur = res;
+
+            while (l1 != null && l2 != null)
+            {
+                if (l1.val <= l2.val)
+                {
+                    cur.next = l1;
+                    l1 = l1.next;
+                }
+                else
+                {
+                    cur.next = l2;
+                    l2 = l2.next;
+                }
+
+                cur = cur.next;
+            }
+
+            cur.next = l1 == null ? l2 : l1;
+
+            return res.next;
+        }
+    }
+}

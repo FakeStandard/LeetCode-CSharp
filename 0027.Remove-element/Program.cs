518febf73aad9c2cc8091fb26bcc2d9c12565d4e
FakeStandard <rileylin.space@gmail.com>
2021-06-03 19:41:33 +0800

    add 0026,0027,0028

--- 0027.Remove-element/Program.cs
+++ 0027.Remove-element/Program.cs
@@ -16,7 +16,18 @@ namespace _0027.Remove_element
     {
         public int RemoveElement(int[] nums, int val)
         {
+            int res = 0;
 
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    nums[res] = nums[i];
+                    res++;
+                }
+            }
+
+            return res;
         }
     }
 }
--- 0028.Implement-strStr/Program.cs
+++ 0028.Implement-strStr/Program.cs
@@ -16,7 +16,13 @@ namespace _0028.Implement_strStr
     {
         public int StrStr(string haystack, string needle)
         {
+            if (string.IsNullOrWhiteSpace(haystack) && string.IsNullOrWhiteSpace(needle))
+                return 0;
 
+            if (haystack.Contains(needle))
+                return haystack.IndexOf(needle);
+            else
+                return -1;
         }
     }
 }

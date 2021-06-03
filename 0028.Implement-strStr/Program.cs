9ee6284325fe29eabf73a6bba772053c7e80ec7c
FakeStandard <rileylin.space@gmail.com>
2021-06-03 18:09:39 +0800

    new

---
+++ 0028.Implement-strStr/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _0028.Implement_strStr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+        public int StrStr(string haystack, string needle)
+        {
+
+        }
+    }
+}

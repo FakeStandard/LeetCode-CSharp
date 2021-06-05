4db1d9e8fb205d9d2687c20701691a37474b2d88
FakeStandard <rileylin.space@gmail.com>
2021-06-05 16:32:13 +0800

    new

---
+++ 0083.Remove-duplicates-from-sorted-list/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace _0083.Remove_duplicates_from_sorted_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     public int val;
+     *     public ListNode next;
+     *     public ListNode(int val=0, ListNode next=null) {
+     *         this.val = val;
+     *         this.next = next;
+     *     }
+     * }
+     */
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public class Solution
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head != null && head.next != null)
+            {
+                var current = head;
+
+                while(current!=null && current.next != null)
+                {
+                    if (current.val == current.next.val)
+                        current.next = current.next.next;
+                    else
+                        current = current.next;
+                }
+            }
+
+            return head;
+
+            //if (head == null) return null;
+
+            //ListNode current = new ListNode(head.val);
+            //ListNode res = current;
+
+            //while (head != null)
+            //{
+            //    if (head.val != current.val)
+            //    {
+            //        current.next = new ListNode(head.val);
+            //        current = current.next;
+            //    }
+
+            //    head = head.next;
+            //}
+
+            //return res;
+        }
+    }
+}

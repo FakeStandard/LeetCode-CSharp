6ee47e06786300563367caca04fd40f2e763b471
FakeStandard <rileylin.space@gmail.com>
2021-06-05 18:57:53 +0800

    new

---
+++ 0118.Pascal's-triangle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0118.Pascal_s_triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            List<List<int>> res = new List<List<int>>();
+
+            res.Add(new List<int>() { 1 });
+            res.Add(new List<int>() { 1, 1 });
+
+            for (int i = 2; i <= numRows; i++)
+            {
+                List<int> list = new List<int>();
+                list.Add(1);
+
+                for (int j = 1; j < i - 1; j++)
+                    list.Add(res[i - 2][j-1] + res[i - 2][j]);
+
+                list.Add(1);
+                res.Add(list);
+            }
+
+            return res.ToArray();
+        }
+    }
+}

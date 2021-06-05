c63fb692664bf339d667c340fbc582f33d05e6bd
FakeStandard <rileylin.space@gmail.com>
2021-06-05 17:53:34 +0800

    new

---
+++ 0104.Maxium-depth-of-binary-tree/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace _0104.Maxium_depth_of_binary_tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // [3,9,20,null,null,15,7]
+            TreeNode n1 = new TreeNode(3);
+            TreeNode n2 = new TreeNode(9);
+            TreeNode n3 = new TreeNode(20);
+            TreeNode n4 = null;
+            TreeNode n5 = null;
+            TreeNode n6 = new TreeNode(15);
+            TreeNode n7 = new TreeNode(7);
+
+            TreeNode root = n1;
+            n1.left = n2;
+            n1.right = n3;
+            n2.left = n4;
+            n2.right = n5;
+            n3.left = n6;
+            n3.right = n7;
+
+            Solution s = new Solution();
+
+            Console.WriteLine(s.MaxDepth(root));
+
+            Console.ReadLine();
+        }
+    }
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     public int val;
+     *     public TreeNode left;
+     *     public TreeNode right;
+     *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+        {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class Solution
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            int left = MaxDepth(root.left);
+            int right = MaxDepth(root.right);
+
+            return left > right ? left + 1 : right + 1;
+        }
+    }
+}

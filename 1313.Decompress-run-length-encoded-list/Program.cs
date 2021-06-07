72f29c878b2eb23b0418b4f89bf2d583b392d4c6
FakeStandard <rileylin.space@gmail.com>
2021-06-07 17:45:49 +0800

    new

---
+++ 1313.Decompress-run-length-encoded-list/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1313.Decompress_run_length_encoded_list
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+        public int[] DecompressRLElist(int[] nums)
+        {
+            List<int> res = new List<int>();
+
+            for (int i = 0; i < nums.Length; i += 2)
+                for(int j = 0; j < nums[i]; j++)
+                    res.Add(nums[i+1]);
+
+            return res.ToArray();
+        }
+    }
+}

4ac17508c2ac96f1979cc670dddc521737d25e76
FakeStandard <rileylin.space@gmail.com>
2021-06-07 14:18:18 +0800

    new

---
+++ 0167.Two-sum-II-input-array-is-sorted/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace _0167.Two_sum_II_input_array_is_sorted
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int left = 0;
+            int right = numbers.Length - 1;
+
+            while (left < right)
+            {
+                if (numbers[left] + numbers[right] == target)
+                    return new int[] { left + 1, right + 1 };
+
+                if (numbers[left] + numbers[right] < target)
+                    left++;
+                else
+                    right--;
+            }
+
+            return new int[] { left + 1, right + 1 };
+
+            //// 逐一取得每個元素
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+            //    // 依照取得元素的索引，與索引後的陣列
+            //    for (int j = i + 1; j < numbers.Length; j++)
+            //    {
+            //        if (numbers[i] + numbers[j] == target)
+            //            return new int[] { i + 1, j + 1 };
+            //    }
+            //}
+
+            //throw new Exception("No two sum solution.");
+        }
+    }
+}

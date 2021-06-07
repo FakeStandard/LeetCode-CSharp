03ba2a288343e06b2cef1eb582f4f76f0fe53b37
FakeStandard <rileylin.space@gmail.com>
2021-06-07 12:02:16 +0800

    new

---
+++ 1342.Number-of-steps-to-reduce-a-number-to-zero/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _1342.Number_of_steps_to_reduce_a_number_to_zero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+        public int NumberOfSteps(int num)
+        {
+            int count = 0;
+
+            while (num > 0)
+            {
+                if (num % 2 == 0)
+                    num /= 2;
+                else
+                    num--;
+
+                count++;
+            }
+
+            return count;
+        }
+    }
+}

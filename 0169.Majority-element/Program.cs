abeeb2af9335cfb1f4dacbb7c080dc4a79a8883c
FakeStandard <rileylin.space@gmail.com>
2021-06-07 14:33:57 +0800

    new

---
+++ 0169.Majority-element/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0169.Majority_element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+        public int MajorityElement(int[] nums)
+        {
+            int num = nums.Length / 2;
+
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]))
+                    dic[nums[i]]++;
+                else
+                    dic.Add(nums[i], 1);
+            }
+
+            foreach(var d in dic)
+            {
+                if (d.Value > num)
+                    return d.Key;
+            }
+
+            return 0;
+        }
+    }
+}

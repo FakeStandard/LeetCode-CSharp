1912165e2ca6f9f5c49e8566f3f08e1f03b002b3
FakeStandard <rileylin.space@gmail.com>
2021-06-07 14:48:57 +0800

    new

---
+++ 0172.Factorial-trailing-zeroes/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _0172.Factorial_trailing_zeroes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+
+            s.TrailingZeroes(13);
+
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+        public int TrailingZeroes(int n)
+        {
+            // 每 5 就有一個零
+            int count = 0;
+
+            while (n > 0)
+            {
+                count += n / 5;
+                n /= 5;
+            }
+
+            return count++;
+        }
+    }
+}

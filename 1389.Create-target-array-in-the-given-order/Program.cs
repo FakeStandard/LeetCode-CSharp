75f5a7f47767948a195284c979a3a936469cbb13
FakeStandard <rileylin.space@gmail.com>
2021-06-07 21:30:56 +0800

    new

---
+++ 1389.Create-target-array-in-the-given-order/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1389.Create_target_array_in_the_given_order
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+        public int[] CreateTargetArray(int[] nums, int[] index)
+        {
+            List<int> res = new List<int>();
+
+            for(int i = 0; i < nums.Length; i++)
+                res.Insert(index[i], nums[i]);
+
+            return res.ToArray();
+        }
+    }
+}

b5b082e080d99f8b2f27ba8b975d184e9630ffdf
FakeStandard <rileylin.space@gmail.com>
2021-06-08 20:23:46 +0800

    new

---
+++ 1773.Count-items-matching-a-rule/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1773.Count_items_matching_a_rule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine();
+        }
+    }
+
+    public class Solution
+    {
+        public int CountMatches(IList<IList<string>> items, string ruleKey, string ruleValue)
+        {
+            int index = 0;
+            int count = 0;
+
+            if (ruleKey == "type") index = 0;
+            else if (ruleKey == "color") index = 1;
+            else if (ruleKey == "name") index = 2;
+
+            foreach (var item in items)
+            {
+                if (item[index] == ruleValue)
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}

ea74fbcee3c86bdb8ca4d188fec79aed6b8eb217
FakeStandard <rileylin.space@gmail.com>
2021-06-09 20:09:10 +0800

    (1614) new

---
+++ 1614.Maximum-nesting-depth-of-the-parentheses/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1614.Maximum_nesting_depth_of_the_parentheses
+{
+    public class Solution
+    {
+        public int MaxDepth(string s)
+        {
+            int count = 0;
+            int depth = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == '(')
+                {
+                    count++;
+                    if (count > depth)
+                        depth = count;
+                }
+                else if (s[i] == ')')
+                    count--;
+            }
+
+            return depth;
+        }
+    }
+}
---
+++ UnitTest/_1614_Maximum_nesting_depth_of_the_parentheses_Test.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1614.Maximum_nesting_depth_of_the_parentheses;
+
+namespace _1614.Maximum_nesting_depth_of_the_parentheses.Tests
+{
+    [TestClass()]
+    public class _1614_Maximum_nesting_depth_of_the_parentheses_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void MaxDepth_Test1()
+        {
+            // Arrange
+            string s = "(1+(2*3)+((8)/4))+1";
+            var expected = 3;
+
+            // Act
+            var actual = solution.MaxDepth(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MaxDepth_Test2()
+        {
+            // Arrange
+            string s = "(1)+((2))+(((3)))";
+            var expected = 3;
+
+            // Act
+            var actual = solution.MaxDepth(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MaxDepth_Test3()
+        {
+            // Arrange
+            string s = "1+(2*3)/(2-1)";
+            var expected = 1;
+
+            // Act
+            var actual = solution.MaxDepth(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MaxDepth_Test4()
+        {
+            // Arrange
+            string s = "1";
+            var expected = 0;
+
+            // Act
+            var actual = solution.MaxDepth(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

420c61e2393c0854cdb953f845adefbe7d51b5f5
FakeStandard <rileylin.space@gmail.com>
2021-06-09 22:49:08 +0800

    (1480) add unit test and change the project to lib

---
+++ 1480.Running-sum-of-1d-array/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _1480.Running_sum_of_1d_array
+{
+    public class Solution
+    {
+        public int[] RunningSum(int[] nums)
+        {
+            for (int i = 1; i < nums.Length; i++)
+                nums[i] = nums[i] + nums[i - 1];
+
+            return nums;
+
+            //int i, j, tmp;
+            //int[] result = new int[nums.Length];
+
+            //for (i = 0; i < nums.Length; i++)
+            //{
+            //    tmp = 0;
+
+            //    for (j = 0; j <= i; j++)
+            //        tmp = tmp + nums[j];
+
+            //    result[i] = tmp;
+            //}
+
+            //return result;
+        }
+    }
+}
---
+++ UnitTest/_1480_Running_sum_of_1d_array_Test.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1480.Running_sum_of_1d_array;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+
+namespace _1480.Running_sum_of_1d_array.Tests
+{
+    [TestClass()]
+    public class _1480_Running_sum_of_1d_array_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void RunningSum_Test1()
+        {
+            // Arrange
+            int[] nums = { 1, 2, 3, 4 };
+            int[] expected = { 1, 3, 6, 10 };
+
+            // Act
+            var actual = solution.RunningSum(nums);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void RunningSum_Test2()
+        {
+            // Arrange
+            int[] nums = { 1, 1, 1, 1, 1 };
+            int[] expected = { 1, 2, 3, 4, 5 };
+
+            // Act
+            var actual = solution.RunningSum(nums);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void RunningSum_Test3()
+        {
+            // Arrange
+            int[] nums = { 3, 1, 2, 10, 1 };
+            int[] expected = { 3, 4, 6, 16, 17 };
+
+            // Act
+            var actual = solution.RunningSum(nums);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

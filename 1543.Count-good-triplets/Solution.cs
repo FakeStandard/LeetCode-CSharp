d263cd7f94dc5dcb423b6c6a44ebb86618f19567
FakeStandard <rileylin.space@gmail.com>
2021-06-09 21:38:13 +0800

    (1543) new

---
+++ 1543.Count-good-triplets/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _1543.Count_good_triplets
+{
+    public class Solution
+    {
+        public int CountGoodTriplets(int[] arr, int a, int b, int c)
+        {
+            int count = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (Math.Abs(arr[i] - arr[j]) <= a)
+                    {
+                        for (int k = j + 1; k < arr.Length; k++)
+                        {
+                            if (Math.Abs(arr[j] - arr[k]) <= b)
+                            {
+                                if (Math.Abs(arr[i] - arr[k]) <= c)
+                                {
+                                    count++;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ UnitTest/_1543_Count_good_triplets_Test.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1543.Count_good_triplets;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1543.Count_good_triplets.Tests
+{
+    [TestClass()]
+    public class _1543_Count_good_triplets_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void CountGoodTriplets_Test1()
+        {
+            // Arrange
+            int[] arr = { 3, 0, 1, 1, 9, 7 };
+            int a = 7;
+            int b = 2;
+            int c = 3;
+            var expected = 4;
+
+            // Act
+            var actual = solution.CountGoodTriplets(arr, a, b, c);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CountGoodTriplets_Test2()
+        {
+            // Arrange
+            int[] arr = { 1, 1, 2, 2, 3 };
+            int a = 0;
+            int b = 0;
+            int c = 1;
+            var expected = 4;
+
+            // Act
+            var actual = solution.CountGoodTriplets(arr, a, b, c);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

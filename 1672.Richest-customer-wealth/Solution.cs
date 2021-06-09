73d584e5fc81841c8a964d27bb10af92910a5c32
FakeStandard <rileylin.space@gmail.com>
2021-06-09 13:54:53 +0800

    (1672) add unit test and change the project to lib

---
+++ 1672.Richest-customer-wealth/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _1672.Richest_customer_wealth
+{
+    public class Solution
+    {
+        public int MaximumWealth(int[][] accounts)
+        {
+            int i, j, sum;
+            int max = 0;
+
+            for (i = 0; i < accounts.GetLength(0); i++)
+            {
+                sum = 0;
+
+                for (j = 0; j < accounts[i].Length; j++)
+                    sum += accounts[i][j];
+
+                if (sum >= max)
+                    max = sum;
+            }
+
+            return max;
+
+            //if (accounts.Length == 0) return 0;
+
+            //int max = 0;
+
+            //for(int i = 0; i < accounts.Length; i++)
+            //{
+            //    int sum = accounts[i].Sum();
+            //    max = Math.Max(max, sum);
+            //}
+
+            //return max;
+        }
+    }
+}
---
+++ UnitTest/_1672_Richest_customer_wealth_Test.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1672.Richest_customer_wealth;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1672.Richest_customer_wealth.Tests
+{
+    [TestClass()]
+    public class _1672_Richest_customer_wealth_Test
+    {
+        Solution s = new Solution();
+
+        [TestMethod()]
+        public void MaximumWealth_Test1()
+        {
+            // Arrange
+            int[][] accounts =
+        {
+                new int[]{ 1,2,3 },
+                new int[]{ 3,2,1 }
+            };
+
+            var expected = 6;
+
+            // Act
+            var actual = s.MaximumWealth(accounts);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MaximumWealth_Test2()
+        {
+            // Arrange
+            int[][] accounts =
+            {
+                new int[]{ 1,5 },
+                new int[]{ 7,3 },
+                new int[]{ 3,5 }
+            };
+
+            var expected = 10;
+
+            // Act
+            var actual = s.MaximumWealth(accounts);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MaximumWealth_Test3()
+        {
+            // Arrange
+            int[][] accounts =
+            {
+                new int[]{ 2,8,7 },
+                new int[]{ 7,1,3 },
+                new int[]{ 1, 9, 5  }
+            };
+
+            var expected = 17;
+
+            // Act
+            var actual = s.MaximumWealth(accounts);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

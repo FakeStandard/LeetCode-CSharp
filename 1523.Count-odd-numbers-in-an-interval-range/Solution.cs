6135d692bf1d9fa09296542d56e87058400ba8c9
FakeStandard <rileylin.space@gmail.com>
2021-06-09 22:04:45 +0800

    (1523) new

---
+++ 1523.Count-odd-numbers-in-an-interval-range/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1523.Count_odd_numbers_in_an_interval_range
+{
+    public class Solution
+    {
+        public int CountOdds(int low, int high)
+        {
+            return (high + 1) / 2 - (low / 2);
+
+            #region Time Limit Exceeded
+            //int count = 0;
+
+            //while (low <= high)
+            //{
+            //    if (low % 2 == 1)
+            //        count++;
+            //    low++;
+            //}
+
+            //return count;
+            #endregion
+        }
+    }
+}
---
+++ UnitTest/_1523_Count_odd_numbers_in_an_interval_range_Test.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1523.Count_odd_numbers_in_an_interval_range;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1523.Count_odd_numbers_in_an_interval_range.Tests
+{
+    [TestClass()]
+    public class _1523_Count_odd_numbers_in_an_interval_range_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void CountOdds_Test1()
+        {
+            // Arrange
+            int low = 3;
+            int high = 7;
+            var expected = 3;
+
+            // Act
+            var actual = solution.CountOdds(low, high);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CountOdds_Test2()
+        {
+            // Arrange
+            int low = 8;
+            int high = 10;
+            var expected = 1;
+
+            // Act
+            var actual = solution.CountOdds(low, high);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CountOdds_Test3()
+        {
+            // Arrange
+            int low = 407656828;
+            int high = 722359384;
+            var expected = 157351278;
+
+            // Act
+            var actual = solution.CountOdds(low, high);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

893b85b6e508c6ba7a05226f0396deec77d4b513
FakeStandard <rileylin.space@gmail.com>
2021-06-09 21:15:48 +0800

    (1486) new

---
+++ 1486.XOR-Operation-in-an-array/Solution.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _1486.XOR_Operation_in_an_array
+{
+    public class Solution
+    {
+        public int XorOperation(int n, int start)
+        {
+            int res = 0;
+
+            for(int i = 0; i < n; i++)
+                res ^= start + 2 * i;
+
+            return res;
+        }
+    }
+}
---
+++ UnitTest/_1486_XOR_Operation_in_an_array_Test.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1486.XOR_Operation_in_an_array;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1486.XOR_Operation_in_an_array.Tests
+{
+    [TestClass()]
+    public class _1486_XOR_Operation_in_an_array_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void XorOperation_Test1()
+        {
+            // Arrange
+            int n = 5;
+            int start = 0;
+            var expected = 8;
+
+            // Act
+            var actual = solution.XorOperation(n, start);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void XorOperation_Test2()
+        {
+            // Arrange
+            int n = 4;
+            int start = 3;
+            var expected = 8;
+
+            // Act
+            var actual = solution.XorOperation(n, start);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void XorOperation_Test3()
+        {
+            // Arrange
+            int n = 1;
+            int start = 7;
+            var expected = 7;
+
+            // Act
+            var actual = solution.XorOperation(n, start);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void XorOperation_Test4()
+        {
+            // Arrange
+            int n = 10;
+            int start = 5;
+            var expected = 2;
+
+            // Act
+            var actual = solution.XorOperation(n, start);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

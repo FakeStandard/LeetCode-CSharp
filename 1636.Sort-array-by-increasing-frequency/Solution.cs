06932046359687158b41e2e05f7d6fca0407b8ad
FakeStandard <rileylin.space@gmail.com>
2021-06-09 14:04:30 +0800

    (1636) add unit test and change the project to lib

---
+++ 1636.Sort-array-by-increasing-frequency/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _1636.Sort_array_by_increasing_frequency
+{
+    public class Solution
+    {
+        public int[] FrequencySort(int[] nums)
+        {
+            return nums
+                    .GroupBy(x => x)
+                    .OrderBy(x => x.Count())
+                    .ThenByDescending(x => x.Key)
+                    .SelectMany(x => Enumerable.Repeat(x.Key, x.Count()))
+                    .ToArray();
+        }
+    }
+}
---
+++ UnitTest/_1636_Sort_array_by_increasing_frequency_Test.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+
+namespace _1636.Sort_array_by_increasing_frequency.Tests
+{
+    [TestClass()]
+    public class _1636_Sort_array_by_increasing_frequency_Test
+    {
+        Solution s = new Solution();
+
+        [TestMethod()]
+        public void FrequencySort_Test1()
+        {
+            // Arrange
+            int[] nums = { 1, 1, 2, 2, 2, 3 };
+            int[] expected = { 3, 1, 1, 2, 2, 2 };
+
+            // Act
+            var act = s.FrequencySort(nums);
+
+            // Assert
+            act.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void FrequencySort_Test2()
+        {
+            // Arrange
+            int[] nums = { 2, 3, 1, 3, 2 };
+            int[] expected = { 1, 3, 3, 2, 2 };
+
+            // Act
+            var act = s.FrequencySort(nums);
+
+            // Assert
+            act.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void FrequencySort_Test3()
+        {
+            // Arrange
+            int[] nums = { -1, 1, -6, 4, 5, -6, 1, 4, 1 };
+            int[] expected = { 5, -1, 4, 4, -6, -6, 1, 1, 1 };
+
+            // Act
+            var act = s.FrequencySort(nums);
+
+            // Assert
+            act.Should().BeEquivalentTo(expected);
+        }
+    }
+}

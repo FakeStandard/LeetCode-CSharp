26a67dd9148b8fc276a9d78929aa2fbdf13240bf
FakeStandard <rileylin.space@gmail.com>
2021-06-09 22:13:15 +0800

    (1512) add unit test and change the project to lib

---
+++ 1512.Number-of-good-pairs/Solution.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _1512.Number_of_good_pairs
+{
+    public class Solution
+    {
+        public int NumIdenticalPairs(int[] nums)
+        {
+            int i, j, count = 0;
+
+            for (i = 0; i < nums.Length; i++)
+                for (j = i + 1; j < nums.Length; j++)
+                    if (nums[i] == nums[j])
+                        count++;
+
+            return count;
+        }
+    }
+}
---
+++ UnitTest/_1512_Number_of_good_pairs_Test.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1512.Number_of_good_pairs;
+
+namespace _1512.Number_of_good_pairs.Tests
+{
+    [TestClass()]
+    public class _1512_Number_of_good_pairs_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void NumIdenticalPairs_Test1()
+        {
+            // Arrange
+            int[] nums = { 1, 2, 3, 1, 1, 3 };
+            var expected = 4;
+
+            // Act
+            var actual = solution.NumIdenticalPairs(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void NumIdenticalPairs_Test2()
+        {
+            // Arrange
+            int[] nums = { 1, 1, 1, 1 };
+            var expected = 6;
+
+            // Act
+            var actual = solution.NumIdenticalPairs(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void NumIdenticalPairs_Test3()
+        {
+            // Arrange
+            int[] nums = { 1, 2, 3 };
+            var expected = 0;
+
+            // Act
+            var actual = solution.NumIdenticalPairs(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

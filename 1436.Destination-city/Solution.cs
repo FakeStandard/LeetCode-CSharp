5c421d7801a4c92fd6cfa7c3523a266b9305253b
FakeStandard <rileylin.space@gmail.com>
2021-06-10 16:48:26 +0800

    (1436) new

---
+++ 1436.Destination-city/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1436.Destination_city
+{
+    public class Solution
+    {
+        public string DestCity(IList<IList<string>> paths)
+        {
+            string end = paths[0][1];
+            bool flag = true;
+
+            while (flag)
+            {
+                flag = false;
+
+                for (int i = 0; i < paths.Count; i++)
+                {
+                    if (end == paths[i][0])
+                    {
+                        end = paths[i][1];
+                        flag = true;
+                    }
+                }
+            }
+
+            return end;
+        }
+    }
+}
---
+++ UnitTest/_1436_Destination_city_Test.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1436.Destination_city;
+using System.Collections.Generic;
+
+namespace _1436.Destination_city.Tests
+{
+    [TestClass()]
+    public class _1436_Destination_city_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void DestCity_Test1()
+        {
+            // Arrange
+            IList<IList<string>> paths = new List<IList<string>>()
+            {
+                new List<string>{ "London", "New York" },
+                new List<string>{ "New York", "Lima" },
+                new List<string>{ "Lima", "Sao Paulo" }
+            };
+            var expected = "Sao Paulo";
+
+            // Act
+            var actual = solution.DestCity(paths);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void DestCity_Test2()
+        {
+            // Arrange
+            IList<IList<string>> paths = new List<IList<string>>
+            {
+                new List<string>{ "B", "C" },
+                new List<string>{ "D", "B"},
+                new List<string>{ "C", "A"}
+            };
+            var expected = "A";
+
+            // Act
+            var actual = solution.DestCity(paths);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void DestCity_Test3()
+        {
+            // Arrange
+            IList<IList<string>> paths = new List<IList<string>>
+            {
+                new List<string>{ "A", "Z" }
+            };
+            var expected = "Z";
+
+            // Act
+            var actual = solution.DestCity(paths);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

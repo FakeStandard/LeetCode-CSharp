94a3e5fde92a8e53933dc10ab07e3a9a08ed8448
FakeStandard <rileylin.space@gmail.com>
2021-06-10 13:53:43 +0800

    (1662) new

---
+++ 1662.Check-if-two-string-arrays-are-equivalent/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _1662.Check_if_two_string_arrays_are_equivalent
+{
+    public class Solution
+    {
+        public bool ArrayStringsAreEqual(string[] word1, string[] word2)
+        {
+            return string.Join("", word1) == string.Join("", word2);
+
+            //string res1=string.Empty;
+            //string res2=string.Empty;
+
+            //int i = 0;
+
+            //for (i = 0; i < word1.Length; i++)
+            //    res1 += word1[i];
+
+            //for (i = 0; i < word2.Length; i++)
+            //    res2 += word2[i];
+
+            //return res1 == res2;
+        }
+    }
+}
---
+++ UnitTest/_1662_Check_if_two_string_arrays_are_equivalent_Test.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1662.Check_if_two_string_arrays_are_equivalent;
+
+namespace _1662.Check_if_two_string_arrays_are_equivalent.Tests
+{
+    [TestClass()]
+    public class _1662_Check_if_two_string_arrays_are_equivalent_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void ArrayStringsAreEqual_Test1()
+        {
+            // Arrange
+            string[] word1 = { "ab", "c" };
+            string[] word2 = { "a", "bc" };
+
+            // Act
+            var actual = solution.ArrayStringsAreEqual(word1, word2);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void ArrayStringsAreEqual_Test2()
+        {
+            // Arrange
+            string[] word1 = { "a", "cb" };
+            string[] word2 = { "ab", "c" };
+
+            // Act
+            var actual = solution.ArrayStringsAreEqual(word1, word2);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void ArrayStringsAreEqual_Test3()
+        {
+            // Arrange
+            string[] word1 = { "abc", "d", "defg" };
+            string[] word2 = { "abcddefg" };
+
+            // Act
+            var actual = solution.ArrayStringsAreEqual(word1, word2);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+    }
+}

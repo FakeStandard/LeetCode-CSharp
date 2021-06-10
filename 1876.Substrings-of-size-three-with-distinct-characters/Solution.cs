a4fd8d0994f29765105319c6472bc5b719ea4803
FakeStandard <rileylin.space@gmail.com>
2021-06-10 14:07:50 +0800

    (1876) new

---
+++ 1876.Substrings-of-size-three-with-distinct-characters/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _1876.Substrings_of_size_three_with_distinct_characters
+{
+    public class Solution
+    {
+        public int CountGoodSubstrings(string s)
+        {
+            if (s.Length < 3) return 0;
+            int count = 0;
+
+            for (int i = 0; i < s.Length - 2; i++)
+            {
+                if ((s[i] != s[i + 1]) && (s[i + 1] != s[i + 2]) && (s[i + 2] != s[i]))
+                    count++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ UnitTest/_1876_Substrings_of_size_three_with_distinct_characters_Test.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1876.Substrings_of_size_three_with_distinct_characters;
+
+namespace _1876.Substrings_of_size_three_with_distinct_characters.Tests
+{
+    [TestClass()]
+    public class _1876_Substrings_of_size_three_with_distinct_characters_Test
+    {
+        Solution solution = new Solution();
+     
+        [TestMethod()]
+        public void CountGoodSubstrings_Test1()
+        {
+            // Arrange
+            string s = "xyzzaz";
+            var expected = 1;
+
+            // Act
+            var actual = solution.CountGoodSubstrings(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CountGoodSubstrings_Test2()
+        {
+            // Arrange
+            string s = "aababcabc";
+            var expected = 4;
+
+            // Act
+            var actual = solution.CountGoodSubstrings(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

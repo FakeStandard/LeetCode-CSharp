a55a3f4c258fd1e5dbf5f59269fb0678ff238d40
FakeStandard <rileylin.space@gmail.com>
2021-06-10 16:08:47 +0800

    (1827) new

---
+++ 1827.Minimum-operations-to-make-the-array-increasing/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _1827.Minimum_operations_to_make_the_array_increasing
+{
+    public class Solution
+    {
+        public int MinOperations(int[] nums)
+        {
+            int count = 0;
+
+            for(int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i - 1] >= nums[i])
+                {
+                    int add = nums[i - 1] - nums[i] + 1;
+                    nums[i] += add;
+                    count += add;
+                }
+            }
+
+            return count;
+
+            //int count = 0;
+
+            //for(int i = 1; i < nums.Length; i++)
+            //{
+            //    while(nums[i - 1] >= nums[i])
+            //    {
+            //        nums[i]++;
+            //        count++;
+            //    }
+            //}
+
+            //return count;
+        }
+    }
+}
---
+++ UnitTest/_1827_Minimum_operations_to_make_the_array_increasing_Test.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1827.Minimum_operations_to_make_the_array_increasing;
+
+namespace _1827.Minimum_operations_to_make_the_array_increasing.Tests
+{
+    [TestClass()]
+    public class _1827_Minimum_operations_to_make_the_array_increasing_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void MinOperations_Test1()
+        {
+            // Arrange
+            int[] nums = { 1, 1, 1 };
+            var expected = 3;
+
+            // Act
+            var actual = solution.MinOperations(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MinOperations_Test2()
+        {
+            // Arrange
+            int[] nums = { 1, 5, 2, 4, 1 };
+            var expected = 14;
+
+            // Act
+            var actual = solution.MinOperations(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MinOperations_Test3()
+        {
+            // Arrange
+            int[] nums = { 8 };
+            var expected = 0;
+
+            // Act
+            var actual = solution.MinOperations(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

8aca696d05025b2f81ed4657f3d978445566be90
FakeStandard <rileylin.space@gmail.com>
2021-06-10 12:41:14 +0800

    (1822) new

---
+++ 1822.Sign-of-the-product-of-an-array/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _1822.Sign_of_the_product_of_an_array
+{
+    public class Solution
+    {
+        public int ArraySign(int[] nums)
+        {
+            int negative = 1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                    return 0;
+                else if (nums[i] < 0)
+                    negative = -negative;
+            }
+
+            return negative;
+        }
+    }
+}
---
+++ UnitTest/_1822_Sign_of_the_product_of_an_array_Test.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1822.Sign_of_the_product_of_an_array;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1822.Sign_of_the_product_of_an_array.Tests
+{
+    [TestClass()]
+    public class _1822_Sign_of_the_product_of_an_array_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void ArraySign_Test1()
+        {
+            // Arrange
+            int[] nums = { -1, -2, -3, -4, 3, 2, 1 };
+            var expected = 1;
+
+            // Act
+            var actual = solution.ArraySign(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ArraySign_Test2()
+        {
+            // Arrange
+            int[] nums = { 1, 5, 0, 2, -3 };
+            var expected = 0;
+
+            // Act
+            var actual = solution.ArraySign(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ArraySign_Test3()
+        {
+            // Arrange
+            int[] nums = { -1, 1, -1, 1, -1 };
+            var expected = -1;
+
+            // Act
+            var actual = solution.ArraySign(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

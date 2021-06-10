a64ea1858f3ddce26306b9ad61a2ee16549d0b86
FakeStandard <rileylin.space@gmail.com>
2021-06-10 16:26:53 +0800

    (1848) new

---
+++ 1848.Minimum-distance-to-the-target-element/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1848.Minimum_distance_to_the_target_element
+{
+    public class Solution
+    {
+        public int GetMinDistance(int[] nums, int target, int start)
+        {
+            int min = int.MaxValue;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == target)
+                    min = Math.Min(min, Math.Abs(i - start));
+            }
+
+            return min;
+        }
+    }
+}
---
+++ UnitTest/_1848_Minimum_distance_to_the_target_element_Test.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1848.Minimum_distance_to_the_target_element;
+namespace _1848.Minimum_distance_to_the_target_element.Tests
+{
+    [TestClass()]
+    public class _1848_Minimum_distance_to_the_target_element_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void GetMinDistance_Test1()
+        {
+            // Arrange
+            int[] nums = { 1, 2, 3, 4, 5 };
+            int target = 5;
+            int start = 3;
+            var expected = 1;
+
+            // Act
+            var actual = solution.GetMinDistance(nums, target, start);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetMinDistance_Test2()
+        {
+            // Arrange
+            int[] nums = { 1 };
+            int target = 1;
+            int start = 0;
+            var expected = 0;
+
+            // Act
+            var actual = solution.GetMinDistance(nums, target, start);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void GetMinDistance_Test3()
+        {
+            // Arrange
+            int[] nums = { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 };
+            int target = 1;
+            int start = 0;
+            var expected = 0;
+
+            // Act
+            var actual = solution.GetMinDistance(nums, target, start);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

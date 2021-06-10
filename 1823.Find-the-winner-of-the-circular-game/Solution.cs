f7801eca4202ee4fc4b194de9c4f2eac3db4eb37
FakeStandard <rileylin.space@gmail.com>
2021-06-10 20:51:59 +0800

    (1823) new

---
+++ 1823.Find-the-winner-of-the-circular-game/Solution.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1823.Find_the_winner_of_the_circular_game
+{
+    public class Solution
+    {
+        public int FindTheWinner(int n, int k)
+        {
+            List<int> gamer = new List<int>();
+
+            for (int i = 0; i < n; i++)
+                gamer.Add(i + 1);
+
+            int count = n;
+            int index = 0;
+
+            while (count > 1)
+            {
+                index = (index + k - 1) % gamer.Count;
+                gamer.RemoveAt(index);
+                count--;
+            }
+
+            return gamer[0];
+        }
+    }
+}
---
+++ UnitTest/_1823_Find_the_winner_of_the_circular_game_Test.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1823.Find_the_winner_of_the_circular_game;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1823.Find_the_winner_of_the_circular_game.Tests
+{
+    [TestClass()]
+    public class _1823_Find_the_winner_of_the_circular_game_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void FindTheWinner_Test1()
+        {
+            // Arrange
+            int n = 5;
+            int k = 2;
+            var expected = 3;
+
+            // Act
+            var actual = solution.FindTheWinner(n, k);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FindTheWinner_Test2()
+        {
+            // Arrange
+            int n = 6;
+            int k= 5;
+            var expected = 1;
+
+            // Act
+            var actual = solution.FindTheWinner(n, k);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

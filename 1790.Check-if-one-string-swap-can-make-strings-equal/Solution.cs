c2593c5cb4a3a968d67b0d8143af8d98da84b645
FakeStandard <rileylin.space@gmail.com>
2021-06-11 19:58:21 +0800

    (1790) new

---
+++ 1790.Check-if-one-string-swap-can-make-strings-equal/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _1790.Check_if_one_string_swap_can_make_strings_equal
+{
+    public class Solution
+    {
+        public bool AreAlmostEqual(string s1, string s2)
+        {
+            if (s1 == s2) return true;
+
+            int firstIndex = -1;
+            int secondIndex = -1;
+
+            char[] c = s1.ToCharArray();
+
+            for (int i = 0; i < c.Length; i++)
+            {
+                if (c[i] != s2[i] && firstIndex == -1)
+                    firstIndex = i;
+                else if (c[i] != s2[i] && secondIndex == -1)
+                    secondIndex = i;
+                else if (firstIndex != -1 && secondIndex != -1)
+                    break;
+            }
+
+            if (firstIndex == -1 || secondIndex == -1) return false;
+
+            char temp;
+
+            temp = c[firstIndex];
+            c[firstIndex] = c[secondIndex];
+            c[secondIndex] = temp;
+
+            return new string(c) == s2;
+        }
+    }
+}
---
+++ UnitTest/_1790_Check_if_one_string_swap_can_make_strings_equal_Test.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1790.Check_if_one_string_swap_can_make_strings_equal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1790.Check_if_one_string_swap_can_make_strings_equal.Tests
+{
+    [TestClass()]
+    public class _1790_Check_if_one_string_swap_can_make_strings_equal_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void AreAlmostEqual_Test1()
+        {
+            // Arrange
+            string s1 = "bank";
+            string s2 = "kanb";
+
+            // Act
+            var actual = solution.AreAlmostEqual(s1, s2);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void AreAlmostEqual_Test2()
+        {
+            // Arrange
+            string s1 = "attack";
+            string s2 = "defend";
+
+            // Act
+            var actual = solution.AreAlmostEqual(s1, s2);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void AreAlmostEqual_Test3()
+        {
+            // Arrange
+            string s1 = "kelb";
+            string s2 = "kelb";
+
+            // Act
+            var actual = solution.AreAlmostEqual(s1, s2);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void AreAlmostEqual_Test4()
+        {
+            // Arrange
+            string s1 = "abcd";
+            string s2 = "dcba";
+
+            // Act
+            var actual = solution.AreAlmostEqual(s1, s2);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+    }
+}

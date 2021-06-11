8b4e5246114115a04329fc0cbd72ff120894dbc0
FakeStandard <rileylin.space@gmail.com>
2021-06-11 12:30:37 +0800

    (1688) new

---
+++ 1688.Count-of-matches-in-tournament/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _1688.Count_of_matches_in_tournament
+{
+    public class Solution
+    {
+        public int NumberOfMatches(int n)
+        {
+            int count = 0;
+
+            while (n > 1)
+            {
+                count += n / 2;
+                n = n / 2 + n % 2;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ UnitTest/_1688_Count_of_matches_in_tournament_Test.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1688.Count_of_matches_in_tournament;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1688.Count_of_matches_in_tournament.Tests
+{
+    [TestClass()]
+    public class _1688_Count_of_matches_in_tournament_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void NumberOfMatches_Test1()
+        {
+            // Arrange
+            int n = 7;
+            var expected = 6;
+
+            // Act
+            var actual = solution.NumberOfMatches(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void NumberOfMatches_Test2()
+        {
+            // Arrange
+            int n = 14;
+            var expected = 13;
+
+            // Act
+            var actual = solution.NumberOfMatches(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

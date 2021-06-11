6d974c0f325e3d2c253e2f844d23d25532c22c11
FakeStandard <rileylin.space@gmail.com>
2021-06-11 18:18:34 +0800

    (1800) new

---
+++ 1800.Maximum-ascending-subarray-sum/Solution.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _1800.Maximum_ascending_subarray_sum
+{
+    public class Solution
+    {
+        public int MaxAscendingSum(int[] nums)
+        {
+            int max = nums[0];
+            int res = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > nums[i - 1])
+                {
+                    res += nums[i];
+                    if (res > max)
+                        max = res;
+                }
+                else
+                    res = nums[i];
+            }
+
+            return max;
+        }
+    }
+}
---
+++ UnitTest/_1800_Maximum_ascending_subarray_sum_Test.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1800.Maximum_ascending_subarray_sum;
+
+namespace _1800.Maximum_ascending_subarray_sum.Tests
+{
+    [TestClass()]
+    public class _1800_Maximum_ascending_subarray_sum_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void MaxAscendingSum_Test1()
+        {
+            // Arrange
+            int[] nums = { 10, 20, 30, 5, 10, 50 };
+            var expected = 65;
+
+            // Act
+            var actual = solution.MaxAscendingSum(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MaxAscendingSum_Test2()
+        {
+            // Arrange
+            int[] nums = { 10, 20, 30, 40, 50 };
+            var expected = 150;
+
+            // Act
+            var actual = solution.MaxAscendingSum(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MaxAscendingSum_Test3()
+        {
+            // Arrange
+            int[] nums = { 12, 17, 15, 13, 10, 11, 12 };
+            var expected = 33;
+
+            // Act
+            var actual = solution.MaxAscendingSum(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MaxAscendingSum_Test4()
+        {
+            // Arrange
+            int[] nums = { 100, 10, 1 };
+            var expected = 100;
+
+            // Act
+            var actual = solution.MaxAscendingSum(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

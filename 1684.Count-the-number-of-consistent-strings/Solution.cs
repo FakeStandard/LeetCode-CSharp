ca1e426ad25b94f3d4212dbe6dde89fa2234ef8b
FakeStandard <rileylin.space@gmail.com>
2021-06-13 11:44:11 +0800

    (1684) new

---
+++ 1684.Count-the-number-of-consistent-strings/Solution.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _1684.Count_the_number_of_consistent_strings
+{
+    public class Solution
+    {
+        public int CountConsistentStrings(string allowed, string[] words)
+        {
+            int count = 0;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                foreach (var word in words[i])
+                {
+                    if (!allowed.Contains(word))
+                    {
+                        count++;
+                        break;
+                    }
+                }
+            }
+
+            return words.Length - count;
+        }
+    }
+}
---
+++ UnitTest/_1684_Count_the_number_of_consistent_strings_Test.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1684.Count_the_number_of_consistent_strings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1684.Count_the_number_of_consistent_strings.Tests
+{
+    [TestClass()]
+    public class _1684_Count_the_number_of_consistent_strings_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void CountConsistentStrings_Test1()
+        {
+            // Arrange
+            string allowed = "ab";
+            string[] words = { "ad", "bd", "aaab", "baa", "badab" };
+            var expected = 2;
+
+            // Act
+            var actual = solution.CountConsistentStrings(allowed, words);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CountConsistentStrings_Test2()
+        {
+            // Arrange
+            string allowed = "abc";
+            string[] words = { "a", "b", "c", "ab", "ac", "bc", "abc" };
+            var expected = 7;
+
+            // Act
+            var actual = solution.CountConsistentStrings(allowed, words);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CountConsistentStrings_Test3()
+        {
+            // Arrange
+            string allowed = "cad";
+            string[] words = { "cc", "acd", "b", "ba", "bac", "bad", "ac", "d" };
+            var expected = 4;
+
+            // Act
+            var actual = solution.CountConsistentStrings(allowed, words);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

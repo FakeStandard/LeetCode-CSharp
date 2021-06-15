2e92aac42124c62e12d0a2a3a1dfbea54db8e57a
FakeStandard <rileylin.space@gmail.com>
2021-06-15 12:50:58 +0800

    (1700) new

---
+++ 1700.Number-of-students-unabled-to-eat-lunch/Solution.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _1700.Number_of_students_unabled_to_eat_lunch
+{
+    public class Solution
+    {
+        public int CountStudents(int[] students, int[] sandwiches)
+        {
+            Queue<int> queue = new Queue<int>();
+
+            foreach (var stu in students)
+                queue.Enqueue(stu);
+
+            bool flag;
+            int i;
+            int index = 0;
+            int student;
+
+            while (queue.Count > 0)
+            {
+                flag = false;
+
+                for (i = 0; i < queue.Count; i++)
+                {
+                    student = queue.Dequeue();
+
+                    if (student == sandwiches[index])
+                    {
+                        index++;
+                        flag = true;
+                    }
+                    else
+                        queue.Enqueue(student);
+                }
+
+                if (!flag)
+                    break;
+            }
+
+            return queue.Count;
+        }
+    }
+}
---
+++ UnitTest/_1700_Number_of_students_unable_to_eat_lunch_Test.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1700.Number_of_students_unabled_to_eat_lunch;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1700.Number_of_students_unabled_to_eat_lunch.Tests
+{
+    [TestClass()]
+    public class _1700_Number_of_students_unable_to_eat_lunch_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void CountStudents_Test1()
+        {
+            // Arrange
+            int[] students = { 1, 1, 0, 0 };
+            int[] sandwiches = { 0, 1, 0, 1 };
+            var expected = 0;
+
+            // Act
+            var actual = solution.CountStudents(students, sandwiches);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CountStudents_Test2()
+        {
+            // Arrange
+            int[] students = { 1, 1, 1, 0, 0, 1 };
+            int[] sandwiches = { 1, 0, 0, 0, 1, 1 };
+            var expected = 3;
+
+            // Act
+            var actual = solution.CountStudents(students, sandwiches);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

0c6cbaef8777625c4379a63038603544a4a9fefc
FakeStandard <rileylin.space@gmail.com>
2021-06-15 11:47:26 +0800

    (1365) add unit test and changed the project to lib

---
+++ 1365.How-many-numbers-are-smaller-than-current-number/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1365.How_many_numbers_are_smaller_than_current_number
+{
+    public class Solution
+    {
+        public int[] SmallerNumbersThanCurrent(int[] nums)
+        {
+            int i, j;
+            int count;
+            int[] result = new int[nums.Length];
+
+            for (i = 0; i < result.Length; i++)
+            {
+                count = 0;
+
+                for (j = 0; j < nums.Length; j++)
+                {
+                    if (nums[j] < nums[i])
+                        count++;
+                }
+
+                result[i] = count;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ UnitTest/_1365_How_many_numbers_are_smaller_than_current_number_Test.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1365.How_many_numbers_are_smaller_than_current_number;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+
+namespace _1365.How_many_numbers_are_smaller_than_current_number.Tests
+{
+    [TestClass()]
+    public class _1365_How_many_numbers_are_smaller_than_current_number_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void SmallerNumbersThanCurrent_Test1()
+        {
+            // Arrange
+            int[] nums = { 8, 1, 2, 2, 3 };
+            var expected = new int[] { 4, 0, 1, 1, 3 };
+
+            // Act
+            var actual = solution.SmallerNumbersThanCurrent(nums);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void SmallerNumbersThanCurrent_Test2()
+        {
+            // Arrange
+            int[] nums = { 6, 5, 4, 8 };
+            var expected = new int[] { 2, 1, 0, 3 };
+
+            // Act
+            var actual = solution.SmallerNumbersThanCurrent(nums);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void SmallerNumbersThanCurrent_Test3()
+        {
+            // Arrange
+            int[] nums = { 7, 7, 7, 7 };
+            var expected = new int[] { 0, 0, 0, 0 };
+
+            // Act
+            var actual = solution.SmallerNumbersThanCurrent(nums);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

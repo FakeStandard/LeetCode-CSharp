a3c9aab66e67f11e25c54af7178c8574f6d9631a
FakeStandard <rileylin.space@gmail.com>
2021-06-15 13:20:24 +0800

    (0824) new

---
+++ 0824.Goat-latin/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace _0824.Goat_latin
+{
+    public class Solution
+    {
+        public string ToGoatLatin(string sentence)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string[] s = sentence.Split(" ");
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i].ToLower().StartsWith("a") || s[i].ToLower().StartsWith("e") || s[i].ToLower().StartsWith("i") || s[i].ToLower().StartsWith("o") || s[i].ToLower().StartsWith("u"))
+                {
+                    sb.Append(s[i]);
+                    sb.Append("ma");
+                }
+                else
+                {
+                    sb.Append(s[i].Substring(1));
+                    sb.Append(s[i][0]);
+                    sb.Append("ma");
+                }
+
+                for (int j = 0; j < i + 1; j++)
+                    sb.Append("a");
+
+                sb.Append(" ");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ UnitTest/_0824_goat_latin_Test.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0824.Goat_latin;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0824.Goat_latin.Tests
+{
+    [TestClass()]
+    public class _0824_goat_latin_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void ToGoatLatin_Test1()
+        {
+            // Arrange
+            string sentence = "I speak Goat Latin";
+            var expected = "Imaa peaksmaaa oatGmaaaa atinLmaaaaa";
+
+            // Act
+            var actual = solution.ToGoatLatin(sentence);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void ToGoatLatin_Test2()
+        {
+            // Arrange
+            string sentence = "The quick brown fox jumped over the lazy dog";
+            var expected = "heTmaa uickqmaaa rownbmaaaa oxfmaaaaa umpedjmaaaaaa overmaaaaaaa hetmaaaaaaaa azylmaaaaaaaaa ogdmaaaaaaaaaa";
+
+            // Act
+            var actual = solution.ToGoatLatin(sentence);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

e1a1531fe10fd79e03475bf42d40e385ce149a20
FakeStandard <rileylin.space@gmail.com>
2021-06-15 14:11:28 +0800

    (1380) new

---
+++ 1380.Lucky-numbers-in-a-matrix/Solution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1380.Lucky_numbers_in_a_matrix
+{
+    public class Solution
+    {
+        public IList<int> LuckyNumbers(int[][] matrix)
+        {
+            IList<int> res = new List<int>();
+            int min = int.MaxValue;
+            int i, j, k, column = 0;
+            bool flag = true;
+
+            for (i = 0; i < matrix.Length; i++)
+            {
+                flag = true;
+                min = int.MaxValue;
+
+                // Find the smallest value.
+                for (j = 0; j < matrix[0].Length; j++)
+                {
+                    if (matrix[i][j] < min)
+                    {
+                        min = matrix[i][j];
+                        column = j;
+                    }
+                }
+
+                // Judge the maximum value.
+                for (k = 0; k < matrix.Length; k++)
+                {
+                    if (matrix[k][column] > min)
+                    {
+                        flag = false;
+                        break;
+                    }
+                }
+
+                if (flag)
+                    res.Add(min);
+            }
+
+            return res;
+        }
+    }
+}
---
+++ UnitTest/_1380_Lucky_numbers_in_a_matrix_Test.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1380.Lucky_numbers_in_a_matrix;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+
+namespace _1380.Lucky_numbers_in_a_matrix.Tests
+{
+    [TestClass()]
+    public class _1380_Lucky_numbers_in_a_matrix_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void LuckyNumbers_Test1()
+        {
+            // Arrange
+            int[][] matrix = new int[][]
+            {
+                new int[]{ 3, 7, 8 },
+                new int[]{ 9, 11, 13 },
+                new int[]{ 15, 16, 17 }
+            };
+
+            IList<int> expected = new List<int> { 15 };
+
+            // Act
+            var actual = solution.LuckyNumbers(matrix);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+            //Assert.ReferenceEquals(expected, actual);
+        }
+
+        [TestMethod()]
+        public void LuckyNumbers_Test2()
+        {
+            // Arrange
+            int[][] matrix = new int[][]
+            {
+                new int[]{ 1, 10, 4, 2 },
+                new int[]{ 9, 3, 8, 7 },
+                new int[]{ 15, 16, 17, 12 }
+            };
+
+            IList<int> expected = new List<int> { 12 };
+
+            // Act
+            var actual = solution.LuckyNumbers(matrix);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+            //Assert.ReferenceEquals(expected, actual);
+        }
+
+        [TestMethod()]
+        public void LuckyNumbers_Test3()
+        {
+            // Arrange
+            int[][] matrix = new int[][]
+            {
+                new int[]{ 7, 8 },
+                new int[]{ 1, 2, }
+            };
+
+            IList<int> expected = new List<int> { 7 };
+
+            // Act
+            var actual = solution.LuckyNumbers(matrix);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+            //Assert.ReferenceEquals(expected, actual);
+        }
+    }
+}

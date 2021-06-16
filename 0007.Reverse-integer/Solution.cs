34cffce04464e2ee922baa77bb5e69566f6ee292
FakeStandard <rileylin.space@gmail.com>
2021-06-16 11:52:43 +0800

    (0007) add unit test and changed the project to lib

---
+++ 0007.Reverse-integer/Solution.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _0007.Reverse_integer
+{
+    public class Solution
+    {
+        public int Reverse(int x)
+        {
+            try
+            {
+                // 拆解成字元
+                char[] ch = x.ToString().ToCharArray();
+
+                // 反轉陣列
+                Array.Reverse(ch);
+
+                string result = new string(ch);
+
+                if (result.Contains("-"))
+                    result = "-" + (result.Replace("-", ""));
+
+                return int.Parse(result);
+            }
+            catch (Exception ex)
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ UnitTest/_0007_Reverse_integer_Test.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0007.Reverse_integer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0007.Reverse_integer.Tests
+{
+    [TestClass()]
+    public class _0007_Reverse_integer_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void Reverse_Test1()
+        {
+            // Arrange
+            int x = 123;
+            var expected = 321;
+
+            // Act
+            var actual = solution.Reverse(x);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Reverse_Test2()
+        {
+            // Arrange
+            int x = -123;
+            var expected = -321;
+
+            // Act
+            var actual = solution.Reverse(x);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Reverse_Test3()
+        {
+            // Arrange
+            int x = 120;
+            var expected = 21;
+
+            // Act
+            var actual = solution.Reverse(x);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Reverse_Test4()
+        {
+            // Arrange
+            int x = 0;
+            var expected = 0;
+
+            // Act
+            var actual = solution.Reverse(x);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

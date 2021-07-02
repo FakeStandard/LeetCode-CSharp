935195dcf6cfc9bdce6f2d7973b8d8c63aaedad7
FakeStandard <rileylin.space@gmail.com>
2021-07-02 19:33:31 +0800

    (0204) new

---
+++ 0204.Count-primes/Solution.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _0204.Count_primes
+{
+    public class Solution
+    {
+        public int CountPrimes(int n)
+        {
+            if (n < 2)
+                return 0;
+
+            bool[] primes = new bool[n];
+            primes[0] = false;
+            primes[1] = false;
+
+            int i = 0;
+
+            for (i = 2; i < n; i++)
+                primes[i] = true;
+
+            i = 2;
+            while (i < n)
+            {
+                if (primes[i])
+                    for (int j = 2; i * j < n; j++)
+                        primes[i * j] = false;
+                i++;
+            }
+
+            int count = 0;
+            i = 2;
+
+            while (i < n)
+            {
+                if (primes[i])
+                    count++;
+                i++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ UnitTest/_0204_Count_primes_Test.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0204.Count_primes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0204.Count_primes.Tests
+{
+    [TestClass()]
+    public class _0204_Count_primes_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void CountPrimes_Test1()
+        {
+            // Arrange
+            int n = 10;
+            var expected = 4;
+
+            // Act
+            var actual = solution.CountPrimes(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CountPrimes_Test2()
+        {
+            // Arrange
+            int n = 0;
+            var expected = 0;
+
+            // Act
+            var actual = solution.CountPrimes(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CountPrimes_Test3()
+        {
+            // Arrange
+            int n = 1;
+            var expected = 0;
+
+            // Act
+            var actual = solution.CountPrimes(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CountPrimes_Test4()
+        {
+            // Arrange
+            int n = 2;
+            var expected = 0;
+
+            // Act
+            var actual = solution.CountPrimes(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

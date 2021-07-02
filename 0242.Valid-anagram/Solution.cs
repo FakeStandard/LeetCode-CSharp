822ef92bf1e4c1498043d9dcb24c4e334ff90740
FakeStandard <rileylin.space@gmail.com>
2021-07-02 18:29:29 +0800

    (0242) new

---
+++ 0242.Valid-anagram/Solution.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _0242.Valid_anagram
+{
+    public class Solution
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            if (s.Length != t.Length)
+                return false;
+
+            char[] ch1 = s.ToCharArray();
+            char[] ch2 = t.ToCharArray();
+
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+
+            for (int i = 0; i < ch1.Length; i++)
+                if (ch1[i] != ch2[i])
+                    return false;
+
+            return true;
+        }
+    }
+}
---
+++ UnitTest/_0242_Valid_anagram_Test.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0242.Valid_anagram;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0242.Valid_anagram.Tests
+{
+    [TestClass()]
+    public class _0242_Valid_anagram_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void IsAnagram_Test1()
+        {
+            // Arrange
+            string s = "anagram";
+            string t = "nagaram";
+
+            // Act
+            var actual = solution.IsAnagram(s, t);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void IsAnagram_Test2()
+        {
+            // Arrange
+            string s = "rat";
+            string t = "car";
+
+            // Act
+            var actual = solution.IsAnagram(s, t);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void IsAnagram_Test3()
+        {
+            // Arrange
+            string s = "anaaram";
+            string t = "naagram";
+
+            // Act
+            var actual = solution.IsAnagram(s, t);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+    }
+}

e9352ade6257cfc3846b48b227d5fc313e3667ec
FakeStandard <rileylin.space@gmail.com>
2021-07-02 17:29:53 +0800

    (0058) add unit test and changed the project to lib

---
+++ UnitTest/_0058_Length_of_last_word_Test.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0058.Length_of_last_word;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0058.Length_of_last_word.Tests
+{
+    [TestClass()]
+    public class _0058_Length_of_last_word_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void LengthOfLastWord_Test1()
+        {
+            // Arrange
+            string s = "Hello World";
+            var expected = 5;
+
+            // Act
+            var actual = solution.LengthOfLastWord(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void LengthOfLastWord_Test2()
+        {
+            // Arrange
+            string s = " ";
+            var expected = 0;
+
+            // Act
+            var actual = solution.LengthOfLastWord(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

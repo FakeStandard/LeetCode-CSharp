d0ec1742075a929ffa351452c32ffb015c952928
FakeStandard <rileylin.space@gmail.com>
2021-07-02 19:49:39 +0800

    (0217) new

---
+++ 0217.Contains-Duplicate/Solution.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0217.Contains_Duplicate
+{
+    public class Solution
+    {
+        public bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> hash = new HashSet<int>();
+
+            foreach(var num in nums)
+            {
+                if (!hash.Add(num))
+                    return true;
+            }
+
+            return false;
+
+            // Solution 1
+             /* Runtime: 2388 ms, faster than 5.19% of C# online submissions for Contains Duplicate.
+Memory Usage: 29.5 MB, less than 79.02% of C# online submissions for Contains Duplicate.
+             */
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    for (int j = i + 1; j < nums.Length; j++)
+            //        if (nums[i] == nums[j])
+            //            return true;
+            //}
+
+            //return false;
+        }
+    }
+}
---
+++ UnitTest/_0217_Contains_duplicate_Test.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0217.Contains_Duplicate;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0217.Contains_Duplicate.Tests
+{
+    [TestClass()]
+    public class _0217_Contains_duplicate_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void ContainsDuplicate_Test1()
+        {
+            // Arrange
+            int[] nums = { 1, 2, 3, 1 };
+
+            // Act
+            var actual = solution.ContainsDuplicate(nums);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void ContainsDuplicate_Test2()
+        {
+            // Arrange
+            int[] nums = { 1, 2, 3, 4 };
+
+            // Act
+            var actual = solution.ContainsDuplicate(nums);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void ContainsDuplicate_Test3()
+        {
+            // Arrange
+            int[] nums = { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 };
+
+            // Act
+            var actual = solution.ContainsDuplicate(nums);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+    }
+}

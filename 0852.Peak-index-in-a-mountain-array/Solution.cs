3382d4331ed1c0f267dadf04172e63b1b3cfbac6
FakeStandard <rileylin.space@gmail.com>
2021-07-03 20:37:18 +0800

    (0852) new

---
+++ 0852.Peak-index-in-a-mountain-array/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _0852.Peak_index_in_a_mountain_array
+{
+    public class Solution
+    {
+        public int PeakIndexInMountainArray(int[] arr)
+        {
+            int count = 0;
+
+            for (int i = 1; i < arr.Length; i++)
+                if (arr[i] > arr[i - 1])
+                    count++;
+
+            return count;
+        }
+    }
+}
---
+++ UnitTest/_0852_Peak_index_in_a_mountain_array_Test.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0852.Peak_index_in_a_mountain_array;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0852.Peak_index_in_a_mountain_array.Tests
+{
+    [TestClass()]
+    public class _0852_Peak_index_in_a_mountain_array_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void PeakIndexInMountainArray_Test1()
+        {
+            // Arrange
+            int[] arr = { 0, 1, 0 };
+            var expected = 1;
+
+            // Act
+            var actual = solution.PeakIndexInMountainArray(arr);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void PeakIndexInMountainArray_Test2()
+        {
+            // Arrange
+            int[] arr = { 0, 2, 1, 0 };
+            var expected = 1;
+
+            // Act
+            var actual = solution.PeakIndexInMountainArray(arr);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void PeakIndexInMountainArray_Test3()
+        {
+            // Arrange
+            int[] arr = { 0, 10, 5, 2 };
+            var expected = 1;
+
+            // Act
+            var actual = solution.PeakIndexInMountainArray(arr);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void PeakIndexInMountainArray_Test4()
+        {
+            // Arrange
+            int[] arr = { 3, 4, 5, 1 };
+            var expected = 2;
+
+            // Act
+            var actual = solution.PeakIndexInMountainArray(arr);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void PeakIndexInMountainArray_Test5()
+        {
+            // Arrange
+            int[] arr = { 24, 69, 100, 99, 79, 78, 67, 36, 26, 19 };
+            var expected = 2;
+
+            // Act
+            var actual = solution.PeakIndexInMountainArray(arr);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

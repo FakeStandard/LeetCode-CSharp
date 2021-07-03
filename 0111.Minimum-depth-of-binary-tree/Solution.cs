7a9118333b914aae015d062c1f6c1bc474ff1dd5
FakeStandard <rileylin.space@gmail.com>
2021-07-03 17:51:40 +0800

    (0111) pending

---
+++ 0111.Minimum-depth-of-binary-tree/Solution.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+using System;
+
+namespace _0111.Minimum_depth_of_binary_tree
+{
+    public class Solution
+    {
+        public int MinDepth(TreeNode root)
+        {
+            return 0;
+        }
+    }
+}

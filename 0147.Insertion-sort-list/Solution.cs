2a45192ea8826011343de5c06f6877c6ec778f6a
FakeStandard <rileylin.space@gmail.com>
2021-07-03 17:55:28 +0800

    (0147) pending

---
+++ 0147.Insertion-sort-list/Solution.cs
@@ -0,0 +1,13 @@
+﻿using Common;
+using System;
+
+namespace _0147.Insertion_sort_list
+{
+    public class Solution
+    {
+        public ListNode InsertionSortList(ListNode head)
+        {
+            return null;
+        }
+    }
+}

dd1e2b3ac824c8eefa949bd4c8b1e85e259743ec
FakeStandard <rileylin.space@gmail.com>
2021-07-03 20:25:16 +0800

    (1047) new

---
+++ 1047.Remove-all-adjacent-duplicates-in-string/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace _1047.Remove_all_adjacent_duplicates_in_string
+{
+    public class Solution
+    {
+        public string RemoveDuplicates(string s)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (sb.Length > 0 && s[i] == sb[sb.Length - 1])
+                    sb.Remove(sb.Length - 1, 1);
+                else
+                    sb.Append(s[i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ UnitTest/_1047_Remove_all_adjacent_duplicates_in_string_Test.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1047.Remove_all_adjacent_duplicates_in_string;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1047.Remove_all_adjacent_duplicates_in_string.Tests
+{
+    [TestClass()]
+    public class _1047_Remove_all_adjacent_duplicates_in_string_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void RemoveDuplicates_Test1()
+        {
+            // Arrange
+            string s = "abbaca";
+            var expected = "ca";
+
+            // Act
+            var actual = solution.RemoveDuplicates(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void RemoveDuplicates_Test2()
+        {
+            // Arrange
+            string s = "azxxzy";
+            var expected = "ay";
+
+            // Act
+            var actual = solution.RemoveDuplicates(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

e01b95fbb308e41bab75ad5c98bad9aa4c6bba19
FakeStandard <rileylin.space@gmail.com>
2021-07-03 18:15:46 +0800

    (0118) add unit test and changed the project to lib

--- 0118.Pascal's-triangle/Solution.cs
+++ 0118.Pascal's-triangle/Solution.cs
@@ -3,14 +3,6 @@ using System.Collections.Generic;
 
 namespace _0118.Pascal_s_triangle
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.ReadLine();
-        }
-    }
-
     public class Solution
     {
         public IList<IList<int>> Generate(int numRows)
@@ -26,7 +18,7 @@ namespace _0118.Pascal_s_triangle
                 list.Add(1);
 
                 for (int j = 1; j < i - 1; j++)
-                    list.Add(res[i - 2][j-1] + res[i - 2][j]);
+                    list.Add(res[i - 2][j - 1] + res[i - 2][j]);
 
                 list.Add(1);
                 res.Add(list);

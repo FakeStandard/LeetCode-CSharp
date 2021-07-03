c79e48a6faf4953d466f0a249c0a75bb25b63fc5
FakeStandard <rileylin.space@gmail.com>
2021-07-03 21:15:09 +0800

    (0728) new

---
+++ 0728.Self-dividing-numbers/Solution.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0728.Self_dividing_numbers
+{
+    public class Solution
+    {
+        public IList<int> SelfDividingNumbers(int left, int right)
+        {
+            IList<int> res = new List<int>();
+
+            while (left <= right)
+            {
+                int num = left;
+
+                // 不允許包含零
+                if (!(num % 10 == 0))
+                {
+                    while (num > 0)
+                    {
+                        int d = num % 10;
+
+                        if (left % d != 0)
+                            break;
+                        num /= 10;
+                    }
+
+                    if (num == 0)
+                        res.Add(left);
+                }
+
+                left++;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ UnitTest/_0728_Self_dividing_numbers_Test.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0728.Self_dividing_numbers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+
+namespace _0728.Self_dividing_numbers.Tests
+{
+    [TestClass()]
+    public class _0728_Self_dividing_numbers_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void SelfDividingNumbers_Test1()
+        {
+            // Arrange
+            int left = 1;
+            int right = 22;
+            var expected = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22 };
+
+            // Act
+            var actual = solution.SelfDividingNumbers(left, right);
+
+            // Assert
+            //Assert.ReferenceEquals(expected, actual);
+
+            // Assert 對參考型別是驗證記憶體位置, 故透過 FluentAssertions 套件來驗證其內容值
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void SelfDividingNumbers_Test2()
+        {
+            // Arrange
+            int left = 47;
+            int right = 85;
+            var expected = new List<int> { 48, 55, 66, 77 };
+
+            // Act
+            var actual = solution.SelfDividingNumbers(left, right);
+
+            // Assert
+            //Assert.ReferenceEquals(expected, actual);
+
+            // Assert 對參考型別是驗證記憶體位置, 故透過 FluentAssertions 套件來驗證其內容值
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

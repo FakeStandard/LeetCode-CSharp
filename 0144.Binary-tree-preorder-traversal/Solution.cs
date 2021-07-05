7eeac88315e5f30db3005a3fbab4149da073123a
FakeStandard <rileylin.space@gmail.com>
2021-07-05 16:08:01 +0800

    (0144) changed the project to lib

---
+++ 0144.Binary-tree-preorder-traversal/Solution.cs
@@ -0,0 +1,30 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+
+namespace _0144.Binary_tree_preorder_traversal
+{
+    public class Solution
+    {
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            IList<int> res = new List<int>();
+
+            if (root == null) return res;
+
+            PreOrder(root, ref res);
+
+            return res;
+        }
+
+        private void PreOrder(TreeNode node, ref IList<int> res)
+        {
+            if (node != null)
+            {
+                res.Add(node.val);
+                PreOrder(node.left, ref res);
+                PreOrder(node.right, ref res);
+            }
+        }
+    }
+}

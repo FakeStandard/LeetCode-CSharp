03b2253c060b1cebae12e2c8b73d5245c640fa75
FakeStandard <rileylin.space@gmail.com>
2021-07-05 17:04:53 +0800

    (0169) add unit test and changed the project to lib

---
+++ 0169.Majority-element/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _0169.Majority_element
+{
+    public class Solution
+    {
+        public int MajorityElement(int[] nums)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            int max = int.MinValue;
+            int key = 0;
+
+            foreach (int num in nums)
+            {
+                if (dic.ContainsKey(num))
+                    dic[num]++;
+                else
+                    dic.Add(num, 1);
+
+                if (dic[num] > max)
+                {
+                    max = dic[num];
+                    key = num;
+                }
+            }
+
+            return key;
+        }
+    }
+}
---
+++ UnitTest/_0169_Majority_element_Test.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0169.Majority_element;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0169.Majority_element.Tests
+{
+    [TestClass()]
+    public class _0169_Majority_element_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void MajorityElement_Test1()
+        {
+            // Arrange
+            int[] nums = { 3, 2, 3 };
+            var expected = 3;
+
+            // Act
+            var actual = solution.MajorityElement(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MajorityElement_Test2()
+        {
+            // Arrange
+            int[] nums = { 2, 2, 1, 1, 1, 2, 2 };
+            var expected = 2;
+
+            // Act
+            var actual = solution.MajorityElement(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MajorityElement_Test3()
+        {
+            // Arrange
+            int[] nums = { 1 };
+            var expected = 1;
+
+            // Act
+            var actual = solution.MajorityElement(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

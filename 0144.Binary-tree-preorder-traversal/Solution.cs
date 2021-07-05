7d5c2a4db40618a7dc269c8dbd99a4caf3c94e3f
FakeStandard <rileylin.space@gmail.com>
2021-07-05 16:20:35 +0800

    (0144) Add Iterativeley Solution

--- 0144.Binary-tree-preorder-traversal/Solution.cs
+++ 0144.Binary-tree-preorder-traversal/Solution.cs
@@ -1,30 +1,58 @@
 ﻿using Common;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace _0144.Binary_tree_preorder_traversal
 {
     public class Solution
     {
+        // Iterativeley solution
         public IList<int> PreorderTraversal(TreeNode root)
         {
-            IList<int> res = new List<int>();
+            IList<int> result = new List<int>();
 
-            if (root == null) return res;
+            if (root == null) return result;
 
-            PreOrder(root, ref res);
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(root);
 
-            return res;
-        }
-
-        private void PreOrder(TreeNode node, ref IList<int> res)
-        {
-            if (node != null)
+            while (stack.Count > 0)
             {
-                res.Add(node.val);
-                PreOrder(node.left, ref res);
-                PreOrder(node.right, ref res);
+                TreeNode current = stack.Pop();
+
+                if (current != null)
+                {
+                    result.Add(current.val);
+                    stack.Push(current.right);
+                    stack.Push(current.left);
+                }
             }
+
+            return result;
         }
+
+
+        // Recursive solution
+        //public IList<int> PreorderTraversal(TreeNode root)
+        //{
+        //    IList<int> res = new List<int>();
+
+        //    if (root == null) return res;
+
+        //    PreOrder(root, ref res);
+
+        //    return res;
+        //}
+
+        //private void PreOrder(TreeNode node, ref IList<int> res)
+        //{
+        //    if (node != null)
+        //    {
+        //        res.Add(node.val);
+        //        PreOrder(node.left, ref res);
+        //        PreOrder(node.right, ref res);
+        //    }
+        //}
     }
 }

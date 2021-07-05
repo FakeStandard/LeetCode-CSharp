a002b573b4d03f78f0408db11b44e905d39cf709
FakeStandard <rileylin.space@gmail.com>
2021-07-05 18:56:45 +0800

    (0258) new

---
+++ 0258.Add-digits/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _0258.Add_digits
+{
+    public class Solution
+    {
+        // Solution 2
+        public int AddDigits(int num)
+        {
+            if (num < 10) return num;
+            if (num % 9 == 0) return 9;
+            return num % 9;
+        }
+
+        // Solution 1
+        //public int AddDigits(int num)
+        //{
+        //    while (num > 9)
+        //        num = Add(num);
+
+        //    return num;
+        //}
+
+        //private int Add(int num)
+        //{
+        //    int result = 0;
+
+        //    while (num > 0)
+        //    {
+        //        result += num % 10;
+        //        num /= 10;
+        //    }
+
+        //    return result;
+        //}
+    }
+}
---
+++ UnitTest/_0258_Add_digits_Test.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0258.Add_digits;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0258.Add_digits.Tests
+{
+    [TestClass()]
+    public class _0258_Add_digits_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void AddDigits_Test1()
+        {
+            // Arrange
+            int num = 38;
+            var expected = 2;
+
+            // Act
+            var actual = solution.AddDigits(num);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void AddDigits_Test2()
+        {
+            // Arrange
+            int num = 0;
+            var expected = 0;
+
+            // Act
+            var actual = solution.AddDigits(num);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

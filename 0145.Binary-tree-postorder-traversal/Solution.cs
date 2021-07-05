55de395d6c3f7343aa09befd1069e9ddbe5878e1
FakeStandard <rileylin.space@gmail.com>
2021-07-05 16:42:48 +0800

    (0145) add iteratively solution and changed the project to lib

---
+++ 0145.Binary-tree-postorder-traversal/Solution.cs
@@ -0,0 +1,61 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _0145.Binary_tree_postorder_traversal
+{
+    public class Solution
+    {
+        // Iteratively solution
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+            IList<int> result = new List<int>();
+            if (root == null) return result;
+
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(root);
+
+            while (stack.Count > 0)
+            {
+                TreeNode current = stack.Pop();
+
+                if (current != null)
+                {
+                    result.Add(current.val);
+
+                    if (current.left != null)
+                        stack.Push(current.left);
+                    if (current.right != null)
+                        stack.Push(current.right);
+                }
+            }
+
+            result.Reverse();
+
+            return result;
+        }
+
+        // Recursive solution
+        //public IList<int> PostorderTraversal(TreeNode root)
+        //{
+        //    IList<int> res = new List<int>();
+
+        //    if (root == null) return res;
+
+        //    PostOrder(root, ref res);
+
+        //    return res;
+        //}
+
+        //private void PostOrder(TreeNode node, ref IList<int> res)
+        //{
+        //    if (node != null)
+        //    {
+        //        PostOrder(node.left, ref res);
+        //        PostOrder(node.right, ref res);
+        //        res.Add(node.val);
+        //    }
+        //}
+    }
+}

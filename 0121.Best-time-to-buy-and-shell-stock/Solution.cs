2f8b1e4d32890af7f1efce7fba133fac968c1367
FakeStandard <rileylin.space@gmail.com>
2021-07-05 15:31:55 +0800

    (0121) new

---
+++ 0121.Best-time-to-buy-and-shell-stock/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace _0121.Best_time_to_buy_and_shell_stock
+{
+    public class Solution
+    {
+        public int MaxProfit(int[] prices)
+        {
+            if (prices.Length == 0) return 0;
+
+            int min = int.MaxValue;
+            int max = int.MinValue;
+
+            for(int i = 0; i < prices.Length; i++)
+            {
+                min = Math.Min(min, prices[i]);
+                max = Math.Max(max, prices[i] - min);
+            }
+
+            return max;
+        }
+    }
+}
---
+++ UnitTest/_0121_Bset_time_to_buy_and_shell_stock_Test.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0121.Best_time_to_buy_and_shell_stock;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0121.Best_time_to_buy_and_shell_stock.Tests
+{
+    [TestClass()]
+    public class _0121_Bset_time_to_buy_and_shell_stock_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void MaxProfit_Test1()
+        {
+            // Arrange
+            int[] prices = { 7, 1, 5, 3, 6, 4 };
+            var expected = 5;
+
+            // Act
+            var actual = solution.MaxProfit(prices);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MaxProfit_Test2()
+        {
+            // Arrange
+            int[] prices = { 7, 6, 4, 3, 1 };
+            var expected = 0;
+
+            // Act
+            var actual = solution.MaxProfit(prices);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MaxProfit_Test3()
+        {
+            // Arrange
+            int[] prices = { 2, 4, 1 };
+            var expected = 2;
+
+            // Act
+            var actual = solution.MaxProfit(prices);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

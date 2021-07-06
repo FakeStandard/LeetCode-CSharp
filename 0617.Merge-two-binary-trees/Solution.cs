1bc35662d1b4d0d4f9d32a1862b5e5f746477ea8
FakeStandard <rileylin.space@gmail.com>
2021-07-06 14:14:25 +0800

    (0617) changed the project to lib

---
+++ 0617.Merge-two-binary-trees/Solution.cs
@@ -0,0 +1,24 @@
+﻿using Common;
+using System;
+
+namespace _0617.Merge_two_binary_trees
+{
+    public class Solution
+    {
+        public TreeNode MergeTrees(TreeNode root1, TreeNode root2)
+        {
+            if (root1 == null && root2 == null)
+                return null;
+            if (root1 == null)
+                return root2;
+            if (root2 == null)
+                return root1;
+
+            root1.val += root2.val;
+            root1.left = MergeTrees(root1.left, root2.left);
+            root1.right = MergeTrees(root1.right, root2.right);
+
+            return root1;
+        }
+    }
+}

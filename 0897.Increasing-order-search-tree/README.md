fc39686a10e87a78ace614af13b77ca00e0f56c0
FakeStandard <rileylin.space@gmail.com>
2021-07-06 14:39:11 +0800

    (0897) changed the project to lib

--- 0897.Increasing-order-search-tree/README.md
+++ 0897.Increasing-order-search-tree/README.md
@@ -1,8 +1,6 @@
 # 0897. Increasing Order Search Tree
 Given the `root` of a binary search tree, rearrange the tree in **in-order** so that the leftmost node in the tree is now the root of the tree, and every node has no left child and only one right child.
 
- 
-
 ### Example 1:
 ```
 Input: root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
---
+++ 0897.Increasing-order-search-tree/Solution.cs
@@ -0,0 +1,37 @@
+﻿using Common;
+using System;
+
+namespace _0897.Increasing_order_search_tree
+{
+    public class Solution
+    {
+        private TreeNode res;
+        private TreeNode cur;
+        public TreeNode IncreasingBST(TreeNode root)
+        {
+            Find(root);
+
+            return res;
+        }
+
+        private void Find(TreeNode node)
+        {
+            if (node.left != null)
+                Find(node.left);
+
+            if (cur == null)
+            {
+                cur = new TreeNode(node.val);
+                res = cur;
+            }
+            else
+            {
+                cur.right = new TreeNode(node.val);
+                cur = cur.right;
+            }
+
+            if (node.right != null)
+                Find(node.right);
+        }
+    }
+}

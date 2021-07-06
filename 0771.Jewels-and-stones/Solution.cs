e18546134a44d365040df33cdd7b62d8d84d5594
FakeStandard <rileylin.space@gmail.com>
2021-07-06 14:35:58 +0800

    (0771) add unit test and changed the project to lib

---
+++ 0771.Jewels-and-stones/Solution.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _0771.Jewels_and_stones
+{
+    public class Solution
+    {
+        public int NumJewelsInStones(string jewels, string stones)
+        {
+            int count = 0;
+
+            foreach (var s in stones)
+                if (jewels.Contains(s))
+                    count++;
+
+            return count;
+        }
+    }
+}
---
+++ UnitTest/_0771_Jewels_and_stones_Test.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0771.Jewels_and_stones;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0771.Jewels_and_stones.Tests
+{
+    [TestClass()]
+    public class _0771_Jewels_and_stones_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void NumJewelsInStones_Test1()
+        {
+            // Arrange
+            string jewels = "aA";
+            string stones = "aAAbbbb";
+            var expected = 3;
+
+            // Act
+            var actual = solution.NumJewelsInStones(jewels, stones);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void NumJewelsInStones_Test2()
+        {
+            // Arrange
+            string jewels = "z";
+            string stones = "ZZ";
+            var expected = 0;
+
+            // Act
+            var actual = solution.NumJewelsInStones(jewels, stones);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void NumJewelsInStones_Test3()
+        {
+            // Arrange
+            string jewels = "aA";
+            string stones = "aAAbbbbaabAA";
+            var expected = 7;
+
+            // Act
+            var actual = solution.NumJewelsInStones(jewels, stones);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

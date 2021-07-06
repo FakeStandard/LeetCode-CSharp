fd370d4435ce37b8f6ed3a61ac7101f898388fe2
FakeStandard <rileylin.space@gmail.com>
2021-07-06 14:28:37 +0800

    (0682) add unit test and changed the project to lib

---
+++ 0682.Baseball-game/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _0682.Baseball_game
+{
+    public class Solution
+    {
+        public int CalPoints(string[] ops)
+        {
+            if (ops.Length == 0) return 0;
+
+            Stack<int> stack = new Stack<int>();
+
+            foreach (var s in ops)
+            {
+                switch (s)
+                {
+                    case "D":
+                        stack.Push(stack.Peek() * 2);
+                        break;
+                    case "C":
+                        stack.Pop();
+                        break;
+                    case "+":
+                        int x = stack.Pop();
+                        int y = stack.Pop();
+                        stack.Push(y);
+                        stack.Push(x);
+                        stack.Push(x + y);
+                        break;
+                    default:
+                        stack.Push(int.Parse(s));
+                        break;
+                }
+            }
+
+            int sum = 0;
+
+            while (stack.Count() > 0)
+                sum += stack.Pop();
+
+            return sum;
+
+            // Or directly use Sum() of LINQ's extension method.
+            // return stack.Sum();
+        }
+    }
+}
---
+++ UnitTest/_0682_Baseball_game_Test.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0682.Baseball_game;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0682.Baseball_game.Tests
+{
+    [TestClass()]
+    public class _0682_Baseball_game_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void CalPoints_Test1()
+        {
+            // Arrange
+            string[] ops = new string[] { "5", "2", "C", "D", "+" };
+            var expected = 30;
+
+            // Act
+            var actual = solution.CalPoints(ops);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CalPoints_Test2()
+        {
+            // Arrange
+            string[] ops = new string[] { "5", "-2", "4", "C", "D", "9", "+", "+" };
+            var expected = 27;
+
+            // Act
+            var actual = solution.CalPoints(ops);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CalPoints_Test3()
+        {
+            // Arrange
+            string[] ops = new string[] { "1" };
+            var expected = 1;
+
+            // Act
+            var actual = solution.CalPoints(ops);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

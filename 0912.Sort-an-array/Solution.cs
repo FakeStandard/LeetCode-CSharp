dd4ad3e5ff200c50061bf7842721d9a9f1567068
FakeStandard <rileylin.space@gmail.com>
2021-07-06 15:18:38 +0800

    (0912) add unit test and changed the project to lib

---
+++ 0912.Sort-an-array/Solution.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace _0912.Sort_an_array
+{
+    public class Solution
+    {
+        public int[] SortArray(int[] nums)
+        {
+            //Array.Sort(nums);
+            //return nums;
+
+            QuickSort(ref nums, 0, nums.Length - 1);
+
+            return nums;
+        }
+
+        private void QuickSort(ref int[] data, int left, int right)
+        {
+            if (left >= right)
+                return;
+
+            int i = Partition(data, left, right);
+            QuickSort(ref data, left, i - 1);
+            QuickSort(ref data, i + 1, right);
+        }
+
+        private int Partition(int[] data, int left, int right)
+        {
+            int i = left;
+            int pivot = data[right];
+
+            for(int j = left; j < right; j++)
+            {
+                if (data[j] < pivot)
+                {
+                    Swap(ref data, i, j);
+                    i++;
+                }
+            }
+
+            Swap(ref data, i, right);
+
+            return i;
+        }
+
+        private void Swap(ref int[] nums, int i, int j)
+        {
+            int tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+        }
+
+        //public int[] QuickSort(int[] data, int left, int right)
+        //{
+        //    if (left >= right)
+        //        return data;
+
+        //    int pivot = data[left];
+        //    int i = left;
+        //    int j = right;
+
+        //    int tmp;
+
+        //    while (i != j)
+        //    {
+        //        while (data[j] >= pivot && i < j)
+        //            j--;
+
+        //        while (data[i] <= pivot && i < j)
+        //            i++;
+
+        //        if (i < j)
+        //        {
+        //            tmp = data[j];
+        //            data[j] = data[i];
+        //            data[i] = tmp;
+        //        }
+        //    }
+
+        //    data[left] = data[i];
+        //    data[i] = pivot;
+
+        //    QuickSort(data, left, i - 1);
+        //    QuickSort(data, i + 1, right);
+
+        //    return data;
+        //}
+    }
+}
---
+++ UnitTest/_0912_Sort_an_array_Test.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0912.Sort_an_array;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentAssertions;
+
+namespace _0912.Sort_an_array.Tests
+{
+    [TestClass()]
+    public class _0912_Sort_an_array_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void SortArray_Test1()
+        {
+            // Arrange
+            int[] nums = { 5, 2, 3, 1 };
+            var expected = new int[] { 1, 2, 3, 5 };
+
+            // Act
+            var actual = solution.SortArray(nums);
+
+            // Assert
+            //Assert.ReferenceEquals(expected, actual);
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void SortArray_Test2()
+        {
+            // Arrange
+            int[] nums = { 5, 1, 1, 2, 0, 0 };
+            var expected = new int[] { 0, 0, 1, 1, 2, 5 };
+
+            // Act
+            var actual = solution.SortArray(nums);
+
+            // Assert
+            //Assert.ReferenceEquals(expected, actual);
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

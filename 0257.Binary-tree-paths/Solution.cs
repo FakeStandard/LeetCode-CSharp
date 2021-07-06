6dd50976801808799605dd15d1b910652db3db8d
FakeStandard <rileylin.space@gmail.com>
2021-07-06 14:00:31 +0800

    (0257) changed the project to lib

---
+++ 0257.Binary-tree-paths/Solution.cs
@@ -0,0 +1,36 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+
+namespace _0257.Binary_tree_paths
+{
+    public class Solution
+    {
+        public IList<string> BinaryTreePaths(TreeNode root)
+        {
+            List<string> res = new List<string>();
+
+            if (root == null) return null;
+
+            Path(root, string.Empty, res);
+
+            return res;
+        }
+
+        private void Path(TreeNode node, string currentPath, List<string> res)
+        {
+            currentPath += node.val + "->";
+
+            if (node.left == null && node.right == null)
+            {
+                res.Add(currentPath.Substring(0, currentPath.Length - 2));
+                return;
+            }
+
+            if (node.left != null)
+                Path(node.left, currentPath, res);
+            if (node.right != null)
+                Path(node.right, currentPath, res);
+        }
+    }
+}

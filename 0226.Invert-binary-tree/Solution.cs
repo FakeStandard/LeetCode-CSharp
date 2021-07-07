cd2dc16305e82f7470342d0e4818a921c279cd43
FakeStandard <rileylin.space@gmail.com>
2021-07-07 15:58:06 +0800

    (0226) new

---
+++ 0226.Invert-binary-tree/Solution.cs
@@ -0,0 +1,23 @@
+﻿using Common;
+using System;
+
+namespace _0226.Invert_binary_tree
+{
+    public class Solution
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            // DFS
+            if (root == null) return root;
+
+            TreeNode invert = root.left;
+            root.left = root.right;
+            root.right = invert;
+
+            InvertTree(root.left);
+            InvertTree(root.right);
+
+            return root;
+        }
+    }
+}

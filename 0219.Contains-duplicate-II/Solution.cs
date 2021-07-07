fb0d11c04638885d98855bece9c07efc7ce4adb1
FakeStandard <rileylin.space@gmail.com>
2021-07-07 15:23:26 +0800

    (0219) new

---
+++ 0219.Contains-duplicate-II/Solution.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _0219.Contains_duplicate_II
+{
+    public class Solution
+    {
+        public bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i]) && i - dic[nums[i]] <= k)
+                    return true;
+                dic[nums[i]] = i;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ UnitTest/_0219_Contains_duplicate_II_Test.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0219.Contains_duplicate_II;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0219.Contains_duplicate_II.Tests
+{
+    [TestClass()]
+    public class _0219_Contains_duplicate_II_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void ContainsNearbyDuplicate_Test1()
+        {
+            // Arrange
+            int[] nums = { 1, 2, 3, 1 };
+            int k = 3;
+
+            // Act
+            var actual = solution.ContainsNearbyDuplicate(nums, k);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void ContainsNearbyDuplicate_Test2()
+        {
+            // Arrange
+            int[] nums = { 1, 0, 1, 1 };
+            int k = 1;
+
+            // Act
+            var actual = solution.ContainsNearbyDuplicate(nums, k);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void ContainsNearbyDuplicate_Test3()
+        {
+            // Arrange
+            int[] nums = { 1, 2, 3, 1, 2, 3 };
+            int k = 2;
+
+            // Act
+            var actual = solution.ContainsNearbyDuplicate(nums, k);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+    }
+}

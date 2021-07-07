3c149900d878a1a1e5e9b4854d4de63afb9b003a
FakeStandard <rileylin.space@gmail.com>
2021-07-07 17:07:36 +0800

    (0283) new

---
+++ 0283.Move-zeroes/Solution.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _0283.Move_zeroes
+{
+    public class Solution
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int curr = 0;
+            int next = 1;
+
+            while (next <= nums.Length - 1)
+            {
+                if (nums[curr] == 0)
+                {
+                    if (nums[next] == 0)
+                    {
+                        next++;
+                        continue;
+                    }
+                    else
+                    {
+                        nums[curr] = nums[next];
+                        nums[next] = 0;
+                    }
+                }
+
+                curr++;
+                next++;
+            }
+        }
+    }
+}

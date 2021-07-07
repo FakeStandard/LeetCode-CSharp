65f9c6404c9d5917c80548fc9b106907abd63614
FakeStandard <rileylin.space@gmail.com>
2021-07-07 16:45:29 +0800

    (0237) new

---
+++ 0237.Delete-node-in-a-linked-list/Solution.cs
@@ -0,0 +1,14 @@
+﻿using Common;
+using System;
+
+namespace _0237.Delete_node_in_a_linked_list
+{
+    public class Solution
+    {
+        public void DeleteNode(ListNode node)
+        {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}

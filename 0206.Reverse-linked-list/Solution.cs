cb1639eb52c923ace66a22ec40eb7a8358f9eec7
FakeStandard <rileylin.space@gmail.com>
2021-07-07 14:56:52 +0800

    (0206) new, including three solution of recursion, iteration and stacking.

---
+++ 0206.Reverse-linked-list/Solution.cs
@@ -0,0 +1,111 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+
+namespace _0206.Reverse_linked_list
+{
+    public class Solution
+    {
+        /// <summary>
+        /// Recursively Solution
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public ListNode ReverseList(ListNode head)
+        {
+            if (head == null) return head;
+
+            ListNode node = ReverseList(head.next);
+            head.next.next = head;
+            head.next = null;
+
+            return node;
+        }
+
+        /// <summary>
+        /// Iteratively Solution
+        /// faster than the previous solution.
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        //public ListNode ReverseList(ListNode head)
+        //{
+        //    if (head == null) return head;
+
+        //    ListNode prev = null;
+
+        //    // 1->2->3->4->5
+        //    while (head != null)
+        //    {
+        //        ListNode next = head.next;
+        //        head.next = prev;
+        //        prev = head;
+        //        head = next;
+
+        //        /* first time
+        //         * next = 2->3->4->5
+        //         * head.next = null
+        //         * prev = 1
+        //         * head = 2->3->4->5
+        //         */
+        //        /* second time
+        //         * next = 3->4->5
+        //         * head.next = 1
+        //         * prev = 2->1
+        //         * head = 3->4->5
+        //         */
+        //        /* next time
+        //         * next = 4->5
+        //         * head.next = 2->1
+        //         * prev = 3->2->1
+        //         * head = 4->5
+        //         */
+        //        /* next time
+        //         * next = 5
+        //         * head.next = 3->2->1
+        //         * prev = 4->3->2->1
+        //         * head = 5
+        //         */
+        //        /* the last time
+        //         * next = null
+        //         * head.next = 4->3->2->1
+        //         * prev = 5->4->3->2->1
+        //         * head = null
+        //         */
+        //    }
+
+        //    return prev;
+        //}
+
+        /// <summary>
+        /// Stack Solution
+        /// the fastest solution, although not very smart.
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        //public ListNode ReverseList(ListNode head)
+        //{
+        //    if (head == null) return head;
+
+        //    ListNode res = new ListNode();
+        //    ListNode cur = res;
+
+        //    Stack<int> stack = new Stack<int>();
+
+        //    while (head != null)
+        //    {
+        //        stack.Push(head.val);
+        //        head = head.next;
+        //    }
+
+        //    while (stack.Count > 0)
+        //    {
+        //        ListNode node = new ListNode(stack.Pop());
+        //        cur.next = node;
+        //        cur = cur.next;
+        //    }
+
+        //    return res.next;
+        //}
+    }
+}
---
+++ UnitTest/_0206_Reverse_linked_list_Test.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0206.Reverse_linked_list;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Common;
+using FluentAssertions;
+
+namespace _0206.Reverse_linked_list.Tests
+{
+    [TestClass()]
+    public class _0206_Reverse_linked_list_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void ReverseList_Test1()
+        {
+            // Arrange
+            ListNode head = AddNode(new int[] { 1, 2, 3, 4, 5 });
+            var expected = AddNode(new int[] { 5, 4, 3, 2, 1 });
+
+            // Act
+            var actual = solution.ReverseList(head);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void ReverseList_Test2()
+        {
+            // Arrange
+            ListNode head = AddNode(new int[] { 1, 2 });
+            var expected = AddNode(new int[] { 2, 1 });
+
+            // Act
+            var actual = solution.ReverseList(head);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void ReverseList_Test3()
+        {
+            // Arrange
+            ListNode head = AddNode(new int[] { });
+            var expected = AddNode(new int[] { });
+
+            // Act
+            var actual = solution.ReverseList(head);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        private ListNode AddNode(int[] arr)
+        {
+            if (arr.Length <= 0)
+                return new ListNode();
+
+            ListNode res = new ListNode(arr[0]);
+            ListNode current = res;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                ListNode newNode = new ListNode(arr[i]);
+
+                current.next = newNode;
+                current = current.next;
+            }
+
+            return res;
+        }
+    }
+}

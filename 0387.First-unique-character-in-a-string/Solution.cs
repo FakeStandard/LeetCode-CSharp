4cea9ff2c5765fc8f474e61eab97071e40a1921a
FakeStandard <rileylin.space@gmail.com>
2021-07-08 18:24:50 +0800

    (0387) new

---
+++ 0387.First-unique-character-in-a-string/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _0387.First_unique_character_in_a_string
+{
+    public class Solution
+    {
+        public int FirstUniqChar(string s)
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+
+            foreach (char c in s)
+            {
+                if (dic.ContainsKey(c))
+                    dic[c] += 1;
+                else
+                    dic.Add(c, 1);
+            }
+
+            for(int i = 0; i < s.Length; i++)
+                if (dic[s[i]] == 1)
+                    return i;
+
+            return -1;
+
+            //int res = s.IndexOf(dic.FirstOrDefault(x => x.Value == 1).Key);
+
+            //return res;
+        }
+    }
+}
---
+++ UnitTest/_0387_First_unique_character_in_a_string_Test.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0387.First_unique_character_in_a_string;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0387.First_unique_character_in_a_string.Tests
+{
+    [TestClass()]
+    public class _0387_First_unique_character_in_a_string_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void FirstUniqChar_Test1()
+        {
+            // Arrange
+            string s = "leetcode";
+            var expected = 0;
+
+            // Act
+            var actual = solution.FirstUniqChar(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FirstUniqChar_Test2()
+        {
+            // Arrange
+            string s = "loveleetcode";
+            var expected = 2;
+
+            // Act
+            var actual = solution.FirstUniqChar(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FirstUniqChar_Test3()
+        {
+            // Arrange
+            string s = "aabb";
+            var expected = -1;
+
+            // Act
+            var actual = solution.FirstUniqChar(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

c496aed1b0d46411927ddbfa65f1b25087a5ec32
FakeStandard <rileylin.space@gmail.com>
2021-07-08 14:25:14 +0800

    (1220) new

---
+++ 1220.Count-vowels-permutation/Solution.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace _1220.Count_vowels_permutation
+{
+    public class Solution
+    {
+        public int CountVowelPermutation(int n)
+        {
+            if (n == 0) return 0;
+            if (n == 1) return 5;
+
+            int[] curr = new int[5];
+            int[] prev = new int[5], temp;
+
+            int i;
+            int MOD = 1000000007;
+
+            for (i = 0; i < 5; i++)
+                prev[i] = 1;
+
+            for (i = 2; i <= n; i++)
+            {
+                // process a.
+                curr[0] = (((prev[1] + prev[2]) % MOD) + prev[4]) % MOD;
+
+                // process e.
+                curr[1] = (prev[0] + prev[2]) % MOD;
+
+                // process i.
+                curr[2] = (prev[1] + prev[3]) % MOD;
+
+                // process o.
+                curr[3] = prev[2] % MOD;
+
+                // process u.
+                curr[4] = (prev[2] + prev[3]) % MOD;
+
+                temp = prev;
+                prev = curr;
+                curr = temp;
+            }
+
+            int sum = 0;
+
+            for (i = 0; i < prev.Length; i++)
+                sum = (sum + prev[i]) % MOD;
+
+            return sum;
+
+            //if (n == 0) return 0;
+            //if (n == 1) return 5;
+
+            //int a = 1;
+            //int e = 1;
+            //int i = 1;
+            //int o = 1;
+            //int u = 1;
+
+            //int a_new = 0;
+            //int e_new = 0;
+            //int i_new = 0;
+            //int o_new = 0;
+            //int u_new = 0;
+
+            //// a->e
+            //// e->a.i
+            //// i->a.e.o.u
+            //// o->i.u
+            //// u->a
+
+            //for (int k = 2; k <= n; k++)
+            //{
+            //    // process a.
+            //    e_new = a;
+
+            //    // process e.
+            //    a_new = e;
+            //    i_new = e;
+
+            //    // process i.
+            //    a_new += i;
+            //    e_new += i;
+            //    o_new += i;
+            //    u_new += i;
+
+            //    // process o.
+            //    i_new += o;
+            //    u_new += o;
+
+            //    // process u.
+            //    a_new += u;
+
+            //    a = a_new;
+            //    e = e_new;
+            //    i = i_new;
+            //    o = o_new;
+            //    u = u_new;
+
+            //    a_new = 0;
+            //    e_new = 0;
+            //    i_new = 0;
+            //    o_new = 0;
+            //    u_new = 0;
+            //}
+
+            //return a + e + i + o + u;
+        }
+    }
+}
---
+++ UnitTest/_1220_Count_vowels_permutation_Test.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _1220.Count_vowels_permutation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1220.Count_vowels_permutation.Tests
+{
+    [TestClass()]
+    public class _1220_Count_vowels_permutation_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void CountVowelPermutation_Test1()
+        {
+            // Arrange
+            int n = 1;
+            var expected = 5;
+
+            // Act
+            var actual = solution.CountVowelPermutation(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CountVowelPermutation_Test2()
+        {
+            // Arrange
+            int n = 2;
+            var expected = 10;
+
+            // Act
+            var actual = solution.CountVowelPermutation(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CountVowelPermutation_Test3()
+        {
+            // Arrange
+            int n = 3;
+            var expected = 19;
+
+            // Act
+            var actual = solution.CountVowelPermutation(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CountVowelPermutation_Test4()
+        {
+            // Arrange
+            int n = 4;
+            var expected = 35;
+
+            // Act
+            var actual = solution.CountVowelPermutation(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void CountVowelPermutation_Test5()
+        {
+            // Arrange
+            int n = 5;
+            var expected = 68;
+
+            // Act
+            var actual = solution.CountVowelPermutation(n);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

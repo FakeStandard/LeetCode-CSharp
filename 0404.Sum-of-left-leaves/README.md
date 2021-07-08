5821d015412505befb2ef38d9ccf9695f4fccc51
FakeStandard <rileylin.space@gmail.com>
2021-07-08 17:18:24 +0800

    (0404) new

---
+++ 0404.Sum-of-left-leaves/README.md
@@ -0,0 +1,15 @@
+# 0404. Sum of Left Leaves
+Given the `root` of a binary tree, return the sum of all left leaves.
+
+### Example 1:
+```
+Input: root = [3,9,20,null,null,15,7]
+Output: 24
+Explanation: There are two left leaves in the binary tree, with values 9 and 15 respectively.
+```
+
+### Example 2:
+```
+Input: root = [1]
+Output: 0
+```
---
+++ 0404.Sum-of-left-leaves/Solution.cs
@@ -0,0 +1,27 @@
+﻿using Common;
+using System;
+
+namespace _0404.Sum_of_left_leaves
+{
+    public class Solution
+    {
+        private int sum = 0;
+        public int SumOfLeftLeaves(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            if (root.left != null)
+            {
+                if (root.left.left == null && root.left.right == null)
+                    sum += root.left.val;
+                else
+                    SumOfLeftLeaves(root.left);
+            }
+
+            if (root.right != null)
+                SumOfLeftLeaves(root.right);
+
+            return sum;
+        }
+    }
+}

64d4e273f7bbd9f568f292e99d4eecf9c677cd2e
FakeStandard <rileylin.space@gmail.com>
2021-07-08 16:53:27 +0800

    (0326) new

---
+++ UnitTest/_0326_Power_of_three_Test.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0326.Power_of_three;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0326.Power_of_three.Tests
+{
+    [TestClass()]
+    public class _0326_Power_of_three_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void IsPowerOfThree_Test1()
+        {
+            // Arrange
+            int n = 27;
+
+            // Act
+            var actual = solution.IsPowerOfThree(n);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void IsPowerOfThree_Test2()
+        {
+            // Arrange
+            int n = 0;
+
+            // Act
+            var actual = solution.IsPowerOfThree(n);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void IsPowerOfThree_Test3()
+        {
+            // Arrange
+            int n = 9;
+
+            // Act
+            var actual = solution.IsPowerOfThree(n);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void IsPowerOfThree_Test4()
+        {
+            // Arrange
+            int n = 45;
+
+            // Act
+            var actual = solution.IsPowerOfThree(n);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+    }
+}

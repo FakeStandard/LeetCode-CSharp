bc36c2c5ee3a2e228a5ab162790e6500f880109b
FakeStandard <rileylin.space@gmail.com>
2021-07-08 17:41:06 +0800

    (0392) new

---
+++ 0392.Is-subsequence/Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _0392.Is_subsequence
+{
+    public class Solution
+    {
+        public bool IsSubsequence(string s, string t)
+        {
+            if (s.Length == 0) return true;
+
+            int i=0, j=0;
+
+            while (i < s.Length && j < t.Length)
+            {
+                if (s[i] == t[j])
+                    i++;
+                j++;
+            }
+
+            return i == s.Length;
+        }
+    }
+}
---
+++ UnitTest/_0392_Is_subsequence_Test.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0392.Is_subsequence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0392.Is_subsequence.Tests
+{
+    [TestClass()]
+    public class _0392_Is_subsequence_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void IsSubsequence_Test1()
+        {
+            // Arrange
+            string s = "abc";
+            string t = "ahbgdc";
+
+            // Act
+            var actual = solution.IsSubsequence(s, t);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void IsSubsequence_Test2()
+        {
+            // Arrange
+            string s = "axc";
+            string t = "ahbgdc";
+
+            // Act
+            var actual = solution.IsSubsequence(s, t);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void IsSubsequence_Test3()
+        {
+            // Arrange
+            string s = "";
+            string t = "ahbgdc";
+
+            // Act
+            var actual = solution.IsSubsequence(s, t);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void IsSubsequence_Test4()
+        {
+            // Arrange
+            string s = "aaaaaa";
+            string t = "bbaaaa";
+
+            // Act
+            var actual = solution.IsSubsequence(s, t);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+    }
+}

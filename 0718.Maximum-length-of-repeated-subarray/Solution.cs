af281fd2aa5f3d37ad3eb07a0f6992389af0eff0
FakeStandard <rileylin.space@gmail.com>
2021-07-08 16:18:08 +0800

    (0718) new

---
+++ 0718.Maximum-length-of-repeated-subarray/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _0718.Maximum_length_of_repeated_subarray
+{
+    public class Solution
+    {
+        public int FindLength(int[] nums1, int[] nums2)
+        {
+            int max = 0;
+            int[,] matrix = new int[nums1.Length + 1, nums2.Length + 1];
+
+            for (int i = 0; i < nums1.Length; i++)
+            {
+                for (int j = 0; j < nums2.Length; j++)
+                {
+                    if (nums1[i] == nums2[j])
+                    {
+                        matrix[i + 1, j + 1] = matrix[i, j] + 1;
+
+                        if (max < matrix[i + 1, j + 1])
+                            max = matrix[i + 1, j + 1];
+                    }
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ UnitTest/_0718_Maximum_length_of_repeated_subarray_Test.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _0718.Maximum_length_of_repeated_subarray;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0718.Maximum_length_of_repeated_subarray.Tests
+{
+    [TestClass()]
+    public class _0718_Maximum_length_of_repeated_subarray_Test
+    {
+        Solution solution = new Solution();
+
+        [TestMethod()]
+        public void FindLength_Test1()
+        {
+            // Arrange
+            int[] nums1 = { 1, 2, 3, 2, 1 };
+            int[] nums2 = { 3, 2, 1, 4, 7 };
+            var expected = 3;
+
+            // Act
+            var actual = solution.FindLength(nums1, nums2);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FindLength_Test2()
+        {
+            // Arrange
+            int[] nums1 = { 0, 0, 0, 0, 0 };
+            int[] nums2 = { 0, 0, 0, 0, 0 };
+            var expected = 5;
+
+            // Act
+            var actual = solution.FindLength(nums1, nums2);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

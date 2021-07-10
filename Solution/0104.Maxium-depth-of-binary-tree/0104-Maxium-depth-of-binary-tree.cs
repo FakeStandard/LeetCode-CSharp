78a16cf80642fda07c35e9b9916eb42be6a4123c
FakeStandard <rileylin.space@gmail.com>
2021-07-10 16:27:07 +0800

    (0104) merge into Solution lib

---
+++ Solution/0104.Maxium-depth-of-binary-tree/0104-Maxium-depth-of-binary-tree.cs
@@ -0,0 +1,20 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0104.Maxium_depth_of_binary_tree
+{
+    public class _0104_Maxium_depth_of_binary_tree
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root == null) return 0;
+
+            int left = MaxDepth(root.left);
+            int right = MaxDepth(root.right);
+
+            return left > right ? left + 1 : right + 1;
+        }
+    }
+}

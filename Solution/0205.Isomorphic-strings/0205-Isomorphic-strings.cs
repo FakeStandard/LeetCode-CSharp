4e3efc600b0af8d759a0f9ce77495807bcbbe2a4
FakeStandard <rileylin.space@gmail.com>
2021-07-12 17:12:48 +0800

    (0205) new

---
+++ Solution/0205.Isomorphic-strings/0205-Isomorphic-strings.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0205.Isomorphic_strings
+{
+    public class _0205_Isomorphic_strings
+    {
+        public bool IsIsomorphic(string s, string t)
+        {
+            if (s.Length != t.Length) return false;
+
+            Dictionary<char, char> dic = new Dictionary<char, char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dic.ContainsKey(s[i]))
+                {
+                    if (dic[s[i]] != t[i])
+                        return false;
+                }
+                else
+                {
+                    if (dic.ContainsValue(t[i]))
+                        return false;
+                    else
+                        dic.Add(s[i], t[i]);
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ UnitTest/_0205_Isomorphic_strings_Test.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0205.Isomorphic_strings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0205.Isomorphic_strings.Tests
+{
+    [TestClass()]
+    public class _0205_Isomorphic_strings_Test
+    {
+        _0205_Isomorphic_strings solution = new _0205_Isomorphic_strings();
+
+        [TestMethod()]
+        public void IsIsomorphic_Test1()
+        {
+            // Arrange
+            string s = "egg";
+            string t = "add";
+
+            // Act
+            var actual = solution.IsIsomorphic(s, t);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void IsIsomorphic_Test2()
+        {
+            // Arrange
+            string s = "foo";
+            string t = "bar";
+
+            // Act
+            var actual = solution.IsIsomorphic(s, t);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void IsIsomorphic_Test3()
+        {
+            // Arrange
+            string s = "paper";
+            string t = "title";
+
+            // Act
+            var actual = solution.IsIsomorphic(s, t);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+    }
+}

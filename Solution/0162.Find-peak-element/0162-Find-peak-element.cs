4dbe3194b3ec22bb225228c10026e128c4ed335e
FakeStandard <rileylin.space@gmail.com>
2021-07-13 17:18:06 +0800

    (0162) new

---
+++ Solution/0162.Find-peak-element/0162-Find-peak-element.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0162.Find_peak_element
+{
+    public class _0162_Find_peak_element
+    {
+        public int FindPeakElement(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+                if ((i == 0 || nums[i] > nums[i - 1]) && (i == nums.Length - 1 || nums[i] > nums[i + 1]))
+                    return i;
+
+            return 0;
+        }
+    }
+}
---
+++ UnitTest/_0162_Find_peak_element_Test.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0162.Find_peak_element;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0162.Find_peak_element.Tests
+{
+    [TestClass()]
+    public class _0162_Find_peak_element_Test
+    {
+        _0162_Find_peak_element solution = new _0162_Find_peak_element();
+
+        [TestMethod()]
+        public void FindPeakElement_Test1()
+        {
+            // Arrange
+            int[] nums = { 1, 2, 3, 1 };
+            int expected = 2;
+
+            // Act
+            var actual = solution.FindPeakElement(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FindPeakElement_Test2()
+        {
+            // Arrange
+            int[] nums = { 1, 2, 1, 3, 5, 6, 4 };
+            int expected = 1;
+            // or other return
+            //int expected = 2;
+
+            // Act
+            var actual = solution.FindPeakElement(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FindPeakElement_Test3()
+        {
+            // Arrange
+            int[] nums = { 1, 2, 3, 4 };
+            int expected = 3;
+            // or other return
+            //int expected = 2;
+
+            // Act
+            var actual = solution.FindPeakElement(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

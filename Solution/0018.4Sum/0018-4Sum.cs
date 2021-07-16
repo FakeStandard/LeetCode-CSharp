1247182c1232b9e87c216c48e4d59dcd6700f038
FakeStandard <rileylin.space@gmail.com>
2021-07-16 18:39:11 +0800

    (0018) new

---
+++ Solution/0018.4Sum/0018-4Sum.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0018._4Sum
+{
+    public class _0018_4Sum
+    {
+        public IList<IList<int>> FourSum(int[] nums, int target)
+        {
+            if (nums.Length == 0) return null;
+
+            Array.Sort(nums);
+
+            IList<IList<int>> result = new List<IList<int>>();
+            HashSet<(int, int, int, int)> hash = new HashSet<(int, int, int, int)>();
+            int left = 0, right = 0, sum = 0;
+
+            for (int i = 0; i < nums.Length - 3; i++)
+            {
+                for (int j = i + 1; j < nums.Length - 2; j++)
+                {
+                    left = j + 1;
+                    right = nums.Length - 1;
+
+                    while (left < right)
+                    {
+                        sum = nums[i] + nums[j] + nums[left] + nums[right];
+
+                        if (sum == target)
+                        {
+                            if (!hash.Contains((nums[i], nums[j], nums[left], nums[right])))
+                            {
+                                result.Add(new List<int>() { nums[i], nums[j], nums[left], nums[right] });
+                                hash.Add((nums[i], nums[j], nums[left], nums[right]));
+                            }
+                            left++;
+                            right--;
+                        }
+                        else if (sum > target)
+                            right--;
+                        else if (sum < target)
+                            left++;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ UnitTest/0018.4Sum/_0018_4Sum_Test.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0018._4Sum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0018._4Sum.Tests
+{
+    [TestClass()]
+    public class _0018_4Sum_Test
+    {
+        _0018_4Sum solution = new _0018_4Sum();
+
+        [TestMethod()]
+        public void FourSum_Test1()
+        {
+            // Arrange
+            int[] nums = { 1, 0, -1, 0, -2, 2 };
+            int target = 0;
+            IList<IList<int>> expected = new List<IList<int>>()
+            {
+                new List<int>(){ -2, -1, 1, 2 },
+                new List<int>(){ -2, 0, 0, 2 },
+                new List<int>(){ -1, 0, 0, 1 }
+            };
+
+            // Act
+            var actual = solution.FourSum(nums, target);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void FourSum_Test2()
+        {
+            // Arrange
+            int[] nums = { 2, 2, 2, 2, 2 };
+            int target = 8;
+            IList<IList<int>> expected = new List<IList<int>>()
+            {
+                new List<int>(){ 2, 2, 2, 2 }
+            };
+
+            // Act
+            var actual = solution.FourSum(nums, target);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

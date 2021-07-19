43ed59d4ba1f90e8404b74d0f711e2d272d06b84
FakeStandard <rileylin.space@gmail.com>
2021-07-19 19:02:10 +0800

    (0235) new

---
+++ Solution/0235.Lowest-common-ancestor-of-a-binary-search-tree/0235-Lowest-common-ancestor-of-a-binary-search-tree.cs
@@ -0,0 +1,51 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0235.Lowest_common_ancestor_of_a_binary_search_tree
+{
+    public class _0235_Lowest_common_ancestor_of_a_binary_search_tree
+    {
+        /// <summary>
+        /// Iterative
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns></returns>
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            while (root != null)
+            {
+                if (root.val > p.val && root.val > q.val)
+                    root = root.left;
+                else if (root.val < p.val && root.val < q.val)
+                    root = root.right;
+                else
+                    break;
+            }
+
+            return root;
+        }
+
+        /// <summary>
+        /// Recursive
+        /// </summary>
+        /// <param name="root"></param>
+        /// <param name="p"></param>
+        /// <param name="q"></param>
+        /// <returns></returns>
+        //public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        //{
+        //    if (root == null) return null;
+
+        //    if (root.val < p.val && root.val < q.val)
+        //        return LowestCommonAncestor(root.right, p, q);
+        //    else if (root.val > p.val && root.val > q.val)
+        //        return LowestCommonAncestor(root.left, p, q);
+
+        //    return root;
+        //}
+    }
+}

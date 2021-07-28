16c9c8ab49718f1904dade38d667bfbad517c87b
FakeStandard <rileylin.space@gmail.com>
2021-07-28 10:29:29 +0800

    (0016) new

---
+++ Solution/0016.3Sum-closest/0016-3Sum-closest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0016._3Sum_closest
+{
+    public class _0016_3Sum_closest
+    {
+        public int ThreeSumClosest(int[] nums, int target)
+        {
+            Array.Sort(nums);
+
+            int left = 0;
+            int right = 0;
+            int sum = 0;
+            int diff = 0;
+            int closer = int.MaxValue;
+            int res = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                left = i + 1;
+                right = nums.Length - 1;
+
+                while (left < right)
+                {
+                    sum = nums[i] + nums[left] + nums[right];
+
+                    if (sum == target)
+                        return sum;
+                    if (sum < target)
+                        left++;
+                    if (sum > target)
+                        right--;
+
+                    diff = Math.Abs(target - sum);
+
+                    if (closer > diff)
+                    {
+                        closer = diff;
+                        res = sum;
+                    }
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ UnitTest/0016.3Sum-closest/_0016_3Sum_Closest_Test.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0016._3Sum_closest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0016._3Sum_closest.Tests
+{
+    [TestClass()]
+    public class _0016_3Sum_Closest_Test
+    {
+        _0016_3Sum_closest solution = new _0016_3Sum_closest();
+
+        [TestMethod()]
+        public void ThreeSumClosest_Test1()
+        {
+            // Arrange
+            int[] nums = { -1, 2, 1, -4 };
+            int target = 4;
+            var expected = 2;
+
+            // Act
+            var actual = solution.ThreeSumClosest(nums, target);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

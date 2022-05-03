75e69098dbff1834e9bdc9bbd3a25bb22aaf5c9b
Riley Lin <rileylin.space@gmail.com>
2022-05-03 15:29:43 +0800

    (2248) new

---
+++ Solution/2248.Intersection-of-Multiple-Arrays/2248-Intersection-of-Multiple-Arrays.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2248.Intersection_of_Multiple_Arrays
+{
+    public class _2248_Intersection_of_Multiple_Arrays
+    {
+        public IList<int> Intersection(int[][] nums)
+        {
+            // Array - no fast
+            //if (nums == null || nums.Length == 0) return new List<int> { };
+
+            //int[] res = nums[0];
+
+            //for (int i = 1; i < nums.Length; i++)
+            //    res = res.Intersect(nums[i]).ToArray();
+
+            //Array.Sort(res);
+
+            //return res;
+
+
+            // HashSet - fast
+            if (nums == null || nums.Length == 0) return new List<int> { };
+
+            HashSet<int> hash = new HashSet<int>(nums[0]);
+
+            for (int i = 1; i < nums.Length; i++)
+                hash.IntersectWith(nums[i]);
+
+            var res = hash.ToList();
+            res.Sort();
+
+            return res;
+        }
+    }
+}
---
+++ UnitTest/_2248_Intersection_of_Multiple_Arrays_Test.cs
@@ -0,0 +1,54 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _2248.Intersection_of_Multiple_Arrays;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2248.Intersection_of_Multiple_Arrays.Tests
+{
+    [TestClass()]
+    public class _2248_Intersection_of_Multiple_Arrays_Test
+    {
+        _2248_Intersection_of_Multiple_Arrays solution = new _2248_Intersection_of_Multiple_Arrays();
+
+        [TestMethod()]
+        public void Intersection_Test1()
+        {
+            // Arrange
+            int[][] nums = new int[][]
+            {
+                new int[]{ 3, 1, 2, 4, 5 },
+                new int[]{ 1, 2, 3, 4 },
+                new int[]{ 3, 4, 5, 6 }
+            };
+
+            IList<int> expected = new List<int> { 3, 4 };
+
+            // Act
+            var actual = solution.Intersection(nums);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void Intersection_Test2()
+        {
+            // Arrange
+            int[][] nums = new int[][]
+            {
+                new int[]{ 1, 2, 3 },
+                new int[]{ 4, 5, 6 }
+            };
+
+            IList<int> expected = new List<int> { };
+
+            // Act
+            var actual = solution.Intersection(nums);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

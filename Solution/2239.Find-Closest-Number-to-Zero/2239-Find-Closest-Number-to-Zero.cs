c6ef2e3e875de44a90178204de1dc43db0e59f68
Riley Lin <rileylin.space@gmail.com>
2022-05-03 15:54:16 +0800

    (2239) new

---
+++ Solution/2239.Find-Closest-Number-to-Zero/2239-Find-Closest-Number-to-Zero.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2239.Find_Closest_Number_to_Zero
+{
+    public class _2239_Find_Closest_Number_to_Zero
+    {
+        public int FindClosestNumber(int[] nums)
+        {
+            if (nums == null || nums.Length == 0) return 0;
+
+            int res = nums[0];
+            int abs = Math.Abs(nums[0]);
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int temp = Math.Abs(nums[i]);
+                if (temp < abs)
+                {
+                    res = nums[i];
+                    abs = temp;
+                }
+
+                if (temp == abs)
+                    if (nums[i] > res)
+                        res = nums[i];
+            }
+
+            return res;
+        }
+    }
+}
---
+++ UnitTest/_2239_Find_Closest_Number_to_Zero_Test.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _2239.Find_Closest_Number_to_Zero;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._2239.Find_Closest_Number_to_Zero.Tests
+{
+    [TestClass()]
+    public class _2239_Find_Closest_Number_to_Zero_Test
+    {
+        _2239_Find_Closest_Number_to_Zero solution = new _2239_Find_Closest_Number_to_Zero();
+
+        [TestMethod()]
+        public void FindClosestNumber_Test1()
+        {
+            // Arrange
+            int[] nums = { -4, -2, 1, 4, 8 };
+            int expected = 1;
+
+            // Act
+            var actual = solution.FindClosestNumber(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FindClosestNumber_Test2()
+        {
+            // Arrange
+            int[] nums = { 2, -1, 1 };
+            int expected = 1;
+
+            // Act
+            var actual = solution.FindClosestNumber(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

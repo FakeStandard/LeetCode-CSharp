8fbcb82f19b633f769d4e8ad0adf3e00a17f54a0
Riley Lin <rileylin.space@gmail.com>
2022-05-03 11:49:23 +0800

    Modify the 0028 method

--- Solution/0028.Implement-strStr/0028-Implement-strStr.cs
+++ Solution/0028.Implement-strStr/0028-Implement-strStr.cs
@@ -8,13 +8,17 @@ namespace Solution._0028.Implement_strStr
     {
         public int StrStr(string haystack, string needle)
         {
-            if (string.IsNullOrWhiteSpace(haystack) && string.IsNullOrWhiteSpace(needle))
-                return 0;
+            // 直接使用 IndexOf 方法回傳結果
+            // By default, it return -1 if nothing is found.
+            return haystack.IndexOf(needle);
 
-            if (haystack.Contains(needle))
-                return haystack.IndexOf(needle);
-            else
-                return -1;
+            //if (string.IsNullOrWhiteSpace(haystack) && string.IsNullOrWhiteSpace(needle))
+            //    return 0;
+
+            //if (haystack.Contains(needle))
+            //    return haystack.IndexOf(needle);
+            //else
+            //    return -1;
         }
     }
 }

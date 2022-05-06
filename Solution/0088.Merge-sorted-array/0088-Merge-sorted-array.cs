6fd1120f00ee3b28ee69830e7701757b6f6ea9b9
Riley Lin <rileylin.space@gmail.com>
2022-05-06 18:17:12 +0800

    Update 0088-Merge-sorted-array.cs

--- Solution/0088.Merge-sorted-array/0088-Merge-sorted-array.cs
+++ Solution/0088.Merge-sorted-array/0088-Merge-sorted-array.cs
@@ -8,10 +8,32 @@ namespace Solution._0088.Merge_sorted_array
     {
         public void Merge(int[] nums1, int m, int[] nums2, int n)
         {
-            for (int i = 0; i < n; i++)
+            int i, j, tmp, flag;
+
+            for (i = 0; i < n; i++)
                 nums1[i + m] = nums2[i];
 
-            Array.Sort(nums1);
+            //Array.Sort(nums1);
+
+            // bubble sort
+            for (i = nums1.Length - 1; i > 0; i--)
+            {
+                flag = 0;
+                for (j = 0; j < i; j++)
+                {
+                    if (nums1[j] > nums1[j + 1])
+                    {
+                        // swap
+                        tmp = nums1[j];
+                        nums1[j] = nums1[j + 1];
+                        nums1[j + 1] = tmp;
+
+                        flag++;
+                    }
+                }
+
+                if (flag == 0) break;
+            }
         }
     }
 }

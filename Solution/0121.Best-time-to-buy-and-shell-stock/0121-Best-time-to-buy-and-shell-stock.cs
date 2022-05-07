31f94aefc04c5c2a5e8ee2ce8bc402b289b49cd7
Riley Lin <rileylin.space@gmail.com>
2022-05-07 12:59:36 +0800

    Update 0121-Best-time-to-buy-and-shell-stock.cs

--- Solution/0121.Best-time-to-buy-and-shell-stock/0121-Best-time-to-buy-and-shell-stock.cs
+++ Solution/0121.Best-time-to-buy-and-shell-stock/0121-Best-time-to-buy-and-shell-stock.cs
@@ -15,8 +15,12 @@ namespace Solution._0121.Best_time_to_buy_and_shell_stock
 
             for (int i = 0; i < prices.Length; i++)
             {
-                min = Math.Min(min, prices[i]);
-                max = Math.Max(max, prices[i] - min);
+                // it's faster than using Math method
+                if (prices[i] < min) min = prices[i];
+                if (prices[i] - min > max) max = prices[i] - min;
+
+                //min = Math.Min(min, prices[i]);
+                //max = Math.Max(max, prices[i] - min);
             }
 
             return max;

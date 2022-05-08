2c628dd9b9d62264365e4ffcfb7e803f0595796e
Riley Lin <rileylin.space@gmail.com>
2022-05-08 13:49:15 +0800

    (0383) new

---
+++ Solution/0383.Ransom-note/0383-Ransom-note.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0383.Ransom_note
+{
+    public class _0383_Ransom_note
+    {
+        public bool CanConstruct(string ransomNote, string magazine)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            foreach (char c in magazine)
+            {
+                if (dic.ContainsKey(c))
+                    dic[c]++;
+                else
+                    dic.Add(c, 1);
+            }
+
+            foreach(char c in ransomNote)
+            {
+                if (dic.ContainsKey(c))
+                    dic[c]--;
+                else return false;
+                if (dic[c] == -1) return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ UnitTest/0383.Ransom-note/_0383_Ransom_note_Test.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0383.Ransom_note;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0383.Ransom_note.Tests
+{
+    [TestClass()]
+    public class _0383_Ransom_note_Test
+    {
+        _0383_Ransom_note solution = new _0383_Ransom_note();
+
+        [TestMethod()]
+        public void CanConstruct_Test1()
+        {
+            // Arrange
+            string ransomNote = "a";
+            string magazine = "b";
+
+            // Act
+            var actual = solution.CanConstruct(ransomNote, magazine);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void CanConstruct_Test2()
+        {
+            // Arrange
+            string ransomNote = "aa";
+            string magazine = "ab";
+
+            // Act
+            var actual = solution.CanConstruct(ransomNote, magazine);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void CanConstruct_Test3()
+        {
+            // Arrange
+            string ransomNote = "aa";
+            string magazine = "aab";
+
+            // Act
+            var actual = solution.CanConstruct(ransomNote, magazine);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+    }
+}

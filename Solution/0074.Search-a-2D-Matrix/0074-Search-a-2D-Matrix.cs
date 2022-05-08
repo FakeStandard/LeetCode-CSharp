cbb9a927656e3b57d893fa0866b04cf732682728
Riley Lin <rileylin.space@gmail.com>
2022-05-08 13:04:18 +0800

    (0074) new

---
+++ Solution/0074.Search-a-2D-Matrix/0074-Search-a-2D-Matrix.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0074.Search_a_2D_Matrix
+{
+    public class _0074_Search_a_2D_Matrix
+    {
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            // easy solution
+            if (matrix.Length == 0) return false;
+
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    if (matrix[i][j] > target) return false;
+                    if (matrix[i][j] == target) return true;
+                }
+            }
+
+            return false;
+
+            // binary search
+        }
+    }
+}
---
+++ UnitTest/_0074_Search_a_2D_Matrix_Test.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0074.Search_a_2D_Matrix;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0074.Search_a_2D_Matrix.Tests
+{
+    [TestClass()]
+    public class _0074_Search_a_2D_Matrix_Test
+    {
+        _0074_Search_a_2D_Matrix solution = new _0074_Search_a_2D_Matrix();
+
+        [TestMethod()]
+        public void SearchMatrix_Test1()
+        {
+            // Arrange
+            int[][] matrix = new int[][] {
+                new int[]{ 1, 3, 5, 7 },
+                new int[]{ 10, 11, 16, 20 },
+                new int[]{ 23, 30, 34, 60 },
+            };
+            int target = 3;
+
+            // Act
+            var actual = solution.SearchMatrix(matrix, target);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void SearchMatrix_Test2()
+        {
+            // Arrange
+            int[][] matrix = new int[][] {
+                new int[]{ 1, 3, 5, 7 },
+                new int[]{ 10, 11, 16, 20 },
+                new int[]{ 23, 30, 34, 60 },
+            };
+            int target = 13;
+
+            // Act
+            var actual = solution.SearchMatrix(matrix, target);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+    }
+}

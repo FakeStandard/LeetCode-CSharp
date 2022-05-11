a7cd8f93b9f580579083ab99b99269f1f7199757
Riley Lin <rileylin.space@gmail.com>
2022-05-11 13:30:21 +0800

    (0028) update

--- Solution/0028.Implement-strStr/0028-Implement-strStr.cs
+++ Solution/0028.Implement-strStr/0028-Implement-strStr.cs
@@ -8,10 +8,39 @@ namespace Solution._0028.Implement_strStr
     {
         public int StrStr(string haystack, string needle)
         {
+            // Solution 3
+            // Implement the StrStr method without using the implemented method.
+            if (needle.Length == 0) return 0;
+            if (needle.Length > haystack.Length) return -1;
+
+            int i = 0, j = 0, index = 0;
+            while (i < haystack.Length)
+            {
+                if (haystack[i] == needle[j])
+                {
+                    // accumulate
+                    i++;
+                    j++;
+
+                    if (j == needle.Length) return index;
+                }
+                else
+                {
+                    // recalculate
+                    index++;
+                    i = index;
+                    j = 0;
+                }
+            }
+
+            return -1;
+
+            // Solution 2
             // 直接使用 IndexOf 方法回傳結果
             // By default, it return -1 if nothing is found.
-            return haystack.IndexOf(needle);
+            //return haystack.IndexOf(needle);
 
+            // Solution 1
             //if (string.IsNullOrWhiteSpace(haystack) && string.IsNullOrWhiteSpace(needle))
             //    return 0;
 
--- Solution/0232.Implement-queue-using-stacks/0232-Implement-queue-using-stacks.cs
+++ Solution/0232.Implement-queue-using-stacks/0232-Implement-queue-using-stacks.cs
@@ -7,56 +7,58 @@ namespace Solution._0232.Implement_queue_using_stacks
     public class _0232_Implement_queue_using_stacks
     {
         /**
-     * Your MyQueue object will be instantiated and called as such:
-     * MyQueue obj = new MyQueue();
-     * obj.Push(x);
-     * int param_2 = obj.Pop();
-     * int param_3 = obj.Peek();
-     * bool param_4 = obj.Empty();
-     */
+         * Your MyQueue object will be instantiated and called as such:
+         * MyQueue obj = new MyQueue();
+         * obj.Push(x);
+         * int param_2 = obj.Pop();
+         * int param_3 = obj.Peek();
+         * bool param_4 = obj.Empty();
+         */
+
         public class MyQueue
         {
             /** Initialize your data structure here. */
 
-            Stack<int> stack;
-            Stack<int> newStack;
+            Stack<int> curr;
+            Stack<int> reverse;
 
             public MyQueue()
             {
-                stack = new Stack<int>();
-                newStack = new Stack<int>();
+                //  using only two stacks.
+                curr = new Stack<int>();
+                reverse = new Stack<int>();
             }
 
             /** Push element x to the back of queue. */
             public void Push(int x)
             {
-                stack.Push(x);
+                curr.Push(x);
             }
 
             /** Removes the element from in front of queue and returns that element. */
             public int Pop()
             {
-                if (newStack.Count == 0)
-                    while (stack.Count > 0)
-                        newStack.Push(stack.Pop());
+                if (reverse.Count == 0)
+                    while (curr.Count > 0)
+                        reverse.Push(curr.Pop());
 
-                return newStack.Pop();
+                return reverse.Pop();
             }
 
             /** Get the front element. */
             public int Peek()
             {
-                if (newStack.Count == 0)
-                    while (stack.Count > 0)
-                        newStack.Push(stack.Pop());
+                if (reverse.Count == 0)
+                    while (curr.Count > 0)
+                        reverse.Push(curr.Pop());
 
-                return newStack.Peek();
+                return reverse.Peek();
             }
 
             /** Returns whether the queue is empty. */
             public bool Empty()
             {
-                return (stack.Count == 0) && (newStack.Count == 0);
+                return (curr.Count == 0) && (reverse.Count == 0);
             }
         }
     }

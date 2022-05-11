20d10c3152037490438f9ec8114eac1c5cf2507f
Riley Lin <rileylin.space@gmail.com>
2022-05-11 12:34:53 +0800

    (0088) update

--- Solution/0088.Merge-sorted-array/0088-Merge-sorted-array.cs
+++ Solution/0088.Merge-sorted-array/0088-Merge-sorted-array.cs
@@ -8,6 +8,7 @@ namespace Solution._0088.Merge_sorted_array
     {
         public void Merge(int[] nums1, int m, int[] nums2, int n)
         {
+            // Solution 1
             int i, j, tmp, flag;
 
             for (i = 0; i < n; i++)
@@ -34,6 +35,33 @@ namespace Solution._0088.Merge_sorted_array
 
                 if (flag == 0) break;
             }
+
+            // Solution 2
+            //int i = m - 1, j = n - 1, index = m + n - 1;
+
+            //while (i >= 0 && j >= 0)
+            //{
+            //    if (nums1[i] > nums2[j])
+            //    {
+            //        nums1[index] = nums1[i];
+            //        i--;
+            //    }
+            //    else
+            //    {
+            //        nums1[index] = nums2[j];
+            //        j--;
+            //    }
+
+            //    index--;
+            //}
+
+            //// No need to handle i >= 0 case. If it's the case, the remaining numbers are already in nums1.
+            //while (j >= 0)
+            //{
+            //    nums1[index] = nums2[j];
+            //    index--;
+            //    j--;
+            //}
         }
     }
 }

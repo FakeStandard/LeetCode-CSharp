a9f65c28f6dbb88d4b56284477d368c3db9ea89a
Riley Lin <rileylin.space@gmail.com>
2022-05-12 14:58:07 +0800

    (0094) update

---
+++ UnitTest/_0094_Binary_tree_inorder_traversal_Test.cs
@@ -0,0 +1,59 @@
+﻿using Common;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0094.Binary_tree_inorder_traversal;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0094.Binary_tree_inorder_traversal.Tests
+{
+    [TestClass()]
+    public class _0094_Binary_tree_inorder_traversal_Test
+    {
+        _0094_Binary_tree_inorder_traversal solution = new _0094_Binary_tree_inorder_traversal();
+
+        [TestMethod()]
+        public void InorderTraversal_Test1()
+        {
+            // Arrange
+            TreeNode root = new TreeNode().AddNode(new string[] { "1", null, "2", "3" });
+            var expected = new int[] { 1, 3, 2 };
+
+            // Act
+            var actual = solution.InorderTraversal(root);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+
+        }
+
+        //[TestMethod()]
+        //public void InorderTraversal_Test2()
+        //{
+        //    // Arrange
+        //    TreeNode root = new TreeNode().AddNode(new string[] { });
+        //    var expected = new int[] { };
+
+        //    // Act
+        //    var actual = solution.InorderTraversal(root);
+
+        //    // Assert
+        //    actual.Should().BeEquivalentTo(expected);
+        //}
+
+        [TestMethod()]
+        public void InorderTraversal_Test3()
+        {
+            // Arrange
+            TreeNode root = new TreeNode().AddNode(new string[] { "1" });
+            var expected = new int[] { 1 };
+
+            // Act
+            var actual = solution.InorderTraversal(root);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

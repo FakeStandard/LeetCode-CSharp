0b75e40b9ac0da365d00ead7b1a6285b53e02a65
Riley Lin <rileylin.space@gmail.com>
2022-05-13 18:07:18 +0800

    (0015) new, but time limit exceeded

---
+++ Solution/0015.3Sum/0015-3Sum.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solution._0015._3Sum
+{
+    public class _0015_3Sum
+    {
+        public IList<IList<int>> ThreeSum(int[] nums)
+        {
+            IList<IList<int>> res = new List<IList<int>>();
+
+            if (nums.Length < 3) return res;
+
+            // bubble sort
+            int tmp = 0;
+            int flag = 0;
+
+            for (int i = nums.Length - 1; i > 0; i--)
+            {
+                flag = 0;
+
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[j] > nums[j + 1])
+                    {
+                        // swap
+                        tmp = nums[j];
+                        nums[j] = nums[j + 1];
+                        nums[j + 1] = tmp;
+
+                        flag++;
+                    }
+                }
+
+                if (flag == 0) break;
+            }
+
+            //
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    for (int k = j + 1; k < nums.Length; k++)
+                    {
+                        if (nums[i] + nums[j] + nums[k] == 0)
+                        {
+                            List<int> list = new List<int>();
+                            list.Add(nums[i]);
+                            list.Add(nums[j]);
+                            list.Add(nums[k]);
+
+                            if (Traverse(res, list))
+                                res.Add(list);
+                        }
+                    }
+                }
+            }
+
+            return res;
+        }
+
+        private bool Traverse(IList<IList<int>> res, List<int> list)
+        {
+            for (int i = 0; i < res.Count; i++)
+            {
+                //if (res[i][0] == list[0] && res[i][1] == list[1] && res[i][2] == list[2]) return false;
+
+                // linq
+                if (Enumerable.SequenceEqual(res[i], list)) return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ UnitTest/_0015_3Sum_Test.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0015._3Sum;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0015._3Sum.Tests
+{
+    [TestClass()]
+    public class _0015_3Sum_Test
+    {
+        _0015_3Sum solution = new _0015_3Sum();
+
+        [TestMethod()]
+        public void ThreeSum_Test1()
+        {
+            // Arrange
+            int[] nums = new int[] { -1, 0, 1, 2, -1, -4 };
+            var expected = new int[][]
+            {
+                new int[]{ -1, -1, 2 },
+                new int[]{ -1, 0, 1 },
+            };
+
+            // Act
+            var actual = solution.ThreeSum(nums);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void ThreeSum_Test2()
+        {
+            // Arrange
+            int[] nums = new int[] { };
+            var expected = new int[] { };
+
+            // Act
+            var actual = solution.ThreeSum(nums);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void ThreeSum_Test3()
+        {
+            // Arrange
+            int[] nums = new int[] { 0 };
+            var expected = new int[] { };
+
+            // Act
+            var actual = solution.ThreeSum(nums);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

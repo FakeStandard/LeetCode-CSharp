49457c627a08e5fdf09aac155a6789c5b8c72073
Riley Lin <rileylin.space@gmail.com>
2022-05-13 15:58:18 +0800

    (2006) new (1534) update

--- Solution/1534.Count-good-triplets/1534-Count-good-triplets.cs
+++ Solution/1534.Count-good-triplets/1534-Count-good-triplets.cs
@@ -14,21 +14,37 @@ namespace Solution._1534.Count_good_triplets
             {
                 for (int j = i + 1; j < arr.Length; j++)
                 {
-                    if (Math.Abs(arr[i] - arr[j]) <= a)
+                    if (Math.Abs(arr[i] - arr[j]) > a) continue;
+
+                    for (int k = j + 1; k < arr.Length; k++)
                     {
-                        for (int k = j + 1; k < arr.Length; k++)
-                        {
-                            if (Math.Abs(arr[j] - arr[k]) <= b)
-                            {
-                                if (Math.Abs(arr[i] - arr[k]) <= c)
-                                {
-                                    count++;
-                                }
-                            }
-                        }
+                        if (Math.Abs(arr[j] - arr[k]) > b) continue;
+                        if (Math.Abs(arr[i] - arr[k]) > c) continue;
+
+                        count++;
                     }
                 }
             }
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    for (int j = i + 1; j < arr.Length; j++)
+            //    {
+            //        if (Math.Abs(arr[i] - arr[j]) <= a)
+            //        {
+            //            for (int k = j + 1; k < arr.Length; k++)
+            //            {
+            //                if (Math.Abs(arr[j] - arr[k]) <= b)
+            //                {
+            //                    if (Math.Abs(arr[i] - arr[k]) <= c)
+            //                    {
+            //                        count++;
+            //                    }
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
 
             return count;
         }
---
+++ Solution/2006.Count-number-of-pairs-with-absolute-difference-k/2006-Count-number-of-pairs-with-absolute-difference-k.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._2006.Count_number_of_pairs_with_absolute_difference_k
+{
+   public class _2006_Count_number_of_pairs_with_absolute_difference_k
+    {
+    }
+}

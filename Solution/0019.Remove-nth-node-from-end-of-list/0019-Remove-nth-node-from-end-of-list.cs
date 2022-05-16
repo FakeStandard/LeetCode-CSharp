677605757f650110c627d8054c8f59a32d8e54f8
Riley Lin <rileylin.space@gmail.com>
2022-05-16 17:02:37 +0800

    (0019) new

---
+++ Solution/0019.Remove-nth-node-from-end-of-list/0019-Remove-nth-node-from-end-of-list.cs
@@ -0,0 +1,42 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0019.Remove_nth_node_from_end_of_list
+{
+    public class _0019_Remove_nth_node_from_end_of_list
+    {
+        public ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+            if (head == null) return head;
+
+            ListNode curr = head, tmp = curr, res = curr;
+            int count = 0;
+
+            // counting
+            while (tmp != null)
+            {
+                count++;
+                tmp = tmp.next;
+            }
+
+            if (count == 1 || count == n) return res.next;
+
+            while (head != null)
+            {
+                if (count == n + 1)
+                {
+                    curr.next = head.next.next;
+                    break;
+                }
+
+                count--;
+                curr = head.next;
+                head = head.next;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ UnitTest/_0019_Remove_nth_node_from_end_of_list_Test.cs
@@ -0,0 +1,76 @@
+﻿using Common;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0019.Remove_nth_node_from_end_of_list;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0019.Remove_nth_node_from_end_of_list.Tests
+{
+    [TestClass()]
+    public class _0019_Remove_nth_node_from_end_of_list_Test
+    {
+        _0019_Remove_nth_node_from_end_of_list solution = new _0019_Remove_nth_node_from_end_of_list();
+
+        [TestMethod()]
+        public void RemoveNthFromEnd_Test1()
+        {
+            // Arrange
+            ListNode head = new ListNode().AddNode(new int[] { 1, 2, 3, 4, 5 });
+            int n = 2;
+            var expected = new ListNode().AddNode(new int[] { 1, 2, 3, 5 });
+
+            // Act
+            var actual = solution.RemoveNthFromEnd(head, n);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        //[TestMethod()]
+        //public void RemoveNthFromEnd_Test2()
+        //{
+        //    // Arrange
+        //    ListNode head = new ListNode().AddNode(new int[] { 1 });
+        //    int n = 1;
+        //    var expected = new ListNode().AddNode(new int[] { });
+
+        //    // Act
+        //    var actual = solution.RemoveNthFromEnd(head, n);
+
+        //    // Assert
+        //    actual.Should().BeEquivalentTo(expected);
+        //}
+
+        [TestMethod()]
+        public void RemoveNthFromEnd_Test3()
+        {
+            // Arrange
+            ListNode head = new ListNode().AddNode(new int[] { 1, 2 });
+            int n = 1;
+            var expected = new ListNode().AddNode(new int[] { 1 });
+
+            // Act
+            var actual = solution.RemoveNthFromEnd(head, n);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void RemoveNthFromEnd_Test4()
+        {
+            // Arrange
+            ListNode head = new ListNode().AddNode(new int[] { 1, 2 });
+            int n = 2;
+            var expected = new ListNode().AddNode(new int[] { 2 });
+
+            // Act
+            var actual = solution.RemoveNthFromEnd(head, n);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

43ded92025cbd4f46fdd1f97e9d7379ef8b59d5e
Riley Lin <rileylin.space@gmail.com>
2022-05-16 11:14:45 +0800

    (0701) new

---
+++ Solution/0701.Insert-into-a-binary-search-tree/0701-Insert-into-a-binary-search-tree.cs
@@ -0,0 +1,43 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0701.Insert_into_a_binary_search_tree
+{
+    public class _0701_Insert_into_a_binary_search_tree
+    {
+        public TreeNode InsertIntoBST(TreeNode root, int val)
+        {
+            if (root == null) return root = new TreeNode(val);
+
+            TreeNode curr = root;
+
+            while (curr != null)
+            {
+                if (curr.val > val)
+                {
+                    if (curr.left == null)
+                    {
+                        curr.left = new TreeNode(val);
+                        break;
+                    }
+
+                    curr = curr.left;
+                }
+                else
+                {
+                    if (curr.right == null)
+                    {
+                        curr.right = new TreeNode(val);
+                        break;
+                    }
+
+                    curr = curr.right;
+                }
+            }
+
+            return root;
+        }
+    }
+}

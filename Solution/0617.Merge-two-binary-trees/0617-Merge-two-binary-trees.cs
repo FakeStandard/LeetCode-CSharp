e5155b1fc594eccf8b684a8b05bef781702977b0
Riley Lin <rileylin.space@gmail.com>
2022-05-16 15:52:46 +0800

    Update 0617-Merge-two-binary-trees.cs

--- Solution/0617.Merge-two-binary-trees/0617-Merge-two-binary-trees.cs
+++ Solution/0617.Merge-two-binary-trees/0617-Merge-two-binary-trees.cs
@@ -9,12 +9,7 @@ namespace Solution._0617.Merge_two_binary_trees
     {
         public TreeNode MergeTrees(TreeNode root1, TreeNode root2)
         {
-            if (root1 == null && root2 == null)
-                return null;
-            if (root1 == null)
-                return root2;
-            if (root2 == null)
-                return root1;
+            if (root1 == null || root2 == null) return root1 == null ? root2 : root1;
 
             root1.val += root2.val;
             root1.left = MergeTrees(root1.left, root2.left);

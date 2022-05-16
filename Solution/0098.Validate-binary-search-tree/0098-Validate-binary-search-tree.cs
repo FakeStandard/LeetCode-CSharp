980d3538a9a969efa296592bcaf8230cbf8b04ab
Riley Lin <rileylin.space@gmail.com>
2022-05-16 10:48:48 +0800

    (0098) new

---
+++ Solution/0098.Validate-binary-search-tree/0098-Validate-binary-search-tree.cs
@@ -0,0 +1,25 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0098.Validate_binary_search_tree
+{
+    public class _0098_Validate_binary_search_tree
+    {
+        public bool IsValidBST(TreeNode root)
+        {
+            if (root == null) return true;
+
+            return IsValidBST(root, long.MinValue, long.MaxValue);
+        }
+
+        public bool IsValidBST(TreeNode root, long min, long max)
+        {
+            if (root == null) return true;
+            if (root.val <= min || root.val >= max) return false;
+
+            return IsValidBST(root.left, min, root.val) && IsValidBST(root.right, root.val, max);
+        }
+    }
+}

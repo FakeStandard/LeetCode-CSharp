a9b5f93964912dd774bac1a79a406c630783a221
Riley Lin <rileylin.space@gmail.com>
2022-05-16 14:34:23 +0800

    (0704) new

---
+++ Solution/0704.Binary-search/0704-Binary-search.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0704.Binary_search
+{
+    public class _0704_Binary_search
+    {
+        public int Search(int[] nums, int target)
+        {
+            // binary search
+            int left = 0;
+            int right = nums.Length - 1;
+            int mid;
+
+            while (left <= right)
+            {
+                mid = (left + right) / 2;
+
+                if (nums[mid] == target) return mid;
+                else if (target > nums[mid]) left = mid + 1;
+                else right = mid - 1;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ UnitTest/_0704_Binary_search_Test.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0704.Binary_search;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0704.Binary_search.Tests
+{
+    [TestClass()]
+    public class _0704_Binary_search_Test
+    {
+        _0704_Binary_search solution = new _0704_Binary_search();
+
+        [TestMethod()]
+        public void Search_Test1()
+        {
+            // Arrange
+            int[] nums = new int[] { -1, 0, 3, 5, 9, 12 };
+            int target = 9;
+            int expected = 4;
+
+            // Act
+            var actual = solution.Search(nums, target);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Search_Test2()
+        {
+            // Arrange
+            int[] nums = new int[] { -1, 0, 3, 5, 9, 12 };
+            int target = 2;
+            int expected = -1;
+
+            // Act
+            var actual = solution.Search(nums, target);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

437dad6d892238686017f053e2552db342a57fd7
Riley Lin <rileylin.space@gmail.com>
2022-05-16 10:14:57 +0800

    (0700) update

--- Solution/0700.Search-in-a-inary-search-tree/0700-Search-in-a-inary-search-tree.cs
+++ Solution/0700.Search-in-a-inary-search-tree/0700-Search-in-a-inary-search-tree.cs
@@ -9,22 +9,16 @@ namespace Solution._0700.Search_in_a_inary_search_tree
     {
         public TreeNode SearchBST(TreeNode root, int val)
         {
-            //if (root == null)
-            //    return null;
-            //else if (root.val == val)
-            //    return root;
-            //else if (val < root.val)
-            //    return SearchBST(root.left, val);
-            //else if (val > root.val)
-            //    return SearchBST(root.right, val);
-            //else
-            //    return null;
+            if (root == null) return null;
 
-            if (root == null)
-                return null;
+            if (root.val == val) return root;
 
-            if (root.val == val)
-                return root;
+            /*
+            if (val > root.val)
+                return SearchBST(root.right, val);
+            else
+                return SearchBST(root.left, val);
+            */
 
             return val < root.val ? SearchBST(root.left, val) : SearchBST(root.right, val);
         }

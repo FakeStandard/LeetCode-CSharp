4eb4887d69f715fbb10f220a3e4fc133e7b6aca0
Riley Lin <rileylin.space@gmail.com>
2022-05-17 10:02:54 +0800

    (0706) new

---
+++ Solution/0706.Design-hashmap/0706-Design-hashmap.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0706.Design_hashmap
+{
+    public class _0706_Design_hashmap
+    {
+        public class MyHashMap
+        {
+            List<MyTuple> tuple;
+
+            public MyHashMap()
+            {
+                tuple = new List<MyTuple>();
+            }
+
+            public void Put(int key, int value)
+            {
+                var obj = Contains(key);
+
+                if (obj == null)
+                {
+                    tuple.Add(new MyTuple(key, value));
+                    return;
+                }
+
+                obj.value = value;
+            }
+
+            public int Get(int key)
+            {
+                var obj = Contains(key);
+
+                return obj == null ? -1 : obj.value;
+            }
+
+            public void Remove(int key)
+            {
+                var obj = Contains(key);
+
+                if (obj != null) tuple.Remove(obj);
+            }
+
+            private MyTuple Contains(int key)
+            {
+                return tuple.Find(r => r.key == key);
+            }
+        }
+
+        public class MyTuple
+        {
+            public int key;
+            public int value;
+            public MyTuple(int k, int v)
+            {
+                key = k;
+                value = v;
+            }
+        }
+    }
+}

a0197d24b45ddac865bc97fe70add55c8945ba56
Riley Lin <rileylin.space@gmail.com>
2022-05-18 10:59:04 +0800

    Update

--- Solution/0111.Minimum-depth-of-binary-tree/0111-Minimum-depth-of-binary-tree.cs
+++ Solution/0111.Minimum-depth-of-binary-tree/0111-Minimum-depth-of-binary-tree.cs
@@ -9,18 +9,18 @@ namespace Solution._0111.Minimum_depth_of_binary_tree
     {
         public int MinDepth(TreeNode root)
         {
-            //if (root == null) return 0;
+            if (root == null) return 0;
 
-            //if (root.left == null)
-            //    return MinDepth(root.right) + 1;
+            if (root.left == null)
+                return MinDepth(root.right) + 1;
 
-            //if (root.right == null)
-            //    return MinDepth(root.left) + 1;
+            if (root.right == null)
+                return MinDepth(root.left) + 1;
 
-            //int l = MinDepth(root.left) + 1;
-            //int r = MinDepth(root.right) + 1;
+            int l = MinDepth(root.left) + 1;
+            int r = MinDepth(root.right) + 1;
 
-            //return l > r ? r : l;
+            return l > r ? r : l;
         }
     }
 }

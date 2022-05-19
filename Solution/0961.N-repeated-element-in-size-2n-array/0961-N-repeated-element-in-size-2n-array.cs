8affcfe999317923c09369592d726c25a7f066a9
Riley Lin <rileylin.space@gmail.com>
2022-05-19 12:05:18 +0800

    (0961)) new

---
+++ Solution/0961.N-repeated-element-in-size-2n-array/0961-N-repeated-element-in-size-2n-array.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0961.N_repeated_element_in_size_2n_array
+{
+    public class _0961_N_repeated_element_in_size_2n_array
+    {
+        public int RepeatedNTimes(int[] nums)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (dic.ContainsKey(nums[i])) return nums[i];
+
+                dic.Add(nums[i], 0);
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ UnitTest/0961.N-repeated-element-in-size-2n-array/_0961_N_repeated_element_in_size_2n_array_Test.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0961.N_repeated_element_in_size_2n_array;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0961.N_repeated_element_in_size_2n_array.Tests
+{
+    [TestClass()]
+    public class _0961_N_repeated_element_in_size_2n_array_Test
+    {
+        _0961_N_repeated_element_in_size_2n_array solution = new _0961_N_repeated_element_in_size_2n_array();
+
+        [TestMethod()]
+        public void RepeatedNTimes_Test1()
+        {
+            // Arrange
+            int[] nums = new int[] { 1, 2, 3, 3 };
+            var expected = 3;
+
+            // Act
+            var actual = solution.RepeatedNTimes(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void RepeatedNTimes_Test2()
+        {
+            // Arrange
+            int[] nums = new int[] { 2, 1, 2, 5, 3, 2 };
+            var expected = 2;
+
+            // Act
+            var actual = solution.RepeatedNTimes(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void RepeatedNTimes_Test3()
+        {
+            // Arrange
+            int[] nums = new int[] { 5, 1, 5, 2, 5, 3, 5, 4 };
+            var expected = 5;
+
+            // Act
+            var actual = solution.RepeatedNTimes(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

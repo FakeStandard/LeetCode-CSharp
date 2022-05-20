d4cbe88ce3861c7cb2812d501eed02aae5f105db
Riley Lin <rileylin.space@gmail.com>
2022-05-20 11:42:09 +0800

    (0202) new

---
+++ Solution/0202.Happy-number/0202-Happy-numbercs.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0202.Happy_number
+{
+    public class _0202_Happy_numbercs
+    {
+        public bool IsHappy(int n)
+        {
+            HashSet<int> set = new HashSet<int>();
+            int sum = 0, digit = 0;
+
+            while (true)
+            {
+                sum = 0;
+
+                while (n > 0)
+                {
+                    digit = n % 10;
+                    sum += digit * digit;
+                    n /= 10;
+                }
+
+                n = sum;
+
+                if (!set.Add(n)) return false;
+                if (n == 1) return true;
+            }
+        }
+    }
+}
---
+++ UnitTest/_0202_Happy_numbercs_Test.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0202.Happy_number;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0202.Happy_number.Tests
+{
+    [TestClass()]
+    public class _0202_Happy_numbercs_Test
+    {
+        _0202_Happy_numbercs solution = new _0202_Happy_numbercs();
+
+        [TestMethod()]
+        public void IsHappy_Test1()
+        {
+            // Arrange
+            int n = 10;
+
+            // Act
+            bool actual = solution.IsHappy(n);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void IsHappy_Test2()
+        {
+            // Arrange
+            int n = 2;
+
+            // Act
+            bool actual = solution.IsHappy(n);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+    }
+}

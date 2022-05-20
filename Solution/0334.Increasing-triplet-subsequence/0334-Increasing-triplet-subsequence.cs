8dca936bd284041a8925ac541233f6500f3c9bd0
Riley Lin <rileylin.space@gmail.com>
2022-05-20 11:03:42 +0800

    (0334) new

---
+++ Solution/0334.Increasing-triplet-subsequence/0334-Increasing-triplet-subsequence.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0334.Increasing_triplet_subsequence
+{
+    public class _0334_Increasing_triplet_subsequence
+    {
+        public bool IncreasingTriplet(int[] nums)
+        {
+            if (nums.Length < 3) return false;
+
+            int min = int.MaxValue;
+            int mid = int.MaxValue;
+
+            foreach (int num in nums)
+            {
+                if (num < min)
+                    min = num;
+                else if (num != min && num < mid)
+                    mid = num;
+                else if (num > mid)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ UnitTest/_0334_Increasing_triplet_subsequence_Test.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0334.Increasing_triplet_subsequence;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0334.Increasing_triplet_subsequence.Tests
+{
+    [TestClass()]
+    public class _0334_Increasing_triplet_subsequence_Test
+    {
+        _0334_Increasing_triplet_subsequence solution = new _0334_Increasing_triplet_subsequence();
+
+        [TestMethod()]
+        public void IncreasingTriplet_Test1()
+        {
+            // Arrange
+            int[] nums = new int[] { 1, 2, 3, 4, 5 };
+
+            // Act
+            var actual = solution.IncreasingTriplet(nums);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void IncreasingTriplet_Test2()
+        {
+            // Arrange
+            int[] nums = new int[] { 5, 4, 3, 2, 1 };
+
+            // Act
+            var actual = solution.IncreasingTriplet(nums);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void IncreasingTriplet_Test3()
+        {
+            // Arrange
+            int[] nums = new int[] { 2, 1, 5, 0, 4, 6 };
+
+            // Act
+            var actual = solution.IncreasingTriplet(nums);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void IncreasingTriplet_Test4()
+        {
+            // Arrange
+            int[] nums = new int[] { 20, 100, 10, 12, 5, 13 };
+
+            // Act
+            var actual = solution.IncreasingTriplet(nums);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+    }
+}

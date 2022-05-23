6d728a3405fbb3f27b4b156dc8ae8578361b2802
Riley Lin <rileylin.space@gmail.com>
2022-05-23 18:24:54 +0800

    (0003) new

---
+++ Solution/0003.Longest-substring-without-repeating-characters/0003-Longest-substring-without-repeating-characters.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0003.Longest_substring_without_repeating_characters
+{
+    public class _0003_Longest_substring_without_repeating_characters
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            if (s.Length == 0) return 0;
+
+            char[] chars = s.ToCharArray();
+            int index = 0, max = 1, res = max;
+            HashSet<char> set = new HashSet<char>();
+            set.Add(chars[0]);
+
+            for (int i = 1; i < chars.Length; i++)
+            {
+                if (set.Add(chars[i])) max++;
+                else
+                {
+                    if (max > res) res = max;
+                    max = 1;
+                    set.Clear();
+                    i = ++index;
+                    set.Add(chars[i]);
+                }
+            }
+
+            return max > res ? max : res;
+        }
+    }
+}
---
+++ UnitTest/_0003_Longest_substring_without_repeating_characters_Test.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0003.Longest_substring_without_repeating_characters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0003.Longest_substring_without_repeating_characters.Tests
+{
+    [TestClass()]
+    public class _0003_Longest_substring_without_repeating_characters_Test
+    {
+        _0003_Longest_substring_without_repeating_characters solution = new _0003_Longest_substring_without_repeating_characters();
+
+        [TestMethod()]
+        public void LengthOfLongestSubstring_Test1()
+        {
+            // Arrange
+            string s = "abcabcbb";
+            var expected = 3;
+
+            // Act
+            var actual = solution.LengthOfLongestSubstring(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void LengthOfLongestSubstring_Test2()
+        {
+            // Arrange
+            string s = "bbbbb";
+            var expected = 1;
+
+            // Act
+            var actual = solution.LengthOfLongestSubstring(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void LengthOfLongestSubstring_Test3()
+        {
+            // Arrange
+            string s = "pwwkew";
+            var expected = 3;
+
+            // Act
+            var actual = solution.LengthOfLongestSubstring(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void LengthOfLongestSubstring_Test4()
+        {
+            // Arrange
+            string s = "au";
+            var expected = 2;
+
+            // Act
+            var actual = solution.LengthOfLongestSubstring(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void LengthOfLongestSubstring_Test5()
+        {
+            // Arrange
+            string s = "dvdf";
+            var expected = 3;
+
+            // Act
+            var actual = solution.LengthOfLongestSubstring(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

dbb39636a3473044c0dffe2b317d9566c4264225
Riley Lin <rileylin.space@gmail.com>
2022-05-24 15:43:24 +0800

    (0290) new

---
+++ Solution/0290.Word-pattern/0290-Word-pattern.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0290.Word_pattern
+{
+    public class _0290_Word_pattern
+    {
+        public bool WordPattern(string pattern, string s)
+        {
+            string[] str = s.Split(' ');
+            if (str.Length != pattern.Length) return false;
+
+            Dictionary<char, string> dic = new Dictionary<char, string>();
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (dic.ContainsKey(pattern[i]))
+                {
+                    if (dic[pattern[i]] != str[i])
+                        return false;
+                }
+                else
+                {
+                    if (dic.ContainsValue(str[i]))
+                        return false;
+
+                    dic.Add(pattern[i], str[i]);
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ UnitTest/0290.Word-pattern/_0290_Word_pattern_Test.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0290.Word_pattern;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0290.Word_pattern.Tests
+{
+    [TestClass()]
+    public class _0290_Word_pattern_Test
+    {
+        _0290_Word_pattern solution = new _0290_Word_pattern();
+
+        [TestMethod()]
+        public void WordPattern_Test1()
+        {
+            // Arrange
+            string pattern = "abba";
+            string s = "dog cat cat dog";
+
+            // Act
+            var actual = solution.WordPattern(pattern, s);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void WordPattern_Test2()
+        {
+            // Arrange
+            string pattern = "abba";
+            string s = "dog cat cat fish";
+
+            // Act
+            var actual = solution.WordPattern(pattern, s);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void WordPattern_Test3()
+        {
+            // Arrange
+            string pattern = "aaaa";
+            string s = "dog cat cat dog";
+
+            // Act
+            var actual = solution.WordPattern(pattern, s);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void WordPattern_Test4()
+        {
+            // Arrange
+            string pattern = "abba";
+            string s = "dog dog dog dog";
+
+            // Act
+            var actual = solution.WordPattern(pattern, s);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void WordPattern_Test5()
+        {
+            // Arrange
+            string pattern = "aaa";
+            string s = "aa aa aa aa";
+
+            // Act
+            var actual = solution.WordPattern(pattern, s);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+    }
+}

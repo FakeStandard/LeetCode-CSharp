9e6306341d6551633ed9988520f01f82b60e5552
Riley Lin <rileylin.space@gmail.com>
2022-05-24 16:20:18 +0800

    (0876) new

---
+++ Solution/0876.Middle-of-the-linked-list/0876-Middle-of-the-linked-list.cs
@@ -0,0 +1,23 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0876.Middle_of_the_linked_list
+{
+    public class _0876_Middle_of_the_linked_list
+    {
+        public ListNode MiddleNode(ListNode head)
+        {
+            List<ListNode> list = new List<ListNode>();
+
+            while (head != null)
+            {
+                list.Add(head);
+                head = head.next;
+            }
+
+            return list[list.Count / 2];
+        }
+    }
+}
---
+++ UnitTest/_0876_Middle_of_the_linked_list_Test.cs
@@ -0,0 +1,44 @@
+﻿using Common;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0876.Middle_of_the_linked_list;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0876.Middle_of_the_linked_list.Tests
+{
+    [TestClass()]
+    public class _0876_Middle_of_the_linked_list_Test
+    {
+        _0876_Middle_of_the_linked_list solution = new _0876_Middle_of_the_linked_list();
+
+        [TestMethod()]
+        public void MiddleNode_Test1()
+        {
+            // Arrange
+            ListNode head = new ListNode().AddNode(new int[] { 1, 2, 3, 4, 5 });
+            var expected = new ListNode().AddNode(new int[] { 3, 4, 5 });
+
+            // Act
+            var actual = solution.MiddleNode(head);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void MiddleNode_Test2()
+        {
+            // Arrange
+            ListNode head = new ListNode().AddNode(new int[] { 1, 2, 3, 4, 5, 6 });
+            var expected = new ListNode().AddNode(new int[] { 4, 5, 6 });
+
+            // Act
+            var actual = solution.MiddleNode(head);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

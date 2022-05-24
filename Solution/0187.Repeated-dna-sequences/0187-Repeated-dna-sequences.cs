a4e34f0dcabe2fc2a2c5daab72ead300a8f7e927
Riley Lin <rileylin.space@gmail.com>
2022-05-24 11:20:17 +0800

    (0187) new

---
+++ Solution/0187.Repeated-dna-sequences/0187-Repeated-dna-sequences.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0187.Repeated_dna_sequences
+{
+    public class _0187_Repeated_dna_sequences
+    {
+        public IList<string> FindRepeatedDnaSequences(string s)
+        {
+            List<string> res = new List<string>();
+
+            if (s.Length <= 10) return res;
+
+            HashSet<string> set = new HashSet<string>();
+
+            string tmp = "";
+            for (int i = 0; i < s.Length - 9; i++)
+            {
+                tmp = s.Substring(i, 10);
+
+                if (!set.Add(tmp))
+                    if (!res.Contains(tmp))
+                        res.Add(tmp);
+            }
+
+            return res;
+        }
+    }
+}
---
+++ UnitTest/_0187_Repeated_dna_sequences_Test.cs
@@ -0,0 +1,57 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0187.Repeated_dna_sequences;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0187.Repeated_dna_sequences.Tests
+{
+    [TestClass()]
+    public class _0187_Repeated_dna_sequences_Test
+    {
+        _0187_Repeated_dna_sequences solution = new _0187_Repeated_dna_sequences();
+
+        [TestMethod()]
+        public void FindRepeatedDnaSequences_Test1()
+        {
+            // Arrange
+            string s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
+            var expected = new List<string>() { "AAAAACCCCC", "CCCCCAAAAA" };
+
+            // Act
+            var actual = solution.FindRepeatedDnaSequences(s);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void FindRepeatedDnaSequences_Test2()
+        {
+            // Arrange
+            string s = "AAAAAAAAAAAAA";
+            var expected = new List<string>() { "AAAAAAAAAA" };
+
+            // Act
+            var actual = solution.FindRepeatedDnaSequences(s);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void FindRepeatedDnaSequences_Test3()
+        {
+            // Arrange
+            string s = "AAAAAAAAAAA";
+            var expected = new List<string>() { "AAAAAAAAAA" };
+
+            // Act
+            var actual = solution.FindRepeatedDnaSequences(s);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

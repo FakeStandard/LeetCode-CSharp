87f9ca86c0b7b183960281b48347aee520cc2f4b
Riley Lin <rileylin.space@gmail.com>
2022-05-24 14:02:03 +0800

    (0043) new

---
+++ Solution/0043.Multiply-strings/0043-Multiply-strings.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0043.Multiply_strings
+{
+    public class _0043_Multiply_strings
+    {
+        public string Multiply(string num1, string num2)
+        {
+            int[] res = new int[num1.Length + num2.Length];
+            int digit = 0, carry = 0;
+            Stack<int> stack = new Stack<int>();
+
+            // Put the multiplication result into the corresponding column.
+            for (int i = num1.Length - 1; i >= 0; i--)
+                for (int j = num2.Length - 1; j >= 0; j--)
+                    res[i + j] += (num1[i] - '0') * (num2[j] - '0');
+
+            // Iterate over array and handle carry.
+            for (int k = res.Length - 1; k >= 0; k--)
+            {
+                if (res[k] == 0 && k == res.Length - 1) continue;
+
+                digit = res[k] % 10;
+                carry = res[k] / 10;
+
+                stack.Push(digit);
+
+                // add carry to next.
+                if (carry > 0)
+                {
+                    if (k == 0)
+                        stack.Push(carry);
+                    else
+                        res[k - 1] += carry;
+                }
+            }
+
+            // skip left zero
+            var str = String.Join("", stack).TrimStart('0');
+            if (str.Length == 0) return "0";
+
+            return str;
+        }
+    }
+}
---
+++ UnitTest/_0043_Multiply_strings_Test.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0043.Multiply_strings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0043.Multiply_strings.Tests
+{
+    [TestClass()]
+    public class _0043_Multiply_strings_Test
+    {
+        _0043_Multiply_strings solution = new _0043_Multiply_strings();
+
+        [TestMethod()]
+        public void Multiply_Test1()
+        {
+            // Arrange
+            string num1 = "2";
+            string num2 = "3";
+            string expected = "6";
+
+            // Act
+            var actual = solution.Multiply(num1, num2);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Multiply_Test2()
+        {
+            // Arrange
+            string num1 = "123";
+            string num2 = "456";
+            string expected = "56088";
+
+            // Act
+            var actual = solution.Multiply(num1, num2);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Multiply_Test3()
+        {
+            // Arrange
+            string num1 = "0";
+            string num2 = "0";
+            string expected = "0";
+
+            // Act
+            var actual = solution.Multiply(num1, num2);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Multiply_Test4()
+        {
+            // Arrange
+            string num1 = "9";
+            string num2 = "9";
+            string expected = "81";
+
+            // Act
+            var actual = solution.Multiply(num1, num2);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Multiply_Test5()
+        {
+            // Arrange
+            string num1 = "9133";
+            string num2 = "0";
+            string expected = "0";
+
+            // Act
+            var actual = solution.Multiply(num1, num2);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

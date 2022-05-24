0447833857d67f32cbe8c7514ae0b340db51f6b6
Riley Lin <rileylin.space@gmail.com>
2022-05-24 15:24:11 +0800

    (0049) new

---
+++ Solution/0049.Group-anagrams/0049-Group-anagrams.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0049.Group_anagrams
+{
+    public class _0049_Group_anagrams
+    {
+        public IList<IList<string>> GroupAnagrams(string[] strs)
+        {
+            IList<IList<string>> res = new List<IList<string>>();
+            if (strs.Length == 0) return res;
+
+            IList<int[]> list = new List<int[]>();
+            bool flag = false;
+
+            foreach (string str in strs)
+            {
+                flag = false;
+                int[] arr = new int[26];
+
+                for (int i = 0; i < str.Length; i++)
+                    arr[str[i] - 97]++;
+
+                // compare
+                for (int i = 0; i < list.Count; i++)
+                {
+                    if (AreEqual(list[i], arr))
+                    {
+                        res[i].Add(str);
+                        flag = true;
+                        break;
+                    }
+                }
+
+                if (!flag)
+                {
+                    list.Add(arr);
+                    res.Add(new List<string>() { str });
+                }
+            }
+
+            return res;
+        }
+
+        private bool AreEqual(int[] s1, int[] s2)
+        {
+            if (s1.Length != s2.Length) return false;
+
+            for (int i = 0; i < s1.Length; i++)
+                if (s1[i] != s2[i]) return false;
+
+            return true;
+        }
+    }
+}
---
+++ UnitTest/_0049_Group_anagrams_Test.cs
@@ -0,0 +1,68 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0049.Group_anagrams;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0049.Group_anagrams.Tests
+{
+    [TestClass()]
+    public class _0049_Group_anagrams_Test
+    {
+        _0049_Group_anagrams solution = new _0049_Group_anagrams();
+
+        [TestMethod()]
+        public void GroupAnagrams_Test1()
+        {
+            // Arrange
+            string[] strs = { "eat", "tea", "tan", "ate", "nat", "bat" };
+            var expected = new string[][]
+            {
+                new string[] { "bat" },
+                new string[] { "nat", "tan" },
+                new string[] { "ate", "eat", "tea" }
+            };
+
+            // Act
+            var actual = solution.GroupAnagrams(strs);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void GroupAnagrams_Test2()
+        {
+            // Arrange
+            string[] strs = { "" };
+            var expected = new string[][]
+            {
+                new string[] { "" },
+            };
+
+            // Act
+            var actual = solution.GroupAnagrams(strs);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void GroupAnagrams_Test3()
+        {
+            // Arrange
+            string[] strs = { "a" };
+            var expected = new string[][]
+            {
+                new string[] { "a" },
+            };
+
+            // Act
+            var actual = solution.GroupAnagrams(strs);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

1b0f8110a0165ec12a96c5a7671884ec520a8ea9
Riley Lin <rileylin.space@gmail.com>
2022-05-24 12:20:05 +0800

    (0005) new

---
+++ Solution/0005.Longest-palindromic-substring/0005-Longest-palindromic-substring.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0005.Longest_palindromic_substring
+{
+    public class _0005_Longest_palindromic_substring
+    {
+        public string LongestPalindrome(string s)
+        {
+            if (s.Length <= 1) return s;
+
+            int left = 0, odd, even, curr, max = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                // ex: 121
+                odd = Palindrome(s, i, i);
+
+                // ex: 1221
+                even = Palindrome(s, i, i + 1);
+
+                // compare
+                curr = (odd > even ? odd : even);
+
+                if (curr > max)
+                {
+                    max = curr;
+                    left = i - (max - 1) / 2;
+                }
+            }
+
+            return s.Substring(left, max);
+        }
+
+        private int Palindrome(string s, int left, int right)
+        {
+            while (left >= 0 && right < s.Length && s[left] == s[right])
+            {
+                left--;
+                right++;
+            }
+
+            return right - left - 1;
+        }
+    }
+}
---
+++ UnitTest/_0005_Longest_palindromic_substring_Test.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0005.Longest_palindromic_substring;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0005.Longest_palindromic_substring.Tests
+{
+    [TestClass()]
+    public class _0005_Longest_palindromic_substring_Test
+    {
+        _0005_Longest_palindromic_substring solution = new _0005_Longest_palindromic_substring();
+
+        [TestMethod()]
+        public void LongestPalindrome_Test1()
+        {
+            // Arrange
+            string s = "babad";
+            string expected = "bab";
+
+            // Act
+            var actual = solution.LongestPalindrome(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void LongestPalindrome_Test2()
+        {
+            // Arrange
+            string s = "cbbd";
+            string expected = "bb";
+
+            // Act
+            var actual = solution.LongestPalindrome(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

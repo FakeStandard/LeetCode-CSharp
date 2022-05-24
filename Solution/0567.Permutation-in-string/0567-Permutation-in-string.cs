9a2c93fdfea0325567d5db51d59c007224c0dba9
Riley Lin <rileylin.space@gmail.com>
2022-05-24 12:49:35 +0800

    (0567) new

---
+++ Solution/0567.Permutation-in-string/0567-Permutation-in-string.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Solution._0567.Permutation_in_string
+{
+    public class _0567_Permutation_in_string
+    {
+        public bool CheckInclusion(string s1, string s2)
+        {
+            if (s1.Length > s2.Length) return false;
+            if (s1.Length == 0 || s1 == s2) return true;
+
+            int[] arr1 = new int[26];
+            int[] arr2 = new int[26];
+
+            for (int i = 0; i < s1.Length; i++)
+            {
+                arr1[s1[i] - 97]++;
+                arr2[s2[i] - 97]++;
+            }
+
+            if (AreEqual(arr1, arr2)) return true;
+
+            for (int i = s1.Length; i < s2.Length; i++)
+            {
+                arr2[s2[i] - 97]++;
+                arr2[s2[i - s1.Length] - 97]--;
+
+                if (AreEqual(arr1, arr2)) return true;
+            }
+
+            return false;
+        }
+
+        private bool AreEqual(int[] s1, int[] s2)
+        {
+            for (int i = 0; i < s1.Length; i++)
+                if (s1[i] != s2[i])
+                    return false;
+
+            return true;
+        }
+    }
+}
---
+++ UnitTest/_0567_Permutation_in_string_Test.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0567.Permutation_in_string;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0567.Permutation_in_string.Tests
+{
+    [TestClass()]
+    public class _0567_Permutation_in_string_Test
+    {
+        _0567_Permutation_in_string solution = new _0567_Permutation_in_string();
+
+        [TestMethod()]
+        public void CheckInclusion_Test1()
+        {
+            // Arrange
+            string s1 = "ab";
+            string s2 = "eidbaooo";
+
+            // Act
+            var actual = solution.CheckInclusion(s1, s2);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void CheckInclusion_Test2()
+        {
+            // Arrange
+            string s1 = "ab";
+            string s2 = "eidboaoo";
+
+            // Act
+            var actual = solution.CheckInclusion(s1, s2);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void CheckInclusion_Test3()
+        {
+            // Arrange
+            string s1 = "a";
+            string s2 = "ab";
+
+            // Act
+            var actual = solution.CheckInclusion(s1, s2);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+    }
+}

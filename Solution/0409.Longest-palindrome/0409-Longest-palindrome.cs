f355f2d026ab7038498e261c1a42902126eee246
Riley Lin <rileylin.space@gmail.com>
2022-05-24 16:07:36 +0800

    (0409) new

---
+++ Solution/0409.Longest-palindrome/0409-Longest-palindrome.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0409.Longest_palindrome
+{
+    public class _0409_Longest_palindrome
+    {
+        public int LongestPalindrome(string s)
+        {
+            HashSet<char> set = new HashSet<char>();
+            int count = 0;
+
+            foreach (char c in s)
+            {
+                if (!set.Add(c))
+                {
+                    set.Remove(c);
+                    count += 2;
+                }
+            }
+
+            return set.Count > 0 ? ++count : count;
+
+            // used more time.
+            //Dictionary<char, int> dic = new Dictionary<char, int>();
+            //int count = 0;
+
+            //foreach (char c in s)
+            //{
+            //    if (dic.ContainsKey(c))
+            //        dic[c]++;
+            //    else
+            //        dic.Add(c, 1);
+            //}
+
+            //bool odd = false;
+
+            //foreach (var pair in dic)
+            //{
+            //    if (pair.Value >= 2)
+            //    {
+            //        if (pair.Value % 2 == 0)
+            //            count += pair.Value;
+            //        else
+            //        {
+            //            count += pair.Value - 1;
+            //            odd = true;
+            //        }
+            //    }
+            //    else odd = true;
+            //}
+
+            //if (odd) count++;
+
+            //return count;
+        }
+    }
+}
---
+++ UnitTest/_0409_Longest_palindrome_Test.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0409.Longest_palindrome;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0409.Longest_palindrome.Tests
+{
+    [TestClass()]
+    public class _0409_Longest_palindrome_Test
+    {
+        _0409_Longest_palindrome solution = new _0409_Longest_palindrome();
+
+        [TestMethod()]
+        public void LongestPalindrome_Test1()
+        {
+            // Arrange
+            string s = "abccccdd";
+            var expected = 7;
+
+            // Act
+            var actual = solution.LongestPalindrome(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void LongestPalindrome_Test2()
+        {
+            // Arrange
+            string s = "a";
+            var expected = 1;
+
+            // Act
+            var actual = solution.LongestPalindrome(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void LongestPalindrome_Test3()
+        {
+            // Arrange
+            string s = "bb";
+            var expected = 2;
+
+            // Act
+            var actual = solution.LongestPalindrome(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

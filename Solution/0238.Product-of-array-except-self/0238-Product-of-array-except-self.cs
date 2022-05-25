bdb7c36a209d8a2830581567931f4be4ac887ad9
Riley Lin <rileylin.space@gmail.com>
2022-05-25 13:03:53 +0800

    (0238) new

---
+++ Solution/0238.Product-of-array-except-self/0238-Product-of-array-except-self.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0238.Product_of_array_except_self
+{
+    public class _0238_Product_of_array_except_self
+    {
+        public int[] ProductExceptSelf(int[] nums)
+        {
+            int[] res = new int[nums.Length];
+            int temp = 1;
+
+            // product from left to right
+            for (int i = 0; i < nums.Length; i++)
+            {
+                res[i] = temp;
+                temp *= nums[i];
+            }
+
+            temp = 1;
+
+            // product from right to left
+            for (int j = res.Length - 1; j >= 0; j--)
+            {
+                res[j] *= temp;
+                temp *= nums[j];
+            }
+
+            return res;
+        }
+    }
+}
---
+++ UnitTest/_0238_Product_of_array_except_self_Test.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0238.Product_of_array_except_self;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0238.Product_of_array_except_self.Tests
+{
+    [TestClass()]
+    public class _0238_Product_of_array_except_self_Test
+    {
+        _0238_Product_of_array_except_self solution = new _0238_Product_of_array_except_self();
+
+        [TestMethod()]
+        public void ProductExceptSelf_Test1()
+        {
+            // Arrange
+            int[] nums = { 1, 2, 3, 4 };
+            int[] expected = { 24, 12, 8, 6 };
+
+            // Act
+            var actual = solution.ProductExceptSelf(nums);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void ProductExceptSelf_Test2()
+        {
+            // Arrange
+            int[] nums = { -1, 1, 0, -3, 3 };
+            int[] expected = { 0, 0, 9, 0, 0 };
+
+            // Act
+            var actual = solution.ProductExceptSelf(nums);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

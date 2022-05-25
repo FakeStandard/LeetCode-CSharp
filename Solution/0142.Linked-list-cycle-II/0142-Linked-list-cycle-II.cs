5daa3b168cd9424fb4dfcd9ecc654fcc4a600535
Riley Lin <rileylin.space@gmail.com>
2022-05-25 20:22:45 +0800

    (0142) new

---
+++ Solution/0142.Linked-list-cycle-II/0142-Linked-list-cycle-II.cs
@@ -0,0 +1,27 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0142.Linked_list_cycle_II
+{
+    public class _0142_Linked_list_cycle_II
+    {
+        public ListNode DetectCycle(ListNode head)
+        {
+            HashSet<ListNode> set = new HashSet<ListNode>();
+
+            while (head != null)
+            {
+                if (set.Contains(head))
+                    return head;
+                else
+                    set.Add(head);
+
+                head = head.next;
+            }
+
+            return null;
+        }
+    }
+}

1cc2f59793e8c151f41d4127756803b7396321b8
Riley Lin <rileylin.space@gmail.com>
2022-05-25 22:40:47 +0800

    (0198) pending

---
+++ Solution/0198.House-robber/0198-House-robbercs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0198.House_robber
+{
+    public class _0198_House_robbercs
+    {
+        public int Rob(int[] nums)
+        {
+            int odd = 0, even = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (i % 2 == 0)
+                    odd += nums[i];
+                else
+                    even += nums[i];
+            }
+            
+            return odd > even ? odd : even;
+        }
+    }
+}
---
+++ UnitTest/_0198_House_robbercs_Test.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0198.House_robber;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0198.House_robber.Tests
+{
+    [TestClass()]
+    public class _0198_House_robbercs_Test
+    {
+        _0198_House_robbercs solution = new _0198_House_robbercs();
+
+        [TestMethod()]
+        public void Rob_Test1()
+        {
+            // Arrange
+            int[] nums = { 1, 2, 3, 1 };
+            int expected = 4;
+
+            // Act
+            var actual = solution.Rob(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Rob_Test2()
+        {
+            // Arrange
+            int[] nums = { 2, 7, 9, 3, 1 };
+            int expected = 12;
+
+            // Act
+            var actual = solution.Rob(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Rob_Test3()
+        {
+            // Arrange
+            int[] nums = { 2, 1, 1, 2 };
+            int expected = 4;
+
+            // Act
+            var actual = solution.Rob(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

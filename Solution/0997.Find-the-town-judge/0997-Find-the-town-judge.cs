a5f093eaa3eeccb67160f97c748b9a2e91699adc
Riley Lin <rileylin.space@gmail.com>
2022-05-25 21:41:13 +0800

    (0997) new

---
+++ Solution/0997.Find-the-town-judge/0997-Find-the-town-judge.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0997.Find_the_town_judge
+{
+    public class _0997_Find_the_town_judge
+    {
+        public int FindJudge(int n, int[][] trust)
+        {
+            // Use only one array.
+            int[] res = new int[n + 1];
+
+            foreach (int[] arr in trust)
+            {
+                res[arr[1]]++; // trusted
+                res[arr[0]]--; // trusting
+            }
+
+            for (int i = 1; i <= n; i++)
+                if (res[i] == n - 1)
+                    return i;
+
+            return -1;
+
+            //Use dictionary, but use too much memory.
+            //if (n == 1) return 1;
+
+            //Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            //foreach (int[] arr in trust)
+            //{
+            //    if (dic.ContainsKey(arr[1]))
+            //        dic[arr[1]]++;
+            //    else
+            //        dic.Add(arr[1], 1);
+
+            //    if (dic.ContainsKey(arr[0]))
+            //        dic[arr[0]]--;
+            //    else
+            //        dic.Add(arr[0], -1);
+            //}
+
+            //foreach (KeyValuePair<int, int> pair in dic)
+            //{
+            //    if (pair.Value == n - 1)
+            //        return pair.Key;
+            //}
+
+            //return -1;
+        }
+    }
+}
---
+++ UnitTest/_0997_Find_the_town_judge_Test.cs
@@ -0,0 +1,123 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0997.Find_the_town_judge;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0997.Find_the_town_judge.Tests
+{
+    [TestClass()]
+    public class _0997_Find_the_town_judge_Test
+    {
+        _0997_Find_the_town_judge solution = new _0997_Find_the_town_judge();
+
+        [TestMethod()]
+        public void FindJudge_Test1()
+        {
+            // Arrange
+            int n = 2;
+            int[][] trust =
+            {
+                new int[] { 1, 2 }
+            };
+            var expected = 2;
+
+            // Act
+            var actual = solution.FindJudge(n, trust);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FindJudge_Test2()
+        {
+            // Arrange
+            int n = 3;
+            int[][] trust =
+            {
+                new int[] { 1, 3 },
+                new int[] { 2, 3 }
+            };
+            var expected = 3;
+
+            // Act
+            var actual = solution.FindJudge(n, trust);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FindJudge_Test3()
+        {
+            // Arrange
+            int n = 3;
+            int[][] trust =
+            {
+                new int[] { 1, 3 },
+                new int[] { 2, 3 },
+                new int[] { 3, 1 }
+            };
+            var expected = -1;
+
+            // Act
+            var actual = solution.FindJudge(n, trust);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FindJudge_Test4()
+        {
+            // Arrange
+            int n = 4;
+            int[][] trust =
+            {
+                new int[] { 1, 3 },
+                new int[] { 1, 4 },
+                new int[] { 2, 3 },
+                new int[] { 2, 4 },
+                new int[] { 4, 3 }
+            };
+            var expected = 3;
+
+            // Act
+            var actual = solution.FindJudge(n, trust);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FindJudge_Test5()
+        {
+            // Arrange
+            int n = 2;
+            int[][] trust = { };
+            var expected = -1;
+
+            // Act
+            var actual = solution.FindJudge(n, trust);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FindJudge_Test6()
+        {
+            // Arrange
+            int n = 1;
+            int[][] trust = { };
+            var expected = 1;
+
+            // Act
+            var actual = solution.FindJudge(n, trust);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

cf3acd05d73c25826bcb3f535a6625ed90791ec5
Riley Lin <rileylin.space@gmail.com>
2022-05-25 22:28:39 +0800

    (0120) pending

---
+++ Solution/0120.Triangle/0120-Triangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0120.Triangle
+{
+    public class _0120_Triangle
+    {
+        public int MinimumTotal(IList<IList<int>> triangle)
+        {
+            int sum = triangle[0][0];
+            int index = 0;
+
+            for (int i = 1; i < triangle.Count; i++)
+            {
+                if (triangle[i][index] < triangle[i][index + 1])
+                    sum += triangle[i][index];
+                else
+                {
+                    sum += triangle[i][index + 1];
+                    index++;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ UnitTest/_0120_Triangle_Test.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0120.Triangle;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0120.Triangle.Tests
+{
+    [TestClass()]
+    public class _0120_Triangle_Test
+    {
+        _0120_Triangle solution = new _0120_Triangle();
+
+        [TestMethod()]
+        public void MinimumTotal_Test1()
+        {
+            // Arrange
+            int[][] triangle ={
+                new int[] { 2 },
+                new int[] { 3, 4 },
+                new int[] { 6,5,7 },
+                new int[] { 4,1,8,3 },
+            };
+
+            int expected = 11;
+
+            // Act
+            var actual = solution.MinimumTotal(triangle);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MinimumTotal_Test2()
+        {
+            // Arrange
+            int[][] triangle ={
+                new int[] { -10 },
+            };
+
+            int expected = -10;
+
+            // Act
+            var actual = solution.MinimumTotal(triangle);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [TestMethod()]
+        public void MinimumTotal_Test3()
+        {
+            // Arrange
+            int[][] triangle ={
+                new int[] { -1 },
+                new int[] { 2, 3 },
+                new int[] { 1, -1, 3 },
+            };
+
+            int expected = -1;
+
+            // Act
+            var actual = solution.MinimumTotal(triangle);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

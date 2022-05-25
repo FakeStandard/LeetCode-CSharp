a8e7a46cd99473d10e9e86c2fc06bfb9e914c8ad
Riley Lin <rileylin.space@gmail.com>
2022-05-25 22:09:01 +0800

    (0160) new

---
+++ Solution/0160.Intersection-of-two-linked-lists/0160-Intersection-of-two-linked-lists.cs
@@ -0,0 +1,26 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0160.Intersection_of_two_linked_lists
+{
+    public class _0160_Intersection_of_two_linked_lists
+    {
+        public ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            if (headA == null || headB == null) return null;
+
+            ListNode a = headA;
+            ListNode b = headB;
+
+            while (a != b)
+            {
+                a = a == null ? headB : a.next;
+                b = b == null ? headA : b.next;
+            }
+
+            return a;
+        }
+    }
+}

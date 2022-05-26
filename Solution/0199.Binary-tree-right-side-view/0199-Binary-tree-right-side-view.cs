263e0fc36b74fb7f85088b9ea3fb7b1f80b88ba2
Riley Lin <rileylin.space@gmail.com>
2022-05-26 14:17:35 +0800

    (0199) pending

---
+++ Solution/0199.Binary-tree-right-side-view/0199-Binary-tree-right-side-view.cs
@@ -0,0 +1,30 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0199.Binary_tree_right_side_view
+{
+    public class _0199_Binary_tree_right_side_view
+    {
+        private IList<int> _res = new List<int>();
+        public IList<int> RightSideView(TreeNode root)
+        {
+            travel(root);
+
+            return _res;
+        }
+
+        private void travel(TreeNode node)
+        {
+            if (node == null) return;
+
+            _res.Add(node.val);
+
+            if (node.right != null)
+                travel(node.right);
+            else
+                travel(node.left);
+        }
+    }
+}

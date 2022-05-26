a776fbebc5e1b608d215560b12fdeea417eafe61
Riley Lin <rileylin.space@gmail.com>
2022-05-26 13:41:11 +0800

    (0841) new

---
+++ Solution/0841.Keys-and-rooms/0841-Keys-and-rooms.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0841.Keys_and_rooms
+{
+    public class _0841_Keys_and_rooms
+    {
+        public bool CanVisitAllRooms(IList<IList<int>> rooms)
+        {
+            if (rooms.Count <= 1) return true;
+
+            // record whether the room is open.
+            Dictionary<int, bool> dic = new Dictionary<int, bool>();
+
+            for (int i = 0; i < rooms.Count; i++)
+                dic.Add(i, false);
+
+            Queue<int> keys = new Queue<int>();
+            keys.Enqueue(0);
+
+            while (keys.Count > 0)
+            {
+                // open the room with key.
+                int key = keys.Dequeue();
+
+                if (dic[key]) continue;
+
+                // pick up the key.
+                for (int i = 0; i < rooms[key].Count; i++)
+                    keys.Enqueue(rooms[key][i]);
+
+                // record the room is opened.
+                dic[key] = true;
+
+                // check whether all room is open.
+                if (dic.ContainsValue(false)) continue;
+                else return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ UnitTest/_0841_Keys_and_rooms_Test.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0841.Keys_and_rooms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0841.Keys_and_rooms.Tests
+{
+    [TestClass()]
+    public class _0841_Keys_and_rooms_Test
+    {
+        _0841_Keys_and_rooms solution = new _0841_Keys_and_rooms();
+
+        [TestMethod()]
+        public void CanVisitAllRooms_Test1()
+        {
+            // Arrange
+            int[][] rooms =
+             {
+                new int[] { 1 },
+                new int[] { 2 },
+                new int[] { 3 },
+                new int[] {  },
+            };
+
+            // Act
+            var actual = solution.CanVisitAllRooms(rooms);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void CanVisitAllRooms_Test2()
+        {
+            // Arrange
+            int[][] rooms =
+             {
+                new int[] { 1, 3 },
+                new int[] { 3, 0, 1 },
+                new int[] { 2 },
+                new int[] { 0 },
+            };
+
+            // Act
+            var actual = solution.CanVisitAllRooms(rooms);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void CanVisitAllRooms_Test3()
+        {
+            // Arrange
+            int[][] rooms =
+             {
+                new int[] { 6, 7, 8 },
+                new int[] { 5, 4, 9 },
+                new int[] {  },
+                new int[] { 8 },
+                new int[] { 4 },
+                new int[] {  },
+                new int[] { 1, 9, 2, 3 },
+                new int[] { 7 },
+                new int[] { 6, 5 },
+                new int[] { 2, 3, 1 },
+            };
+
+            // Act
+            var actual = solution.CanVisitAllRooms(rooms);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+    }
+}

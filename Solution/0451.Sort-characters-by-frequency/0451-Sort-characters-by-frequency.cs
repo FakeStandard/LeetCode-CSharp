280a87d41015cc3ce9c8d17ccbe4eb4267197159
Riley Lin <rileylin.space@gmail.com>
2022-05-26 12:23:30 +0800

    (0451) new

---
+++ Solution/0451.Sort-characters-by-frequency/0451-Sort-characters-by-frequency.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0451.Sort_characters_by_frequency
+{
+    public class _0451_Sort_characters_by_frequency
+    {
+        public string FrequencySort(string s)
+        {
+            Dictionary<char, int> dic = new Dictionary<char, int>();
+
+            foreach (char c in s)
+            {
+                //if (dic.ContainsKey(c))
+                //    dic[c]++;
+                //else
+                //    dic.Add(c, 1);
+
+                //if (!dic.TryAdd(c, 1)) dic[c]++;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            int max = 0;
+            char ch = '0';
+
+            while (dic.Count != 0)
+            {
+                foreach (KeyValuePair<char, int> pair in dic)
+                {
+                    if (pair.Value > max)
+                    {
+                        max = pair.Value;
+                        ch = pair.Key;
+                    }
+                }
+
+                dic.Remove(ch);
+
+                for (int i = 0; i < max; i++)
+                    sb.Append(ch);
+
+                max = 0;
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ UnitTest/_0451_Sort_characters_by_frequency_Test.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0451.Sort_characters_by_frequency;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0451.Sort_characters_by_frequency.Tests
+{
+    [TestClass()]
+    public class _0451_Sort_characters_by_frequency_Test
+    {
+        _0451_Sort_characters_by_frequency solution = new _0451_Sort_characters_by_frequency();
+
+        [TestMethod()]
+        public void FrequencySort_Test1()
+        {
+            // Arrange
+            string s = "tree";
+            string expected = "eetr"; // eert is also correct.
+
+            // Act
+            var actual = solution.FrequencySort(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FrequencySort_Test2()
+        {
+            // Arrange
+            string s = "cccaaa";
+            string expected = "cccaaa"; // aaaccc is also correct.
+
+            // Act
+            var actual = solution.FrequencySort(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FrequencySort_Test3()
+        {
+            // Arrange
+            string s = "Aabb";
+            string expected = "bbAa";
+
+            // Act
+            var actual = solution.FrequencySort(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FrequencySort_Test4()
+        {
+            // Arrange
+            string s = "raaeaedere";
+            string expected = "eeeeaaarrd";
+
+            // Act
+            var actual = solution.FrequencySort(s);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

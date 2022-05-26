4fc42e247158ffc27859281d3373468d73c4d620
Riley Lin <rileylin.space@gmail.com>
2022-05-26 14:20:39 +0800

    (0105) pending

---
+++ Solution/0105.Construct-binary-tree-from-preorder-and-inorder-traversal/0105-Construct-binary-tree-from-preorder-and-inorder-traversal.cs
@@ -0,0 +1,15 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0105.Construct_binary_tree_from_preorder_and_inorder_traversal
+{
+    public class _0105_Construct_binary_tree_from_preorder_and_inorder_traversal
+    {
+        public TreeNode BuildTree(int[] preorder, int[] inorder)
+        {
+
+        }
+    }
+}

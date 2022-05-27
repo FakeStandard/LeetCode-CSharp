f3d29ea3c7e383d97d35c90bc30cee1dcf9a0ee2
Riley Lin <rileylin.space@gmail.com>
2022-05-27 14:56:37 +0800

    (0077) new

---
+++ Solution/0077.Combinations/0077-Combinations.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0077.Combinations
+{
+    public class _0077_Combinations
+    {
+        private IList<IList<int>> res = new List<IList<int>>();
+
+        public IList<IList<int>> Combine(int n, int k)
+        {
+            var temp = new List<int>();
+
+            recursive(n, k, temp, 1);
+
+            return res;
+        }
+
+        private void recursive(int n, int k, IList<int> temp, int level)
+        {
+            if (temp.Count == k)
+                res.Add(new List<int>(temp));
+            else
+            {
+                for (int i = level; i <= n; i++)
+                {
+                    temp.Add(i);
+
+                    recursive(n, k, temp, i + 1);
+
+                    temp.RemoveAt(temp.Count - 1);
+                }
+            }
+        }
+    }
+}
---
+++ UnitTest/_0077_Combinations_Test.cs
@@ -0,0 +1,56 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0077.Combinations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0077.Combinations.Tests
+{
+    [TestClass()]
+    public class _0077_Combinations_Test
+    {
+        _0077_Combinations solution = new _0077_Combinations();
+
+        [TestMethod()]
+        public void Combine_Test1()
+        {
+            // Arrange
+            int n = 4;
+            int k = 2;
+            var expected = new int[][]
+            {
+                new int[]{ 1, 2 },
+                new int[]{ 1, 3 },
+                new int[]{ 1, 4 },
+                new int[]{ 2, 3 },
+                new int[]{ 2, 4 },
+                new int[]{ 3, 4 },
+            };
+
+            // Act
+            var actual = solution.Combine(n, k);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void Combine_Test2()
+        {
+            // Arrange
+            int n = 1;
+            int k = 1;
+            var expected = new int[][]
+            {
+                new int[]{ 1 },
+            };
+
+            // Act
+            var actual = solution.Combine(n, k);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

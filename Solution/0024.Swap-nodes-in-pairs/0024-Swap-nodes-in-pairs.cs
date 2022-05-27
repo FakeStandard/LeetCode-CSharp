780453fe6dcf32b83951488fedcab107af9da3b1
Riley Lin <rileylin.space@gmail.com>
2022-05-27 13:51:57 +0800

    (0024) new

---
+++ Solution/0024.Swap-nodes-in-pairs/0024-Swap-nodes-in-pairs.cs
@@ -0,0 +1,33 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0024.Swap_nodes_in_pairs
+{
+    public class _0024_Swap_nodes_in_pairs
+    {
+        public ListNode SwapPairs(ListNode head)
+        {
+            if (head == null) return null;
+
+            ListNode res = new ListNode();
+            res.next = head;
+            ListNode prev = res;
+
+            while (prev.next != null && prev.next.next != null)
+            {
+                ListNode l1 = prev.next;
+                ListNode l2 = prev.next.next;
+
+                l1.next = l2.next;
+                l2.next = l1;
+
+                prev.next = l2;
+                prev = l1;
+            }
+
+            return res.next;
+        }
+    }
+}
---
+++ UnitTest/_0024_Swap_nodes_in_pairs_Test.cs
@@ -0,0 +1,58 @@
+﻿using Common;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0024.Swap_nodes_in_pairs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0024.Swap_nodes_in_pairs.Tests
+{
+    [TestClass()]
+    public class _0024_Swap_nodes_in_pairs_Test
+    {
+        _0024_Swap_nodes_in_pairs solution = new _0024_Swap_nodes_in_pairs();
+
+        [TestMethod()]
+        public void SwapPairs_Test1()
+        {
+            // Arrange
+            ListNode head = new ListNode().AddNode(new int[] { 1, 2, 3, 4 });
+            var expected = new ListNode().AddNode(new int[] { 2, 1, 4, 3 });
+
+            // Act
+            var actual = solution.SwapPairs(head);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void SwapPairs_Test2()
+        {
+            // Arrange
+            ListNode head = new ListNode().AddNode(new int[] { });
+            var expected = new ListNode().AddNode(new int[] { });
+
+            // Act
+            var actual = solution.SwapPairs(head);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void SwapPairs_Test3()
+        {
+            // Arrange
+            ListNode head = new ListNode().AddNode(new int[] { 1 });
+            var expected = new ListNode().AddNode(new int[] { 1 });
+
+            // Act
+            var actual = solution.SwapPairs(head);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

4f52df3ec9ba55bed8fff9faf0fe049eee9bb062
Riley Lin <rileylin.space@gmail.com>
2022-05-27 15:38:04 +0800

    (0046) pending

---
+++ Solution/0046.Permutations/0046-Permutations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0046.Permutations
+{
+    public class _0046_Permutations
+    {
+        IList<IList<int>> res = new List<IList<int>>();
+
+        public IList<IList<int>> Permute(int[] nums)
+        {
+            var temp = new List<int>();
+
+            //recursive(nums, temp);
+
+            return res;
+        }
+
+        //private void recursive(temp)
+        //{
+
+        //}
+    }
+}
---
+++ UnitTest/_0046_Permutations_Test.cs
@@ -0,0 +1,72 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0046.Permutations;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0046.Permutations.Tests
+{
+    [TestClass()]
+    public class _0046_Permutations_Test
+    {
+        _0046_Permutations solution = new _0046_Permutations();
+
+        [TestMethod()]
+        public void Permute_Test1()
+        {
+            // Arrange
+            int[] nums = { 1, 2, 3 };
+            var expected = new int[][]
+            {
+                new int[] { 1, 2, 3 },
+                new int[] { 1, 3, 2 },
+                new int[] { 2, 1, 3 },
+                new int[] { 2, 3, 1 },
+                new int[] { 3, 1, 2 },
+                new int[] { 3, 2, 1 },
+            };
+
+            // Act
+            var actual = solution.Permute(nums);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void Permute_Test2()
+        {
+            // Arrange
+            int[] nums = { 0, 1 };
+            var expected = new int[][]
+            {
+                new int[] { 0, 1 },
+                new int[] { 1, 0 },
+            };
+
+            // Act
+            var actual = solution.Permute(nums);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void Permute_Test3()
+        {
+            // Arrange
+            int[] nums = { 1 };
+            var expected = new int[][]
+            {
+                new int[] { 1 },
+            };
+
+            // Act
+            var actual = solution.Permute(nums);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

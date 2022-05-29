4464b8daf7c0027bc64022e9b6fc3b38c94e8c3e
Riley Lin <rileylin.space@gmail.com>
2022-05-29 16:14:26 +0800

    (0784) pending

---
+++ Solution/0784.Letter-case-permutation/0784-Letter-case-permutation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0784.Letter_case_permutation
+{
+    public class _0784_Letter_case_permutation
+    {
+        IList<string> res = new List<string>();
+
+        public IList<string> LetterCasePermutation(string s)
+        {
+            if (s == null || s.Length == 0) return res;
+
+            backtrack(s, 0);
+
+            return res;
+        }
+
+        private void backtrack(string str, int index)
+        {
+            
+        }
+    }
+}
---
+++ UnitTest/_0784_Letter_case_permutation_Test.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0784.Letter_case_permutation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0784.Letter_case_permutation.Tests
+{
+    [TestClass()]
+    public class _0784_Letter_case_permutation_Test
+    {
+        _0784_Letter_case_permutation solution = new _0784_Letter_case_permutation();
+
+        [TestMethod()]
+        public void LetterCasePermutation_Test1()
+        {
+            // Arrange
+            string s = "a1b2";
+            var expected = new string[] { "a1b2", "a1B2", "A1b2", "A1B2" };
+
+            // Act
+            var actual = solution.LetterCasePermutation(s);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void LetterCasePermutation_Test2()
+        {
+            // Arrange
+            string s = "3z4";
+            var expected = new string[] { "3z4", "3Z4" };
+
+            // Act
+            var actual = solution.LetterCasePermutation(s);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

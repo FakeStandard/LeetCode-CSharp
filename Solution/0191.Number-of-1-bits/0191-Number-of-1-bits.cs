ead441f1989babe5a6d21fbd9201a783ad8e6dfd
Riley Lin <rileylin.space@gmail.com>
2022-05-29 15:25:46 +0800

    (0191) new

---
+++ Solution/0191.Number-of-1-bits/0191-Number-of-1-bits.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0191.Number_of_1_bits
+{
+    public class _0191_Number_of_1_bits
+    {
+        public int HammingWeight(uint n)
+        {
+            int count = 0;
+
+            while (n != 0)
+            {
+                uint curr = n;
+
+                // 1&1 = 1
+                if ((curr & 1) == 1)
+                    count++;
+
+                n >>= 1;
+            }
+
+            return count;
+        }
+    }
+}

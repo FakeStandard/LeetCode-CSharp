bdb245a634cdf14be969b0c81d827b1a63991c36
Riley Lin <rileylin.space@gmail.com>
2022-05-29 15:42:10 +0800

    (0190) new

---
+++ Solution/0190.Reverse-bits/0190-Reverse-bits.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0190.Reverse_bits
+{
+    public class _0190_Reverse_bits
+    {
+        public uint reverseBits(uint n)
+        {
+            uint res = 0;
+            int bits = 31;
+
+            while (n != 0)
+            {
+                // 00000010100101000001111010011100
+                res += (n & 1) << bits;
+                bits--;
+                n >>= 1;
+            }
+
+            return res;
+        }
+    }
+}

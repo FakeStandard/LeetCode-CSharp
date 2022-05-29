c0f7b63020021e767f76f1fdebbdccf27811fee3
Riley Lin <rileylin.space@gmail.com>
2022-05-29 16:38:23 +0800

    (0994) pending

---
+++ Solution/0994.Rotting-oranges/0994-Rotting-oranges.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0994.Rotting_oranges
+{
+    public class _0994_Rotting_oranges
+    {
+        public int OrangesRotting(int[][] grid)
+        {
+            if (grid == null || grid.Length == 0) return -1;
+
+            int minute = 0;
+
+            // 2,1,1
+            // 0,1,1
+            // 1,0,1
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (grid[i][j] == 0) continue;
+
+                    if (grid[i][j] == 2)
+                    {
+                        minute++;
+
+                        if (i + 1 < grid.Length)
+                            if (grid[i + 1][j] == 1)
+                                grid[i + 1][j] = 2;
+
+                        if (j + 1 < grid[i].Length)
+                            if (grid[i][j + 1] == 1)
+                                grid[i][j + 1] = 2;
+                    }
+                }
+            }
+
+            // check the 1
+            foreach (int[] arr in grid)
+                foreach (int num in arr)
+                    if (num == 1) return -1;
+
+            return minute;
+        }
+    }
+}
---
+++ UnitTest/_0994_Rotting_oranges_Test.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0994.Rotting_oranges;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0994.Rotting_oranges.Tests
+{
+    [TestClass()]
+    public class _0994_Rotting_oranges_Test
+    {
+        _0994_Rotting_oranges solution = new _0994_Rotting_oranges();
+
+        [TestMethod()]
+        public void OrangesRotting_Test1()
+        {
+            // Arrange
+            int[][] grid = new int[][]
+            {
+                new int[] { 2, 1, 1 },
+                new int[] { 1, 1, 0 },
+                new int[] { 0, 1, 1 },
+            };
+            var expected = 4;
+
+            // Act
+            var actual = solution.OrangesRotting(grid);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void OrangesRotting_Test2()
+        {
+            // Arrange
+            int[][] grid = new int[][]
+            {
+                new int[] { 2, 1, 1 },
+                new int[] { 0, 1, 1 },
+                new int[] { 1, 0, 1 },
+            };
+            var expected = -1;
+
+            // Act
+            var actual = solution.OrangesRotting(grid);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

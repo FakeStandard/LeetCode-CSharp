731291280006dfcf18752ce6804e690819f955db
Riley Lin <rileylin.space@gmail.com>
2022-05-30 15:14:38 +0800

    (0733) done

---
+++ Solution/0733.Flood-fill/0733-Flood-fill.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0733.Flood_fill
+{
+    public class _0733_Flood_fill
+    {
+        public int[][] FloodFill(int[][] image, int sr, int sc, int newColor)
+        {
+            if (image[sr][sc] != newColor)
+                DFS(sr, sc, image, image[sr][sc], newColor);
+
+            return image;
+        }
+
+        private void DFS(int row, int col, int[][] image, int oldColor, int newColor)
+        {
+            if (image[row][col] != oldColor) return;
+
+            // change color to new
+            image[row][col] = newColor;
+
+            if (row > 0)
+                DFS(row - 1, col, image, oldColor, newColor); // top
+
+            if (row < image.Length - 1)
+                DFS(row + 1, col, image, oldColor, newColor); // bottom
+
+            if (col > 0)
+                DFS(row, col - 1, image, oldColor, newColor); // left
+
+            if (col < image[0].Length - 1)
+                DFS(row, col + 1, image, oldColor, newColor); // right
+        }
+    }
+}
---
+++ UnitTest/_0733_Flood_fill_Test.cs
@@ -0,0 +1,69 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0733.Flood_fill;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0733.Flood_fill.Tests
+{
+    [TestClass()]
+    public class _0733_Flood_fill_Test
+    {
+        _0733_Flood_fill solution = new _0733_Flood_fill();
+
+        [TestMethod()]
+        public void FloodFill_Test1()
+        {
+            // Arrange
+            int[][] image = new int[][]
+            {
+                new int[] { 1, 1, 1 },
+                new int[] { 1, 1, 0 },
+                new int[] { 1, 0, 1 },
+            };
+
+            int sr = 1;
+            int sc = 1;
+            int newColor = 2;
+            var expected = new int[][]
+            {
+                new int[] { 2, 2, 2 },
+                new int[] { 2, 2, 0 },
+                new int[] { 2, 0, 1 },
+            };
+
+            // Act
+            var actual = solution.FloodFill(image, sr, sc, newColor);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void FloodFill_Test2()
+        {
+            // Arrange
+            int[][] image = new int[][]
+            {
+                new int[] { 0, 0, 0 },
+                new int[] { 0, 0, 0 },
+            };
+
+            int sr = 0;
+            int sc = 0;
+            int newColor = 2;
+            var expected = new int[][]
+            {
+                new int[] { 2, 2, 2 },
+                new int[] { 2, 2, 2 },
+            };
+
+            // Act
+            var actual = solution.FloodFill(image, sr, sc, newColor);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

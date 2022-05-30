1cd2e564590e2a65074cbd2b3f607d2e1c1fc2a7
Riley Lin <rileylin.space@gmail.com>
2022-05-30 14:52:59 +0800

    Update 0784-Letter-case-permutation.cs

--- Solution/0784.Letter-case-permutation/0784-Letter-case-permutation.cs
+++ Solution/0784.Letter-case-permutation/0784-Letter-case-permutation.cs
@@ -12,14 +12,33 @@ namespace Solution._0784.Letter_case_permutation
         {
             if (s == null || s.Length == 0) return res;
 
-            backtrack(s, 0);
+            res.Add(s);
+            backtrack(s.ToCharArray(), 0);
 
             return res;
         }
 
-        private void backtrack(string str, int index)
+        private void backtrack(char[] str, int index)
         {
-            
+            if (index == str.Length) return;
+
+            bool lower = str[index] >= 'a' && str[index] <= 'z';
+            bool upper = str[index] >= 'A' && str[index] <= 'Z';
+
+            // is letter
+            if (lower || upper)
+            {
+                var temp = str[index];
+                backtrack(str, index + 1);
+
+                str[index] = lower ? (char)(str[index] - 32) : (char)(str[index] + 32);
+                backtrack(str, index + 1);
+
+                res.Add(new string(str));
+                str[index] = temp;
+            }
+            else // is digit
+                backtrack(str, index + 1);
         }
     }
 }

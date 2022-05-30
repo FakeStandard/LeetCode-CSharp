97a7fa4facd7bb70725e66d8ea14f1cbba7f6597
Riley Lin <rileylin.space@gmail.com>
2022-05-30 23:13:57 +0800

    (0695) new

---
+++ Solution/0695.Max-area-of-island/0695-Max-area-of-island.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0695.Max_area_of_island
+{
+    public class _0695_Max_area_of_island
+    {
+        private int[][] temp = null;
+        private int maxArea = 0;
+        private int area = 1;
+        private int row;
+        private int col;
+
+        public int MaxAreaOfIsland(int[][] grid)
+        {
+            if (grid == null || grid.Length == 0) return maxArea;
+
+            temp = grid;
+            row = grid.Length;
+            col = grid[0].Length;
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    if (grid[i][j] == 1)
+                        DFS(i, j);
+
+                    // reset a for 1
+                    area = 1;
+                }
+            }
+
+            return maxArea;
+        }
+
+        private void DFS(int x, int y)
+        {
+            maxArea = Math.Max(maxArea, area);
+
+            if (x < 0 || y < 0 || x >= row || y >= col) return;
+            temp[x][y] = 0;
+            area++;
+
+            // top
+            if (x > 0 && temp[x - 1][y] == 1)
+                DFS(x - 1, y);
+
+            // bottom
+            if (x < row - 1 && temp[x + 1][y] == 1)
+                DFS(x + 1, y);
+
+            // left
+            if (y > 0 && temp[x][y - 1] == 1)
+                DFS(x, y - 1);
+
+            // right
+            if (y < col - 1 && temp[x][y + 1] == 1)
+                DFS(x, y + 1);
+        }
+    }
+}
---
+++ UnitTest/_0695_Max_area_of_island_Test.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0695.Max_area_of_island;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0695.Max_area_of_island.Tests
+{
+    [TestClass()]
+    public class _0695_Max_area_of_island_Test
+    {
+        _0695_Max_area_of_island solution = new _0695_Max_area_of_island();
+
+        [TestMethod()]
+        public void MaxAreaOfIsland_Test1()
+        {
+            // Arrange
+            int[][] grid = new int[][]
+            {
+               new int[] { 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+               new int[] { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
+               new int[] { 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0 },
+               new int[] { 0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0 },
+               new int[] { 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0 },
+               new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0 },
+               new int[] { 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0 },
+               new int[] { 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0 }
+            };
+
+            var expected = 6;
+
+            // Act
+            var actual = solution.MaxAreaOfIsland(grid);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MaxAreaOfIsland_Test2()
+        {
+            // Arrange
+            int[][] grid = new int[][]
+            {
+               new int[] { 0, 0, 0, 0, 0, 0, 0, 0 },
+            };
+
+            var expected = 0;
+
+            // Act
+            var actual = solution.MaxAreaOfIsland(grid);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

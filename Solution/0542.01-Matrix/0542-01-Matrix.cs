9b88c12936e365963db7ffa11d8a7e01335ddf10
Riley Lin <rileylin.space@gmail.com>
2022-05-30 18:54:46 +0800

    (0542) new

---
+++ Solution/0542.01-Matrix/0542-01-Matrix.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0542._01_Matrix
+{
+    public class _0542_01_Matrix
+    {
+        private int[][] res = null;
+        private int row;
+        private int col;
+
+        public int[][] UpdateMatrix(int[][] mat)
+        {
+            if (mat.Length == 0) return mat;
+
+            res = mat;
+            row = mat.Length;
+            col = mat[0].Length;
+
+            for (int i = 0; i < row; i++)
+                for (int j = 0; j < col; j++)
+                    if (res[i][j] == 1 && !hasNeighborZero(i, j))
+                        res[i][j] = int.MaxValue;
+
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    if (res[i][j] != 1) continue;
+
+                    DFS(i, j, 1);
+                }
+            }
+
+            return res;
+
+        }
+
+        private void DFS(int x, int y, int val)
+        {
+            if (x < 0 || x >= row || y < 0 || y >= col || res[x][y] < val) return;
+
+            res[x][y] = val;
+
+            val++;
+            DFS(x + 1, y, val); // right
+            DFS(x - 1, y, val); // left
+            DFS(x, y + 1, val); // top
+            DFS(x, y - 1, val); // bottom
+        }
+
+        private bool hasNeighborZero(int x, int y)
+        {
+            // right
+            if (x < row - 1 && res[x + 1][y] == 0) return true;
+
+            // left
+            if (x > 0 && res[x - 1][y] == 0) return true;
+
+            // top
+            if (y > 0 && res[x][y - 1] == 0) return true;
+
+            // bottom
+            if (y < col - 1 && res[x][y + 1] == 0) return true;
+
+            return false;
+        }
+    }
+}
---
+++ UnitTest/_0542_01_Matrix_Test.cs
@@ -0,0 +1,65 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0542._01_Matrix;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0542._01_Matrix.Tests
+{
+    [TestClass()]
+    public class _0542_01_Matrix_Test
+    {
+        _0542_01_Matrix solution = new _0542_01_Matrix();
+
+        [TestMethod()]
+        public void UpdateMatrix_Test1()
+        {
+            // Arrange
+            int[][] max = new int[][]
+            {
+                new int[] { 0, 0, 0 },
+                new int[] { 0, 1, 0 },
+                new int[] { 0, 0, 0 },
+            };
+
+            var expected = new int[][]
+            {
+                new int[] { 0, 0, 0 },
+                new int[] { 0, 1, 0 },
+                new int[] { 0, 0, 0 },
+            };
+
+            // Act
+            var actual = solution.UpdateMatrix(max);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void UpdateMatrix_Test2()
+        {
+            // Arrange
+            int[][] max = new int[][]
+            {
+                new int[] { 0, 0, 0 },
+                new int[] { 0, 1, 0 },
+                new int[] { 1, 1, 1 },
+            };
+
+            var expected = new int[][]
+            {
+                new int[] { 0, 0, 0 },
+                new int[] { 0, 1, 0 },
+                new int[] { 1, 2, 1 },
+            };
+
+            // Act
+            var actual = solution.UpdateMatrix(max);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

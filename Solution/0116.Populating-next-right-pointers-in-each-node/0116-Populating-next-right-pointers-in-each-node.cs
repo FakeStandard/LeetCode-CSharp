352b6ee1eb91e7c57536dfbfe095ff8cb368d52b
Riley Lin <rileylin.space@gmail.com>
2022-05-30 22:17:55 +0800

    (0116) new

---
+++ Solution/0116.Populating-next-right-pointers-in-each-node/0116-Populating-next-right-pointers-in-each-node.cs
@@ -0,0 +1,28 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0116.Populating_next_right_pointers_in_each_node
+{
+    public class _0116_Populating_next_right_pointers_in_each_node
+    {
+        public Node Connect(Node root)
+        {
+            if (root == null) return null;
+
+            Node left = root.left;
+            Node right = root.right;
+
+            if (left != null) left.next = right;
+
+            if (right != null && root.next != null)
+                right.next = root.next.left;
+
+            Connect(left);
+            Connect(right);
+
+            return root;
+        }
+    }
+}

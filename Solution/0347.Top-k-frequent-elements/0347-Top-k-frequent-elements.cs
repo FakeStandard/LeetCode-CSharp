23405cecc5750167974249d0d4b3019a60fbb06e
Riley Lin <rileylin.space@gmail.com>
2022-05-31 11:13:35 +0800

    (0347) new

---
+++ Solution/0347.Top-k-frequent-elements/0347-Top-k-frequent-elements.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0347.Top_k_frequent_elements
+{
+    public class _0347_Top_k_frequent_elements
+    {
+        public int[] TopKFrequent(int[] nums, int k)
+        {
+            if (nums.Length == 1) return nums;
+
+            var dic = new Dictionary<int, int>();
+            var res = new int[k];
+
+            // record the number of occurrences of each element.
+            foreach (int num in nums)
+                if (!dic.TryAdd(num, 1))
+                    dic[num]++;
+
+            int max = int.MinValue;
+            int i = 0;
+
+            while (i < k)
+            {
+                foreach (KeyValuePair<int, int> pair in dic)
+                {
+                    if (max == int.MinValue)
+                        max = pair.Key;
+                    else if (pair.Value > dic[max])
+                        max = pair.Key;
+                }
+
+                res[i] = max;
+                dic.Remove(max);
+                max = int.MinValue;
+
+                i++;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ UnitTest/_0347_Top_k_frequent_elements_Test.cs
@@ -0,0 +1,90 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0347.Top_k_frequent_elements;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0347.Top_k_frequent_elements.Tests
+{
+    [TestClass()]
+    public class _0347_Top_k_frequent_elements_Test
+    {
+        _0347_Top_k_frequent_elements solution = new _0347_Top_k_frequent_elements();
+
+        [TestMethod()]
+        public void TopKFrequent_Test1()
+        {
+            // Arrange
+            int[] nums = new int[] { 1, 1, 1, 2, 2, 3 };
+            int k = 2;
+            int[] expected = new int[] { 1, 2 };
+
+            // Act
+            var actual = solution.TopKFrequent(nums, k);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void TopKFrequent_Test2()
+        {
+            // Arrange
+            int[] nums = new int[] { 1 };
+            int[] expected = new int[] { 1 };
+            int k = 1;
+
+            // Act
+            var actual = solution.TopKFrequent(nums, k);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void TopKFrequent_Test3()
+        {
+            // Arrange
+            int[] nums = new int[] { -1, -1 };
+            int[] expected = new int[] { -1 };
+            int k = 1;
+
+            // Act
+            var actual = solution.TopKFrequent(nums, k);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void TopKFrequent_Test4()
+        {
+            // Arrange
+            int[] nums = new int[] { 3, 0, 1, 0 };
+            int[] expected = new int[] { 0 };
+            int k = 1;
+
+            // Act
+            var actual = solution.TopKFrequent(nums, k);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void TopKFrequent_Test5()
+        {
+            // Arrange
+            int[] nums = new int[] { 4, 1, -1, 2, -1, 2, 3 };
+            int[] expected = new int[] { -1, 2 };
+            int k = 2;
+
+            // Act
+            var actual = solution.TopKFrequent(nums, k);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

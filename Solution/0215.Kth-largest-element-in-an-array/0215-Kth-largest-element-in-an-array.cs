68101a363367504e64b249b702d6d1517324fbbd
Riley Lin <rileylin.space@gmail.com>
2022-05-31 14:18:49 +0800

    (0215) new

---
+++ Solution/0215.Kth-largest-element-in-an-array/0215-Kth-largest-element-in-an-array.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0215.Kth_largest_element_in_an_array
+{
+    public class _0215_Kth_largest_element_in_an_array
+    {
+        public int FindKthLargest(int[] nums, int k)
+        {
+            quickSort(nums, 0, nums.Length - 1, nums.Length - k);
+            return nums[nums.Length - k];
+        }
+
+        private void quickSort(int[] nums, int left, int right, int k)
+        {
+            int i = left;
+            int j = right;
+            int pivot = nums[left];
+            int temp;
+
+            while (i < j)
+            {
+                // begin left.
+                while (nums[j] >= pivot && i < j)
+                    j--;
+
+                // begin right.
+                while (nums[i] <= pivot && i < j)
+                    i++;
+
+                // swap
+                if (i < j)
+                {
+                    temp = nums[i];
+                    nums[i] = nums[j];
+                    nums[j] = temp;
+                }
+            }
+
+            // change i & j position.
+            nums[left] = nums[j];
+            nums[j] = pivot;
+
+            if (j < k)
+                quickSort(nums, j + 1, right, k);
+            else if (j > k)
+                quickSort(nums, left, j - 1, k);
+        }
+    }
+}
---
+++ UnitTest/_0215_Kth_largest_element_in_an_array_Test.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0215.Kth_largest_element_in_an_array;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0215.Kth_largest_element_in_an_array.Tests
+{
+    [TestClass()]
+    public class _0215_Kth_largest_element_in_an_array_Test
+    {
+        _0215_Kth_largest_element_in_an_array solution = new _0215_Kth_largest_element_in_an_array();
+
+        [TestMethod()]
+        public void FindKthLargest_Test1()
+        {
+            // Arrange
+            int[] nums = new int[] { 3, 2, 1, 5, 6, 4 };
+            int k = 2;
+            var expected = 5;
+
+            // Act
+            var actual = solution.FindKthLargest(nums, k);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FindKthLargest_Test2()
+        {
+            // Arrange
+            int[] nums = new int[] { 3, 2, 3, 1, 2, 4, 5, 5, 6 };
+            int k = 4;
+            var expected = 4;
+
+            // Act
+            var actual = solution.FindKthLargest(nums, k);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

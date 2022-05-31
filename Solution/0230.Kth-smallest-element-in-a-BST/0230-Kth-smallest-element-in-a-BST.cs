dae0405011fbd04fbc01828e61bc92c3c0c0b52d
Riley Lin <rileylin.space@gmail.com>
2022-05-31 14:48:12 +0800

    (0230) new

---
+++ Solution/0230.Kth-smallest-element-in-a-BST/0230-Kth-smallest-element-in-a-BST.cs
@@ -0,0 +1,32 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0230.Kth_smallest_element_in_a_BST
+{
+    public class _0230_Kth_smallest_element_in_a_BST
+    {
+        private List<int> bucket;
+        private int temp;
+
+        public int KthSmallest(TreeNode root, int k)
+        {
+            bucket = new List<int>();
+            temp = k;
+
+            InOrder(root);
+
+            return bucket[k - 1];
+        }
+
+        private void InOrder(TreeNode node)
+        {
+            if (node == null || bucket.Count >= temp) return;
+
+            InOrder(node.left);
+            bucket.Add(node.val);
+            InOrder(node.right);
+        }
+    }
+}
---
+++ Solution/0230.Kth-smallest-element-in-a-BST/README.md
@@ -0,0 +1,14 @@
+# 0230. Kth Smallest Element in a BST
+Given the root of a binary search tree, and an integer k, return the kth smallest value (1-indexed) of all the values of the nodes in the tree.
+
+### Example 1:
+```
+Input: root = [3,1,4,null,2], k = 1
+Output: 1
+```
+
+### Example 2:
+```
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+Output: 3
+```

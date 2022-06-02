bc58ce7bf29198e79cae9c594c79080fffbb117a
Riley Lin <rileylin.space@gmail.com>
2022-06-02 16:57:07 +0800

    (2095) new

---
+++ Solution/2095.Delete-the-middle-node-of-a-linked-list/2095-Delete-the-middle-node-of-a-linked-list.cs
@@ -0,0 +1,63 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._2095.Delete_the_middle_node_of_a_linked_list
+{
+    public class _2095_Delete_the_middle_node_of_a_linked_list
+    {
+        public ListNode DeleteMiddle(ListNode head)
+        {
+            // two pointers
+            if (head == null || head.next == null) return null;
+
+            var fast = head.next.next;
+            var slow = head;
+
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+            slow.next = slow.next.next;
+
+            return head;
+
+            // counting
+            //if (head == null) return null;
+
+            //var curr = head;
+            //var res = head;
+
+            //int index = -1;
+            //while (curr != null)
+            //{
+            //    curr = curr.next;
+            //    index++;
+            //}
+
+            //if (index == 0) return new ListNode().AddNode(new int[] { });
+
+            //int mid = index % 2 == 0 ? index / 2 : index / 2 + 1;
+
+            //int count = 0;
+            //while (res != null)
+            //{
+            //    // 2==2
+            //    if (count == mid - 1)
+            //    {
+            //        var temp = res.next.next;
+            //        res.next = temp;
+            //        break;
+            //    }
+
+            //    count++;
+            //    res = res.next;
+            //}
+
+            //return head;
+        }
+    }
+}
---
+++ UnitTest/_2095_Delete_the_middle_node_of_a_linked_list_Test.cs
@@ -0,0 +1,58 @@
+﻿using Common;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._2095.Delete_the_middle_node_of_a_linked_list;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2095.Delete_the_middle_node_of_a_linked_list.Tests
+{
+    [TestClass()]
+    public class _2095_Delete_the_middle_node_of_a_linked_list_Test
+    {
+        _2095_Delete_the_middle_node_of_a_linked_list solution = new _2095_Delete_the_middle_node_of_a_linked_list();
+
+        [TestMethod()]
+        public void DeleteMiddle_Test1()
+        {
+            // Arrange
+            var head = new ListNode().AddNode(new int[] { 1, 3, 4, 7, 1, 2, 6 });
+            var expected = new ListNode().AddNode(new int[] { 1, 3, 4, 1, 2, 6 });
+
+            // Act
+            var actual = solution.DeleteMiddle(head);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void DeleteMiddle_Test2()
+        {
+            // Arrange
+            var head = new ListNode().AddNode(new int[] { 1, 2, 3, 4 });
+            var expected = new ListNode().AddNode(new int[] { 1, 2, 4 });
+
+            // Act
+            var actual = solution.DeleteMiddle(head);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void DeleteMiddle_Test3()
+        {
+            // Arrange
+            var head = new ListNode().AddNode(new int[] { 1 });
+            ListNode expected = null;
+
+            // Act
+            var actual = solution.DeleteMiddle(head);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

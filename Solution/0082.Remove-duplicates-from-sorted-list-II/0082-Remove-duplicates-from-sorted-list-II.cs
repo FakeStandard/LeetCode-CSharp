35a41991038f74713682472f5fc8ee425ad8b8e3
Riley Lin <rileylin.space@gmail.com>
2022-06-02 09:57:48 +0800

    (0082) new

---
+++ Solution/0082.Remove-duplicates-from-sorted-list-II/0082-Remove-duplicates-from-sorted-list-II.cs
@@ -0,0 +1,59 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0082.Remove_duplicates_from_sorted_list_II
+{
+    public class _0082_Remove_duplicates_from_sorted_list_II
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null) return null;
+
+            ListNode res = new ListNode();
+            ListNode prev = res;
+            res.next = head;
+
+            while (head != null)
+            {
+                while (head.next != null && head.val == head.next.val)
+                    head = head.next;
+                if (prev.next == head)
+                    prev = prev.next;
+                else
+                    prev.next = head.next;
+                head = head.next;
+            }
+
+            return res.next;
+
+            // easy
+            //if (head == null) return head;
+
+            //ListNode temp = new ListNode();
+            //ListNode res = temp;
+            //Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            //// because the head is sorted list.
+            //while (head != null)
+            //{
+            //    if (!dic.TryAdd(head.val, 1))
+            //        dic[head.val]++;
+
+            //    head = head.next;
+            //}
+
+            //foreach (KeyValuePair<int, int> pair in dic)
+            //{
+            //    if (pair.Value == 1)
+            //    {
+            //        temp.next = new ListNode(pair.Key);
+            //        temp = temp.next;
+            //    }
+            //}
+
+            //return res.next;
+        }
+    }
+}
---
+++ UnitTest/_0082_Remove_duplicates_from_sorted_list_II_Test.cs
@@ -0,0 +1,44 @@
+﻿using Common;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0082.Remove_duplicates_from_sorted_list_II;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0082.Remove_duplicates_from_sorted_list_II.Tests
+{
+    [TestClass()]
+    public class _0082_Remove_duplicates_from_sorted_list_II_Test
+    {
+        _0082_Remove_duplicates_from_sorted_list_II solution = new _0082_Remove_duplicates_from_sorted_list_II();
+
+        [TestMethod()]
+        public void DeleteDuplicates_Test1()
+        {
+            // Arrange
+            ListNode head = new ListNode().AddNode(new int[] { 1, 2, 3, 3, 4, 4, 5 });
+            var expected = new ListNode().AddNode(new int[] { 1, 2, 5 });
+
+            // Act
+            var actual = solution.DeleteDuplicates(head);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void DeleteDuplicates_Test2()
+        {
+            // Arrange
+            ListNode head = new ListNode().AddNode(new int[] { 1, 1, 1, 2, 3 });
+            var expected = new ListNode().AddNode(new int[] { 2, 3 });
+
+            // Act
+            var actual = solution.DeleteDuplicates(head);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

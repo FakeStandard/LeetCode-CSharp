844f761b692dad83eda4d3efec0f3941ae10aa09
Riley Lin <rileylin.space@gmail.com>
2022-06-03 15:21:25 +0800

    (0240) new

---
+++ Solution/0240.Search-a-2D-matrix-II/0240-Search-a-2D-matrix-II.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0240.Search_a_2D_matrix_II
+{
+    public class _0240_Search_a_2D_matrix_II
+    {
+        public bool SearchMatrix(int[][] matrix, int target)
+        {
+            // matrix properties.
+            // Integers in each row are sorted in ascending from left to right.
+            // Integers in each column are sorted in ascending from top to bottom.
+
+            int row = matrix.Length;
+            int col = matrix[0].Length;
+            int i = row - 1;
+            int j = 0;
+
+            while (i >= 0 && j < col)
+            {
+                if (matrix[i][j] == target)
+                    return true;
+
+                if (matrix[i][j] > target)
+                    i--;
+                else
+                    j++;
+            }
+
+            return false;
+
+            //暴力破解
+            //foreach (int[] arr in matrix)
+            //    foreach (int i in arr)
+            //        if (i == target) return true;
+
+            //return false;
+        }
+    }
+}
---
+++ UnitTest/_0240_Search_a_2D_matrix_II_Test.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0240.Search_a_2D_matrix_II;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0240.Search_a_2D_matrix_II.Tests
+{
+    [TestClass()]
+    public class _0240_Search_a_2D_matrix_II_Test
+    {
+        _0240_Search_a_2D_matrix_II solution = new _0240_Search_a_2D_matrix_II();
+
+        [TestMethod()]
+        public void SearchMatrix_Test1()
+        {
+            // Arrange
+            int[][] matrix = new int[][]
+            {
+                new int[]{ 1, 4, 7, 11, 15 },
+                new int[]{ 2, 5, 8, 12, 19 },
+                new int[]{ 3, 6, 9, 16, 22 },
+                new int[]{ 10, 13, 14, 17, 24 },
+                new int[]{ 18, 21, 23, 26, 30 },
+            };
+
+            int target = 5;
+
+            // Act
+            var actual = solution.SearchMatrix(matrix, target);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void SearchMatrix_Test2()
+        {
+            // Arrange
+            int[][] matrix = new int[][]
+            {
+                new int[]{ 1, 4, 7, 11, 15 },
+                new int[]{ 2, 5, 8, 12, 19 },
+                new int[]{ 3, 6, 9, 16, 22 },
+                new int[]{ 10, 13, 14, 17, 24 },
+                new int[]{ 18, 21, 23, 26, 30 },
+            };
+
+            int target = 20;
+
+            // Act
+            var actual = solution.SearchMatrix(matrix, target);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+    }
+}

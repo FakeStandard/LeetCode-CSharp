0694024cbc4a1d33020ac469e298cdb0413d6432
Riley Lin <rileylin.space@gmail.com>
2022-06-06 12:16:17 +0800

    (0059) new

---
+++ Solution/0059.Spiral-matrix-II/0059-Spiral-matrix-II.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0059.Spiral_matrix_II
+{
+    public class _0059_Spiral_matrix_II
+    {
+        public int[][] GenerateMatrix(int n)
+        {
+            int num = 1;
+            int top = 0, bottom = n - 1, left = 0, right = n - 1;
+
+            int[][] res = new int[n][];
+
+            for (int i = 0; i < n; i++)
+                res[i] = new int[n];
+
+            while (left <= right && top <= bottom)
+            {
+                // left to right
+                for (int i = left; i <= right; i++)
+                {
+                    res[top][i] = num;
+                    num++;
+                }
+
+                top++;
+
+                // top to bottom
+                for (int j = top; j <= bottom; j++)
+                {
+                    res[j][right] = num;
+                    num++;
+                }
+
+                right--;
+
+                // right to left
+                for (int i = right; i >= left; i--)
+                {
+                    res[bottom][i] = num;
+                    num++;
+                }
+
+                bottom--;
+
+                // bottom to top
+                for (int j = bottom; j >= top; j--)
+                {
+                    res[j][left] = num;
+                    num++;
+                }
+
+                left++;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ UnitTest/_0059_Spiral_matrix_II_Test.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0059.Spiral_matrix_II;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0059.Spiral_matrix_II.Tests
+{
+    [TestClass()]
+    public class _0059_Spiral_matrix_II_Test
+    {
+        _0059_Spiral_matrix_II solution = new _0059_Spiral_matrix_II();
+
+        [TestMethod()]
+        public void GenerateMatrix_Test1()
+        {
+            // Arrange
+            int n = 3;
+            var expected = new int[][]
+            {
+                new int[]{ 1, 2, 3, },
+                new int[]{ 8, 9, 4, },
+                new int[]{ 7, 6, 5, },
+            };
+
+            // Act
+            var actual = solution.GenerateMatrix(n);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void GenerateMatrix_Test2()
+        {
+            // Arrange
+            int n = 1;
+            var expected = new int[][]
+            {
+                new int[]{ 1 },
+            };
+
+            // Act
+            var actual = solution.GenerateMatrix(n);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

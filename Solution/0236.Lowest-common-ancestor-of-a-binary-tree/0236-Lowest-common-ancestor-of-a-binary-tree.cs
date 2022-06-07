c827bcd165d1d215a45a80d60b84f9198333e2ba
Riley Lin <rileylin.space@gmail.com>
2022-06-07 11:15:03 +0800

    (0236) new

---
+++ Solution/0236.Lowest-common-ancestor-of-a-binary-tree/0236-Lowest-common-ancestor-of-a-binary-tree.cs
@@ -0,0 +1,23 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0236.Lowest_common_ancestor_of_a_binary_tree
+{
+    public class _0236_Lowest_common_ancestor_of_a_binary_tree
+    {
+        public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
+        {
+            if (root == null || root.val == p.val || root.val == q.val) return root;
+
+            var left = LowestCommonAncestor(root.left, p, q);
+            var right = LowestCommonAncestor(root.right, p, q);
+
+            if (left != null && right != null)
+                return root;
+
+            return left != null ? left : right;
+        }
+    }
+}

59db75cb84a7dd990c725f0aaa475129ac4ffcbf
Riley Lin <rileylin.space@gmail.com>
2022-06-08 10:28:55 +0800

    (1557) new

---
+++ Solution/1557.Minimum-number-of-vertices-to-reach-all-nodes/1557-Minimum-number-of-vertices-to-reach-all-nodes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._1557.Minimum_number_of_vertices_to_reach_all_nodes
+{
+    public class _1557_Minimum_number_of_vertices_to_reach_all_nodes
+    {
+        public IList<int> FindSmallestSetOfVertices(int n, IList<IList<int>> edges)
+        {
+            HashSet<int> hash = new HashSet<int>();
+
+            for (int i = 0; i < n; i++)
+                hash.Add(i);
+
+            // Just remove reachable vertices.
+            foreach (IList<int> edge in edges)
+                hash.Remove(edge[1]);
+
+            return new List<int>(hash);
+        }
+    }
+}
---
+++ UnitTest/_1557_Minimum_number_of_vertices_to_reach_all_nodes_Test.cs
@@ -0,0 +1,61 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._1557.Minimum_number_of_vertices_to_reach_all_nodes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1557.Minimum_number_of_vertices_to_reach_all_nodes.Tests
+{
+    [TestClass()]
+    public class _1557_Minimum_number_of_vertices_to_reach_all_nodes_Test
+    {
+        _1557_Minimum_number_of_vertices_to_reach_all_nodes solution = new _1557_Minimum_number_of_vertices_to_reach_all_nodes();
+
+        [TestMethod()]
+        public void FindSmallestSetOfVertices_Test1()
+        {
+            // Arrange
+            int n = 6;
+            int[][] edges = new int[][]
+            {
+                new int[] { 0, 1 },
+                new int[] { 0, 2 },
+                new int[] { 2, 5 },
+                new int[] { 3, 4 },
+                new int[] { 4, 2 },
+            };
+
+            var expected = new int[] { 0, 3 };
+
+            // Act
+            var actual = solution.FindSmallestSetOfVertices(n, edges);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void FindSmallestSetOfVertices_Test2()
+        {
+            // Arrange
+            int n = 5;
+            int[][] edges = new int[][]
+            {
+                new int[] { 0, 1 },
+                new int[] { 2, 1 },
+                new int[] { 3, 1 },
+                new int[] { 1, 4 },
+                new int[] { 2, 4 },
+            };
+
+            var expected = new int[] { 0, 2, 3 };
+
+            // Act
+            var actual = solution.FindSmallestSetOfVertices(n, edges);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

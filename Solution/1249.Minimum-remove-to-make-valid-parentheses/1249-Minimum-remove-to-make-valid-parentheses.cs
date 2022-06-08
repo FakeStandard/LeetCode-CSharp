232235e2f2b2d0585de6d34cc1d56fd54d237891
Riley Lin <rileylin.space@gmail.com>
2022-06-08 10:47:18 +0800

    (1249) new

---
+++ Solution/1249.Minimum-remove-to-make-valid-parentheses/1249-Minimum-remove-to-make-valid-parentheses.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._1249.Minimum_remove_to_make_valid_parentheses
+{
+    public class _1249_Minimum_remove_to_make_valid_parentheses
+    {
+        public string MinRemoveToMakeValid(string s)
+        {
+            if (s == null || s.Length == 0) return s;
+
+            var stack = new Stack<Tuple<char, int>>();
+            var arr = s.ToCharArray();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (stack.Count > 0 && arr[i] == ')' && stack.Peek().Item1 == '(')
+                    stack.Pop();
+                else if (arr[i] == '(' || arr[i] == ')')
+                    stack.Push(new Tuple<char, int>(arr[i], i));
+            }
+
+            while (stack.Count > 0)
+            {
+                int i = stack.Peek().Item2;
+                arr[i] = ' ';
+                stack.Pop();
+            }
+
+            return new string(arr).Replace(" ", String.Empty);
+        }
+    }
+}
---
+++ UnitTest/_1249_Minimum_remove_to_make_valid_parentheses_Test.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._1249.Minimum_remove_to_make_valid_parentheses;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1249.Minimum_remove_to_make_valid_parentheses.Tests
+{
+    [TestClass()]
+    public class _1249_Minimum_remove_to_make_valid_parentheses_Test
+    {
+        _1249_Minimum_remove_to_make_valid_parentheses solution = new _1249_Minimum_remove_to_make_valid_parentheses();
+
+        [TestMethod()]
+        public void MinRemoveToMakeValid_Test1()
+        {
+            // Arrange
+            string s = "lee(t(c)o)de)";
+            string expected = "lee(t(c)o)de";
+
+            // Act
+            var actual = solution.MinRemoveToMakeValid(s);
+
+            // Assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod()]
+        public void MinRemoveToMakeValid_Test2()
+        {
+            // Arrange
+            string s = "a)b(c)d";
+            string expected = "ab(c)d";
+
+            // Act
+            var actual = solution.MinRemoveToMakeValid(s);
+
+            // Assert
+            Assert.AreEqual(actual, expected);
+        }
+
+        [TestMethod()]
+        public void MinRemoveToMakeValid_Test3()
+        {
+            // Arrange
+            string s = "a)b(c)d";
+            string expected = "ab(c)d";
+
+            // Act
+            var actual = solution.MinRemoveToMakeValid(s);
+
+            // Assert
+            Assert.AreEqual(actual, expected);
+        }
+    }
+}

ac901960a256238a27880facf1ca37352a78bb0d
Riley Lin <rileylin.space@gmail.com>
2022-06-13 12:00:23 +0800

    (1207) new

---
+++ Solution/1207.Unique-number-of-occurrences/1207-Unique-number-of-occurrences.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._1207.Unique_number_of_occurrences
+{
+    public class _1207_Unique_number_of_occurrences
+    {
+        public bool UniqueOccurrences(int[] arr)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (dic.ContainsKey(arr[i]))
+                    dic[arr[i]]++;
+                else
+                    dic.Add(arr[i], 1);
+            }
+
+            HashSet<int> set = new HashSet<int>();
+
+            foreach (KeyValuePair<int, int> pair in dic)
+            {
+                if (set.Contains(pair.Value))
+                    return false;
+                else
+                    set.Add(pair.Value);
+            }
+
+            return true;
+        }
+    }
+}
---
+++ UnitTest/_1207_Unique_number_of_occurrences_Test.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._1207.Unique_number_of_occurrences;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1207.Unique_number_of_occurrences.Tests
+{
+    [TestClass()]
+    public class _1207_Unique_number_of_occurrences_Test
+    {
+        _1207_Unique_number_of_occurrences solution = new _1207_Unique_number_of_occurrences();
+
+        [TestMethod()]
+        public void UniqueOccurrences_Test1()
+        {
+            // Arrange
+            int[] arr = new int[] { 1, 2, 2, 1, 1, 3 };
+
+            // Act
+            var actual = solution.UniqueOccurrences(arr);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void UniqueOccurrences_Test2()
+        {
+            // Arrange
+            int[] arr = new int[] { 1, 2 };
+
+            // Act
+            var actual = solution.UniqueOccurrences(arr);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+
+        [TestMethod()]
+        public void UniqueOccurrences_Test3()
+        {
+            // Arrange
+            int[] arr = new int[] { -3, 0, 1, -3, 1, 1, 1, -3, 10, 0 };
+
+            // Act
+            var actual = solution.UniqueOccurrences(arr);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+    }
+}

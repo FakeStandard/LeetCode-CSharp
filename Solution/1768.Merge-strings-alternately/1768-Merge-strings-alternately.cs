c4ee3456efea7560806719e0e5fcff85acf029a6
Riley Lin <rileylin.space@gmail.com>
2022-06-13 10:00:48 +0800

    (1768) new

---
+++ Solution/1768.Merge-strings-alternately/1768-Merge-strings-alternately.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._1768.Merge_strings_alternately
+{
+    public class _1768_Merge_strings_alternately
+    {
+        public string MergeAlternately(string word1, string word2)
+        {
+            int i = 0, j = 0;
+            string res = "";
+
+            while (i < word1.Length || j < word2.Length)
+            {
+                if (i < word1.Length)
+                    res += word1[i++];
+                if (j < word2.Length)
+                    res += word2[j++];
+            }
+
+            return res;
+        }
+    }
+}
---
+++ UnitTest/_1768_Merge_strings_alternately_Test.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._1768.Merge_strings_alternately;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1768.Merge_strings_alternately.Tests
+{
+    [TestClass()]
+    public class _1768_Merge_strings_alternately_Test
+    {
+        _1768_Merge_strings_alternately solution = new _1768_Merge_strings_alternately();
+
+        [TestMethod()]
+        public void MergeAlternately_Test1()
+        {
+            // Arrange
+            string word1 = "abc";
+            string word2 = "pqr";
+            var expected = "apbqcr";
+
+            // Act
+            var actual = solution.MergeAlternately(word1, word2);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MergeAlternately_Test2()
+        {
+            // Arrange
+            string word1 = "ab";
+            string word2 = "pqrs";
+            var expected = "apbqrs";
+
+            // Act
+            var actual = solution.MergeAlternately(word1, word2);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MergeAlternately_Test3()
+        {
+            // Arrange
+            string word1 = "abcd";
+            string word2 = "pq";
+            var expected = "apbqcd";
+
+            // Act
+            var actual = solution.MergeAlternately(word1, word2);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

6e52a84da1f71a0a4877a9105cf8aa07a3840e78
Riley Lin <rileylin.space@gmail.com>
2022-06-13 13:48:10 +0800

    (1295) new

---
+++ Solution/1295.Find-numbers-with-even-number-of-digits/1295-Find-numbers-with-even-number-of-digits.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._1295.Find_numbers_with_even_number_of_digits
+{
+    public class _1295_Find_numbers_with_even_number_of_digits
+    {
+        public int FindNumbers(int[] nums)
+        {
+            int count = 0;
+
+            // approach 1
+            //for (int i = 0; i < nums.Length; i++)
+            //    if (nums[i].ToString().Length % 2 == 0)
+            //        count++;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int q = nums[i];
+                int num = 0;
+
+                while (q != 0)
+                {
+                    q /= 10;
+                    num++;
+                }
+
+                if (num % 2 == 0) count++;
+            }
+
+            return count;
+        }
+    }
+}
---
+++ UnitTest/_1295_Find_numbers_with_even_number_of_digits_Test.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._1295.Find_numbers_with_even_number_of_digits;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1295.Find_numbers_with_even_number_of_digits.Tests
+{
+    [TestClass()]
+    public class _1295_Find_numbers_with_even_number_of_digits_Test
+    {
+        _1295_Find_numbers_with_even_number_of_digits solution = new _1295_Find_numbers_with_even_number_of_digits();
+
+        [TestMethod()]
+        public void FindNumbers_Test1()
+        {
+            // Arrange
+            int[] nums = new int[] { 12, 345, 2, 6, 7896 };
+            var expected = 2;
+
+            // Act
+            var actual = solution.FindNumbers(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FindNumbers_Test2()
+        {
+            // Arrange
+            int[] nums = new int[] { 555, 901, 482, 1771 };
+            var expected = 1;
+
+            // Act
+            var actual = solution.FindNumbers(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

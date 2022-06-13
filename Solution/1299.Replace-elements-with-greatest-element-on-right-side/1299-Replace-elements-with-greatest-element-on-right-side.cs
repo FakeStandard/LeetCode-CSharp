13269f7162deca636dad0db9ad78fb1ec172b2f8
Riley Lin <rileylin.space@gmail.com>
2022-06-13 14:18:53 +0800

    (1299) new

---
+++ Solution/1299.Replace-elements-with-greatest-element-on-right-side/1299-Replace-elements-with-greatest-element-on-right-side.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._1299.Replace_elements_with_greatest_element_on_right_side
+{
+    public class _1299_Replace_elements_with_greatest_element_on_right_side
+    {
+        public int[] ReplaceElements(int[] arr)
+        {
+            int[] res = new int[arr.Length];
+            int max = 0;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                max = 0;
+
+                // search max on right side
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] > max)
+                        max = arr[j];
+                }
+
+                res[i] = max;
+            }
+
+            res[res.Length - 1] = -1;
+
+            return res;
+        }
+    }
+}
---
+++ UnitTest/_1299_Replace_elements_with_greatest_element_on_right_side_Test.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._1299.Replace_elements_with_greatest_element_on_right_side;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1299.Replace_elements_with_greatest_element_on_right_side.Tests
+{
+    [TestClass()]
+    public class _1299_Replace_elements_with_greatest_element_on_right_side_Test
+    {
+        _1299_Replace_elements_with_greatest_element_on_right_side solution = new _1299_Replace_elements_with_greatest_element_on_right_side();
+
+        [TestMethod()]
+        public void ReplaceElements_Test1()
+        {
+            // Arrange
+            int[] arr = new int[] { 17, 18, 5, 4, 6, 1 };
+            var expected = new int[] { 18, 6, 6, 6, 1, -1 };
+
+            // Act
+            var actual = solution.ReplaceElements(arr);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void ReplaceElements_Test2()
+        {
+            // Arrange
+            int[] arr = new int[] { 400 };
+            var expected = new int[] { -1 };
+
+            // Act
+            var actual = solution.ReplaceElements(arr);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

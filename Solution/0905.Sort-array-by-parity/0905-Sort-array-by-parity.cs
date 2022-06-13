634aab9901c70b71b0dca76192b9fd022500dd71
Riley Lin <rileylin.space@gmail.com>
2022-06-13 10:47:42 +0800

    (0905) new

---
+++ Solution/0905.Sort-array-by-parity/0905-Sort-array-by-parity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0905.Sort_array_by_parity
+{
+    public class _0905_Sort_array_by_parity
+    {
+        public int[] SortArrayByParity(int[] nums)
+        {
+            int[] res = new int[nums.Length];
+            int head = 0, tail = nums.Length - 1;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] % 2 == 0)
+                {
+                    res[head] = nums[i];
+                    head++;
+                }
+                else
+                {
+                    res[tail] = nums[i];
+                    tail--;
+                }
+            }
+
+            return res;
+        }
+    }
+}
---
+++ UnitTest/_0905_Sort_array_by_parity_Test.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0905.Sort_array_by_parity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0905.Sort_array_by_parity.Tests
+{
+    [TestClass()]
+    public class _0905_Sort_array_by_parity_Test
+    {
+        _0905_Sort_array_by_parity solution = new _0905_Sort_array_by_parity();
+
+        [TestMethod()]
+        public void SortArrayByParity_Test1()
+        {
+            // Arrange
+            int[] nums = new int[] { 3, 1, 2, 4 };
+            var expected = new int[] { 2, 4, 3, 1 };
+
+            // Act
+            var actual = solution.SortArrayByParity(nums);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+
+        [TestMethod()]
+        public void SortArrayByParity_Test2()
+        {
+            // Arrange
+            int[] nums = new int[] { 0 };
+            var expected = new int[] { 0 };
+
+            // Act
+            var actual = solution.SortArrayByParity(nums);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

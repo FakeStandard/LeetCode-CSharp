cf874c88e642cbcb7f0315c8f9265dc512d33c12
Riley Lin <rileylin.space@gmail.com>
2022-06-14 11:17:32 +0800

    (1460) new

---
+++ Solution/1460.Make-two-arrays-equal-by-reversing-sub-arrays/1460-Make-two-arrays-equal-by-reversing-sub-arrays.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._1460.Make_two_arrays_equal_by_reversing_sub_arrays
+{
+    public class _1460_Make_two_arrays_equal_by_reversing_sub_arrays
+    {
+        public bool CanBeEqual(int[] target, int[] arr)
+        {
+            Dictionary<int, int> dic = new Dictionary<int, int>();
+
+            //foreach (var i in target)
+            //{
+            //    if (dic.ContainsKey(i))
+            //        dic[i]++;
+            //    else
+            //        dic.Add(i, 1);
+            //}
+
+            //foreach (var j in arr)
+            //{
+            //    if (!dic.ContainsKey(j))
+            //        return false;
+            //    else if (dic[j] == 0)
+            //        return false;
+            //    else
+            //        dic[j]--;
+            //}
+
+            for (int i = 0; i < target.Length; i++)
+            {
+                // target
+                if (dic.ContainsKey(target[i]))
+                    dic[target[i]]++;
+                else
+                    dic.Add(target[i], 1);
+
+                // arr
+                if (dic.ContainsKey(arr[i]))
+                    dic[arr[i]]--;
+                else
+                    dic.Add(arr[i], -1);
+
+                // remove
+                if (dic[target[i]] == 0)
+                    dic.Remove(target[i]);
+
+                if (dic.ContainsKey(arr[i]) && dic[arr[i]] == 0)
+                    dic.Remove(arr[i]);
+            }
+
+            return dic.Count == 0;
+        }
+    }
+}
---
+++ UnitTest/_1460_Make_two_arrays_equal_by_reversing_sub_arrays_Test.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._1460.Make_two_arrays_equal_by_reversing_sub_arrays;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _1460.Make_two_arrays_equal_by_reversing_sub_arrays.Tests
+{
+    [TestClass()]
+    public class _1460_Make_two_arrays_equal_by_reversing_sub_arrays_Test
+    {
+        _1460_Make_two_arrays_equal_by_reversing_sub_arrays solution = new _1460_Make_two_arrays_equal_by_reversing_sub_arrays();
+
+        [TestMethod()]
+        public void CanBeEqual_Test1()
+        {
+            // Arrange
+            int[] target = new int[] { 1, 2, 3, 4 };
+            int[] arr = new int[] { 2, 4, 1, 3 };
+
+            // Act
+            var actual = solution.CanBeEqual(target, arr);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void CanBeEqual_Test2()
+        {
+            // Arrange
+            int[] target = new int[] { 7 };
+            int[] arr = new int[] { 7 };
+
+            // Act
+            var actual = solution.CanBeEqual(target, arr);
+
+            // Assert
+            Assert.IsTrue(actual);
+        }
+
+        [TestMethod()]
+        public void CanBeEqual_Test3()
+        {
+            // Arrange
+            int[] target = new int[] { 3, 7, 9 };
+            int[] arr = new int[] { 3, 7, 11 };
+
+            // Act
+            var actual = solution.CanBeEqual(target, arr);
+
+            // Assert
+            Assert.IsFalse(actual);
+        }
+    }
+}

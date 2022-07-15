aa41b9d8867bd78328c19ffd5be0274092891930
Riley Lin <rileylin.space@gmail.com>
2022-07-15 14:56:34 +0800

    Update 0013-Roman-to-integer.cs

--- Solution/0013.Roman-to-integer/0013-Roman-to-integer.cs
+++ Solution/0013.Roman-to-integer/0013-Roman-to-integer.cs
@@ -22,33 +22,28 @@ namespace Solution._0013.Roman_to_integer
                 { 'M', 1000 }
             };
 
-            int counter = 0;
+            // 儲存第一個字
+            int counter = (int)dic[s[0]];
 
-            for (int i = 0; i < s.Length; i++)
+            for (int i = 1; i < s.Length; i++)
             {
-                // 先處理第一個字
-                if (i == 0)
-                {
-                    counter += (int)dic[s[i]];
-                    continue;
-                }
-
-                // 接著處理後續的字
+                // 做加法原則
                 if ((int)dic[s[i - 1]] >= (int)dic[s[i]])
                 {
                     // 正常情況, keep going
                     counter += (int)dic[s[i]];
                 }
-                else
+                else // 否則做減法原則
                 {
                     // 兩數組合, 依照羅馬數字規則做相減, 如 IV 為 5-1=4
 
                     // 先減去剛剛加的
-                    counter -= (int)dic[s[i - 1]];
+                    //counter -= (int)dic[s[i - 1]];
 
                     // 再將兩數相減後再加回去
-                    counter += (int)dic[s[i]] - (int)dic[s[i - 1]];
+                    //counter += (int)dic[s[i]] - (int)dic[s[i - 1]];
 
+                    counter += (int)dic[s[i]] - (int)dic[s[i - 1]] * 2;
                 }
             }
 

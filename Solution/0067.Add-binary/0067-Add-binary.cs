fa9eed2a979d4adf6681bab698c126cfd72e3b30
Riley Lin <rileylin.space@gmail.com>
2022-09-27 17:48:07 +0800

    Update 0067-Add-binary.cs

--- Solution/0067.Add-binary/0067-Add-binary.cs
+++ Solution/0067.Add-binary/0067-Add-binary.cs
@@ -9,68 +9,58 @@ namespace Solution._0067.Add_binary
     {
         public string AddBinary(string a, string b)
         {
-            if (a == null || a == string.Empty || b == null || b == string.Empty)
-                return a == null || a == string.Empty ? b : a;
+            // 將 a,b 補 0 直到相同長度
+            int maxLen = a.Length > b.Length ? a.Length : b.Length;
+            a = a.PadLeft(maxLen, '0');
+            b = b.PadLeft(maxLen, '0');
 
-            // true 表進位, false 表無進位
+            // true 表示進位, false 則無
             bool carry = false;
-
             StringBuilder sb = new StringBuilder();
 
-            int i;
+            int add;
 
-            // 長度不同時補零到同長度
-            if (a.Length > b.Length)
-            {
-                b = b.PadLeft(a.Length, '0');
-                i = a.Length - 1;
-            }
-            else if (b.Length > a.Length)
-            {
-                a = a.PadLeft(b.Length, '0');
-                i = b.Length - 1;
-            }
-            else
-                i = a.Length - 1;
+            maxLen--;
 
-            // 依據各種情況進行相加
-            while (i > -1)
+            // 依據各種狀況進行相加
+            while (maxLen > -1)
             {
-                if (a[i] == b[i] && a[i] == '0')
+                add = a[maxLen] + b[maxLen];
+
+                if (add == 98) // a+b=2
                 {
-                    if (!carry)
-                        sb.Append("0");
+                    if (carry)
+                        sb.Insert(0, '1');
                     else
                     {
-                        sb.Append("1");
-                        carry = false;
-                    }
-                }
-                else if (a[i] == b[i] && a[i] == '1')
-                {
-                    if (!carry)
-                    {
-                        sb.Append("0");
+                        sb.Insert(0, '0');
                         carry = true;
                     }
-                    else
-                        sb.Append("1");
                 }
-                else if (a[i] == '1' || b[i] == '1')
+                else if (add == 97) // a+b=1
                 {
-                    if (!carry)
-                        sb.Append("1");
+                    if (carry)
+                        sb.Insert(0, '0');
                     else
-                        sb.Append("0");
+                        sb.Insert(0, '1');
+                }
+                else // a+b=0
+                {
+                    if (carry)
+                    {
+                        sb.Insert(0, '1');
+                        carry = false;
+                    }
+                    else
+                        sb.Insert(0, '0');
                 }
 
-                i--;
+                maxLen--;
             }
 
-            if (carry)
-                sb.Append("1");
+            if (carry) sb.Insert(0, '1');
 
-            return new string(sb.ToString().Reverse().ToArray());
+            return sb.ToString();
         }
     }
 }

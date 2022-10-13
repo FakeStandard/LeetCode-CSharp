9b331c8fdec9ed08ef1c8d20e956ab45bbb68a5f
Riley Lin <75247434+FakeStandard@users.noreply.github.com>
2022-10-13 10:21:13 +0800

    Update 0026-Remove-duplicates-from-sorted-array.cs

--- Solution/0026.Remove-duplicates-from-sorted-array/0026-Remove-duplicates-from-sorted-array.cs
+++ Solution/0026.Remove-duplicates-from-sorted-array/0026-Remove-duplicates-from-sorted-array.cs
@@ -13,42 +13,10 @@ namespace Solution._0026.Remove_duplicates_from_sorted_array
             int res = 0;
 
             for (int i = 1; i < nums.Length; i++)
-            {
                 if (nums[res] != nums[i])
-                {
-                    res++;
-                    nums[res] = nums[i];
-                }
-            }
+                    nums[++res] = nums[i];
 
             return ++res;
-
-            //if (nums.Length == 0) return 0;
-
-            //int res = 1;
-            //bool b = false;
-
-            //for(int i = 1; i < nums.Length; i++)
-            //{
-            //    b = false;
-
-            //    for(int j = 0; j < res; j++)
-            //    {
-            //        if (nums[i] == nums[j])
-            //        {
-            //            b = true;
-            //            break;
-            //        }
-            //    }
-
-            //    if (!b)
-            //    {
-            //        nums[res] = nums[i];
-            //        res++;
-            //    }
-            //}
-
-            //return res;
         }
     }
 }

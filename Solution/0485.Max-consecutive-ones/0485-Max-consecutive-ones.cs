c82edc754cdd0e033449e037e27600a9c1378458
Riley Lin <41749800+RileyLin91@users.noreply.github.com>
2022-12-02 10:41:19 +0800

    (0485) new

---
+++ Solution/0485.Max-consecutive-ones/0485-Max-consecutive-ones.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0485.Max_consecutive_ones
+{
+    public class _0485_Max_consecutive_ones
+    {
+        public int FindMaxConsecutiveOnes(int[] nums)
+        {
+            // to be Optimized
+            int max = 0;
+            int temp = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+                if (nums[i] == 1) temp++;
+                else
+                {
+                    max = max > temp ? max : temp;
+                    temp = 0;
+                }
+
+            return max > temp ? max : temp;
+        }
+
+        private int GetMax(int num1, int num2) => num1 > num2 ? num1 : num2;
+    }
+}

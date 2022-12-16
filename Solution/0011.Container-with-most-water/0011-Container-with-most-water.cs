4188f40bc1c7e840e5bcdd4b3fa792ab1258e947
riley.lin <75247434+FakeStandard@users.noreply.github.com>
2022-12-16 14:04:14 +0800

    (0011) pending

---
+++ Solution/0011.Container-with-most-water/0011-Container-with-most-water.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0011.Container_with_most_water
+{
+    public class _0011_Container_with_most_water
+    {
+        public int MaxArea(int[] height)
+        {
+            int area = 0;
+
+            return area;
+        }
+    }
+}
---
+++ UnitTest/_0011_Container_with_most_water_Test.cs
@@ -0,0 +1,40 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0011.Container_with_most_water;
+
+namespace _0011.Container_with_most_water.Tests
+{
+    [TestClass()]
+    public class _0011_Container_with_most_water_Test
+    {
+        _0011_Container_with_most_water solution = new _0011_Container_with_most_water();
+
+        [TestMethod()]
+        public void MaxArea_Test1()
+        {
+            // Arrange
+            int[] height = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+            var expected = 49;
+
+            // Act
+            var actual = solution.MaxArea(height);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MaxArea_Test2()
+        {
+            // Arrange
+            int[] height = { 1, 1 };
+            var expected = 1;
+
+            // Act
+            var actual = solution.MaxArea(height);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

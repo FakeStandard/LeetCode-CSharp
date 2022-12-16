ea41b325b919f13edaccec012cdab38a8775b2bd
riley.lin <75247434+FakeStandard@users.noreply.github.com>
2022-12-16 11:15:52 +0800

    (0476) new

---
+++ Solution/0476.Number-complement/0476-Number-complement.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0476.Number_complement
+{
+    public class _0476_Number_complement
+    {
+        public int FindComplement(int num)
+        {
+            for (int i = 0; (1 << i) <= num && i < 31; i++)
+                num = num ^ (1 << i);
+
+            return num;
+        }
+    }
+}
---
+++ UnitTest/_0476_Number_complement_Test.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0476.Number_complement;
+
+namespace _0476.Number_complement.Tests
+{
+    [TestClass()]
+    public class _0476_Number_complement_Test
+    {
+        _0476_Number_complement solution = new _0476_Number_complement();
+
+        [TestMethod()]
+        public void FindComplement_Test1()
+        {
+            // Arrange
+            int num = 5;
+            int expected = 2;
+
+            // Act
+            var actual = solution.FindComplement(num);
+
+            // Actual
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void FindComplement_Test2()
+        {
+            // Arrange
+            int num = 1;
+            int expected = 0;
+
+            // Act
+            var actual = solution.FindComplement(num);
+
+            // Actual
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

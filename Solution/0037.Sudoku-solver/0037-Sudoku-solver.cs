cc2336b2cafda255b217a8e2511287671f26d8d2
riley.lin <75247434+FakeStandard@users.noreply.github.com>
2022-12-17 16:25:46 +0800

    (0037) new

---
+++ Solution/0037.Sudoku-solver/0037-Sudoku-solver.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Solution._0037.Sudoku_solver
+{
+    public class _0037_Sudoku_solver
+    {
+        public char[][] SolveSudoku(char[][] board)
+        {
+            Stack<Grid> stack = new Stack<Grid>();
+
+            int x = 0;
+            int y = 0;
+            char c = '1';
+
+            while (x < 9 && y < 9)
+            {
+                if (board[x][y] == '.')
+                {
+                    bool b = false;
+
+                    for (char i = c; i <= '9'; i++)
+                    {
+                        b = checkChar(board, x, y, i);
+
+                        if (b)
+                        {
+                            board[x][y] = i;
+                            stack.Push(new Grid { x = x, y = y, c = i });
+                            y++;
+                            c = '1';
+                            break;
+                        }
+                    }
+
+                    // backtracking
+                    if (!b)
+                    {
+                        Grid g = stack.Pop();
+                        x = g.x;
+                        y = g.y;
+                        c = ++g.c; // check from the next
+                        board[x][y] = '.';
+                    }
+                }
+                else y++;
+
+                if (y >= 9)
+                {
+                    y = 0;
+                    x++;
+                }
+            }
+
+            return board;
+        }
+
+        private bool checkChar(char[][] board, int x, int y, int c)
+        {
+            // check row
+            for (int i = 0; i < 9; i++)
+                if (board[x][i] == c)
+                    return false;
+
+            // check column
+            for (int i = 0; i < 9; i++)
+                if (board[i][y] == c)
+                    return false;
+
+            // check box
+            int row = (x / 3) * 3;
+            int col = (y / 3) * 3;
+
+            for (int i = row; i < row + 3; i++)
+                for (int j = col; j < col + 3; j++)
+                    if (board[i][j] == c)
+                        return false;
+
+            return true;
+        }
+
+        private class Grid
+        {
+            public int x;
+            public int y;
+            public char c;
+        }
+    }
+}
---
+++ UnitTest/_0037_Sudoku_solver_Tests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0037.Sudoku_solver;
+using FluentAssertions;
+
+namespace _0037.Sudoku_solver.Tests
+{
+    [TestClass()]
+    public class _0037_Sudoku_solver_Tests
+    {
+        _0037_Sudoku_solver solution = new _0037_Sudoku_solver();
+
+        [TestMethod()]
+        public void SolveSudoku_Test1()
+        {
+            // Arrange
+            char[][] board = {
+               new char[] { '5', '3', '.', '.', '7', '.', '.', '.', '.' },
+               new char[] { '6', '.', '.', '1', '9', '5', '.', '.', '.' },
+               new char[] { '.', '9', '8', '.', '.', '.', '.', '6', '.' },
+               new char[] { '8', '.', '.', '.', '6', '.', '.', '.', '3' },
+               new char[] { '4', '.', '.', '8', '.', '3', '.', '.', '1' },
+               new char[] { '7', '.', '.', '.', '2', '.', '.', '.', '6' },
+               new char[] { '.', '6', '.', '.', '.', '.', '2', '8', '.' },
+               new char[] { '.', '.', '.', '4', '1', '9', '.', '.', '5' },
+               new char[] { '.', '.', '.', '.', '8', '.', '.', '7', '9' }
+            };
+
+            char[][] expected = {
+                new char[] { '5', '3', '4', '6', '7', '8', '9', '1', '2' },
+                new char[] { '6', '7', '2', '1', '9', '5', '3', '4', '8' },
+                new char[] { '1', '9', '8', '3', '4', '2', '5', '6', '7' },
+                new char[] { '8', '5', '9', '7', '6', '1', '4', '2', '3' },
+                new char[] { '4', '2', '6', '8', '5', '3', '7', '9', '1' },
+                new char[] { '7', '1', '3', '9', '2', '4', '8', '5', '6' },
+                new char[] { '9', '6', '1', '5', '3', '7', '2', '8', '4' },
+                new char[] { '2', '8', '7', '4', '1', '9', '6', '3', '5' },
+                new char[] { '3', '4', '5', '2', '8', '6', '1', '7', '9' }
+            };
+
+            // Act
+            var actual = solution.SolveSudoku(board);
+
+            // Assert
+            actual.Should().BeEquivalentTo(expected);
+        }
+    }
+}

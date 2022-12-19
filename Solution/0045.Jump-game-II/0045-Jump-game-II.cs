3b451c1654260af19512af4b91034c2073846c0b
riley.lin <75247434+FakeStandard@users.noreply.github.com>
2022-12-19 10:48:02 +0800

    (0045) pending

---
+++ Solution/0045.Jump-game-II/0045-Jump-game-II.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0045.Jump_game_II
+{
+    public class _0045_Jump_game_II
+    {
+        public int Jump(int[] nums)
+        {
+            int step = 0;
+
+            return 0;
+        }
+    }
+}
---
+++ UnitTest/_0045_Jump_game_II_Tests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0045.Jump_game_II;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _0045.Jump_game_II.Tests
+{
+    [TestClass()]
+    public class _0045_Jump_game_II_Tests
+    {
+        _0045_Jump_game_II solution = new _0045_Jump_game_II();
+
+        [TestMethod()]
+        public void Jump_Test1()
+        {
+            // Arrange
+            int[] nums = { 2, 3, 1, 1, 4 };
+            var expected = 2;
+
+            // Act
+            var actual = solution.Jump(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Jump_Test2()
+        {
+            // Arrange
+            int[] nums = { 2, 3, 0, 1, 4 };
+            var expected = 2;
+
+            // Act
+            var actual = solution.Jump(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}

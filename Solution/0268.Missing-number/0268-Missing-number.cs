6faae3c758ded7d1ecd21ed5da03c1a9a40cf9e7
riley.lin <75247434+FakeStandard@users.noreply.github.com>
2022-12-27 10:38:41 +0800

    (0268) new

---
+++ Solution/0268.Missing-number/0268-Missing-number.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Solution._0268.Missing_number
+{
+    public class _0268_Missing_number
+    {
+        public int MissingNumber(int[] nums)
+        {
+            int res = nums.Length;
+
+            for (int i = 0; i < nums.Length; i++)
+                res = res ^ i ^ nums[i];
+
+            return res;
+        }
+    }
+}
---
+++ UnitTest/_0268_Missing_number_Test.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solution._0268.Missing_number;
+
+namespace _0268.Missing_number.Tests
+{
+    [TestClass()]
+    public class _0268_Missing_number_Test
+    {
+        _0268_Missing_number solution = new _0268_Missing_number();
+
+        [TestMethod()]
+        public void MissingNumber_Test1()
+        {
+            // Arrange
+            int[] nums = { 3, 0, 1 };
+            int expected = 2;
+
+            // Act
+            var actual = solution.MissingNumber(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MissingNumber_Test2()
+        {
+
+            // Arrange
+            int[] nums = { 0, 1 };
+            int expected = 2;
+
+            // Act
+            var actual = solution.MissingNumber(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void MissingNumber_Test3()
+        {
+
+            // Arrange
+            int[] nums = { 9, 6, 4, 2, 3, 5, 7, 0, 1 };
+            int expected = 8;
+
+            // Act
+            var actual = solution.MissingNumber(nums);
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
